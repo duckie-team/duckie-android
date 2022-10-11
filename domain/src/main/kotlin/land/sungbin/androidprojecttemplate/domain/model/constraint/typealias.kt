3e25d606993e1888a33eae0c83171886571fdacc
jisungbin <ji@sungb.in>
2022-10-12 06:47:00 +0900

    feat: typealias 에 internal 지정

--- domain/src/main/kotlin/land/sungbin/androidprojecttemplate/domain/model/constraint/typealias.kt
+++ domain/src/main/kotlin/land/sungbin/androidprojecttemplate/domain/model/constraint/typealias.kt
@@ -1,7 +1,7 @@
 package land.sungbin.androidprojecttemplate.domain.model.constraint
 
-typealias Categories = List<Category>
-typealias Users = List<String>
-typealias Tags = List<String>
-typealias Badges = List<Badge>
-typealias Collections = List<Int>
+internal typealias Categories = List<Category>
+internal typealias Users = List<String>
+internal typealias Tags = List<String>
+internal typealias Badges = List<Badge>
+internal typealias Collections = List<Int>
