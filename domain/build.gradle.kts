92b51f05573095ac1e789dd0f2d0bea6f057d4b9
jisungbin <ji@sungb.in>
2022-10-12 06:46:36 +0900

    chore: 그레이들 코드 정리

--- domain/build.gradle.kts
+++ domain/build.gradle.kts
@@ -1,40 +1,12 @@
 plugins {
     id("com.android.library")
     id("kotlin-android")
-    id("org.jetbrains.dokka") version Versions.BuildUtil.Dokka
-    id("de.mannodermaus.android-junit5")
-    id("name.remal.check-dependency-updates") version Versions.BuildUtil.CheckDependencyUpdates
-    // jacoco
 }
-
-/*jacoco {
-    toolVersion = Versions.Test.JaCoCo
-}
-
-tasks.withType<JacocoReport> {
-    reports {
-        html.required.set(true)
-        html.outputLocation.set(layout.projectDirectory.dir("../documents/coverage/jacoco/html"))
-        xml.required.set(true) // codecov depends on xml format report
-        xml.outputLocation.set(layout.projectDirectory.file("../documents/coverage/jacoco/report.xml"))
-    }
-}
-
-tasks.withType<Test> {
-    useJUnitPlatform()
-    finalizedBy("jacocoTestReport")
-}*/
 
 android {
     namespace = "land.sungbin.androidprojecttemplate.domain"
 }
 
 dependencies {
-    implementation("androidx.annotation:annotation:1.3.0")
-    val projects = listOf(
-        ProjectConstants.SharedDomain
-    )
-    projects.forEach(::projectImplementation)
-
-    Dependencies.Test.forEach(::testImplementation)
+    implementation("androidx.annotation:annotation:1.5.0")
 }
