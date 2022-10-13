3110251fcb035efacfcc4a8a2b4316c0ab1b771c
EvergreenTree97 <tkdfhr7497@naver.com>
2022-10-14 01:26:48 +0900

    feat: 홈 피드 공통 틀

--- presentation/src/main/kotlin/land/sungbin/androidprojecttemplate/home/component/feeds.kt
+++ presentation/src/main/kotlin/land/sungbin/androidprojecttemplate/home/component/feeds.kt
@@ -4,16 +4,25 @@ import androidx.annotation.DrawableRes
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.Stable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.DpSize
 import androidx.compose.ui.unit.dp
 import kotlinx.collections.immutable.PersistentList
 import kotlinx.collections.immutable.persistentListOf
 import land.sungbin.androidprojecttemplate.R
+import team.duckie.quackquack.ui.color.QuackColor
 import team.duckie.quackquack.ui.component.QuackBody1
+import team.duckie.quackquack.ui.component.QuackBody3
+import team.duckie.quackquack.ui.component.QuackIconTextToggle
 import team.duckie.quackquack.ui.component.QuackImage
+import team.duckie.quackquack.ui.component.QuackRoundImage
 import team.duckie.quackquack.ui.component.QuackRowTag
 import team.duckie.quackquack.ui.component.QuackSubtitle2
+import team.duckie.quackquack.ui.icon.QuackIcon
 
 val dummyTags = persistentListOf(
     "디즈니", "픽사", "마블", "DC", "애니메이션", "지브리", "ost", "피규어"
@@ -67,5 +76,106 @@ internal fun FeedHeader(
             )
         }
 
+    }
+}
+
+data class FeedHolder(
+    val profile: Any?,
+    val nickname: String,
+    val time: String,
+    val content: String,
+    val commentCount: () -> String,
+    val onClickComment: () -> Unit,
+    val isLike: () -> Boolean,
+    val likeCount: () -> String,
+    val onClickLike: () -> Unit,
+)
+
+@Stable
+private val ProfileSize = DpSize(
+    width = 36.dp,
+    height = 36.dp,
+)
+
+@Composable
+internal fun HomeFeed(
+    feedHolder: FeedHolder,
+    component: (@Composable () -> Unit)? = null,
+) {
+    Row(
+        horizontalArrangement = Arrangement.spacedBy(
+            space = 8.dp,
+        )
+    ) {
+        QuackRoundImage(
+            src = feedHolder.profile,
+            size = ProfileSize,
+        )
+        Column(
+            verticalArrangement = Arrangement.spacedBy(
+                space = 4.dp,
+            )
+        ) {
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalArrangement = Arrangement.SpaceBetween,
+                verticalAlignment = Alignment.CenterVertically,
+            ) {
+                Row(
+                    horizontalArrangement = Arrangement.spacedBy(
+                        space = 4.dp,
+                    ),
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    QuackSubtitle2(
+                        text = feedHolder.nickname,
+                    )
+                    QuackBody3(
+                        text = feedHolder.time,
+                    )
+                }
+                QuackImage(
+                    src = QuackIcon.More,
+                    overrideSize = DpSize(
+                        width = 16.dp,
+                        height = 16.dp,
+                    ),
+                    tint = QuackColor.Gray1,
+                )
+            }
+            Column(
+                verticalArrangement = Arrangement.spacedBy(
+                    space = 8.dp,
+                )
+            ) {
+                QuackBody1(
+                    text = feedHolder.content,
+                    singleLine = false,
+                )
+                if (component != null) {
+                    component()
+                }
+                Row(
+                    horizontalArrangement = Arrangement.spacedBy(
+                        space = 24.dp,
+                    )
+                ) {
+                    QuackIconTextToggle(
+                        checkedIcon = null,
+                        uncheckedIcon = QuackIcon.Comment,
+                        checked = false,
+                        text = feedHolder.commentCount(),
+                        onToggle = feedHolder.onClickComment,
+                    )
+                    QuackIconTextToggle(
+                        checkedIcon = QuackIcon.FilledHeart,
+                        uncheckedIcon = QuackIcon.Heart,
+                        checked = feedHolder.isLike(),
+                        text = feedHolder.likeCount(),
+                        onToggle = feedHolder.onClickLike,
+                    )
+                }
+            }
+        }
     }
 }
