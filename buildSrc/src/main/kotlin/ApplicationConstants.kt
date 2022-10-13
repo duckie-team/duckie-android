b0288964e106ad8868e4722cb087123e179a4275
EvergreenTree97 <tkdfhr7497@naver.com>
2022-10-14 01:10:13 +0900

    chore: sdk 버전 변경

--- buildSrc/src/main/kotlin/ApplicationConstants.kt
+++ buildSrc/src/main/kotlin/ApplicationConstants.kt
@@ -11,9 +11,9 @@ import org.gradle.api.JavaVersion
 
 // TODO: input your constants
 object ApplicationConstants {
-    const val minSdk = 21
-    const val targetSdk = 32
-    const val compileSdk = 32
+    const val minSdk = 23
+    const val targetSdk = 33
+    const val compileSdk = 33
     const val jvmTarget = "11"
     const val versionCode = 11
     const val versionName = "0.0.0"
