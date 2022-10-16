28a3bf5adb83bd5f88a69587906c2f505c8063cb
EvergreenTree97 <tkdfhr7497@naver.com>
2022-10-16 16:51:22 +0900

    feat: Navigation이 MainActivity에 위치하도록 변경

--- presentation/src/main/kotlin/land/sungbin/androidprojecttemplate/MainActivity.kt
+++ presentation/src/main/kotlin/land/sungbin/androidprojecttemplate/MainActivity.kt
@@ -3,13 +3,38 @@ package land.sungbin.androidprojecttemplate
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
 import land.sungbin.androidprojecttemplate.home.HomeScreen
+import land.sungbin.androidprojecttemplate.home.HomeViewModel
+import team.duckie.quackquack.ui.component.QuackBottomNavigation
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            HomeScreen()
+            var selectedNavigationIndex by remember {
+                mutableStateOf(0)
+            }
+            Column(modifier = Modifier.fillMaxSize()) {
+                Box(modifier = Modifier.weight(1f)) {
+                    HomeScreen(
+                        HomeViewModel()
+                    )
+                }
+                QuackBottomNavigation(
+                    selectedIndex = selectedNavigationIndex,
+                    onClick = { index ->
+                        selectedNavigationIndex = index
+                    },
+                )
+            }
         }
     }
 }
