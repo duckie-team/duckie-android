60947f1a09a18271701b4ff88adb28b51dd269b9
jisungbin <ji@sungb.in>
2022-10-18 02:28:49 +0900

    feat: 모델 어노테이션들 추가

--- domain/src/main/kotlin/land/sungbin/androidprojecttemplate/domain/model/util/key.kt
+++
@@ -1,9 +0,0 @@
-package land.sungbin.androidprojecttemplate.domain.model.util
-
-@Target(
-    AnnotationTarget.PROPERTY,
-    AnnotationTarget.VALUE_PARAMETER,
-)
-annotation class PK
-
-annotation class FK
