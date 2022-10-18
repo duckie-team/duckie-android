6f666c816ade3045ff4ad8bddb92808beadb6cfc
EvergreenTree97 <tkdfhr7497@naver.com>
2022-10-19 02:15:04 +0900

    feat: homeState에 필요한 값들

--- presentation/src/main/kotlin/land/sungbin/androidprojecttemplate/home/HomeState.kt
+++ presentation/src/main/kotlin/land/sungbin/androidprojecttemplate/home/HomeState.kt
@@ -1,9 +1,17 @@
 package land.sungbin.androidprojecttemplate.home
 
 import land.sungbin.androidprojecttemplate.common.UiStatus
+import land.sungbin.androidprojecttemplate.domain.model.Comment
 import land.sungbin.androidprojecttemplate.domain.model.Feed
+import land.sungbin.androidprojecttemplate.domain.model.Follow
+import land.sungbin.androidprojecttemplate.domain.model.Heart
+import land.sungbin.androidprojecttemplate.domain.model.User
 
 data class HomeState(
     val status: UiStatus = UiStatus.Loading,
+    val user: User? = null,
+    val follow: Follow? = null,
     val feeds: List<Feed> = emptyList(),
+    val heart: List<Heart> = emptyList(),
+    val comment: List<Comment> = emptyList(),
 )
