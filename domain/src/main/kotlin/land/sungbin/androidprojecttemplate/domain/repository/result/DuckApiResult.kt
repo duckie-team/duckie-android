b809a36e5d1cd7d16cae6faebe6e24c650b5b738
jisungbin <ji@sungb.in>
2022-10-20 02:37:07 +0900

    feat: DuckApiResult 에 공변성 추가 및 확장 함수 추가

--- domain/src/main/kotlin/land/sungbin/androidprojecttemplate/domain/repository/result/DuckApiResult.kt
+++ domain/src/main/kotlin/land/sungbin/androidprojecttemplate/domain/repository/result/DuckApiResult.kt
@@ -5,12 +5,52 @@ import land.sungbin.androidprojecttemplate.domain.repository.DuckRepository
 /**
  * [DuckRepository] 의 결과를 나타냅니다.
  */
-// TODO: 공변성 체크. 왜 `<out T>` 을 해야 하는지 아직 정확히 모름.
-sealed interface DuckApiResult<T> {
+sealed interface DuckApiResult<out T> {
     /**
      * [DuckRepository] 에서 API 호출에 예외가 발생했을 때 반환되는 결과입니다.
      *
      * @param message 발생한 예외 메시지
      */
-    data class Exception<T>(val message: String?) : DuckApiResult<T>
+    data class Exception<out T>(val message: String?) : DuckApiResult<T>
+}
+
+/**
+ * [runCatching] 과 유사하며, 작업 중 예외 발생시 [DuckApiResult.Exception] 을
+ * 반환합니다.
+ *
+ * @param execute 실행할 [DuckRepository] 작업
+ * @return [execute] 작업의 결과.
+ * 만약 [execute] 를 실행한 결과가 성공적이라면 그대로 [DuckApiResult] 를 반환하고,
+ * 실행 도중에 예외가 발생했다면 예외 메시지를 [DuckApiResult.Exception] 으로 반환합니다.
+ */
+internal inline fun <T> runDuckApiCatching(
+    execute: () -> DuckApiResult<T>,
+) = try {
+    execute()
+} catch (exception: Throwable) {
+    DuckApiResult.Exception(
+        message = exception.message,
+    )
+}
+
+/**
+ * [DuckApiResult] 가 [DuckApiResult.Exception] 일 때 주어진
+ * 콜백을 실행하는 확장 함수 입니다.
+ *
+ * @receiver [DuckRepository] 로 부터 받은 결과
+ * @param onException [DuckApiResult.Exception] 일 때 실행할 콜백
+ * @return receiver 로 받는 [DuckApiResult].
+ * method chaining 을 위해 반환합니다.
+ */
+fun DuckApiResult<*>.onExcepion(
+    onException: (message: String?) -> Unit,
+): DuckApiResult<*> = when (this) {
+    is DuckApiResult.Exception -> {
+        onException(
+            /* message = */
+            message,
+        )
+        this
+    }
+    else -> this
 }
