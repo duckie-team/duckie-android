390acb50848c8f063406419698c3c994a4b5d7c4
jisungbin <ji@sungb.in>
2022-10-20 02:37:35 +0900

    feat: 공변성 추가 및 확장 함수 추가

--- domain/src/main/kotlin/land/sungbin/androidprojecttemplate/domain/repository/result/DuckFetchResult.kt
+++ domain/src/main/kotlin/land/sungbin/androidprojecttemplate/domain/repository/result/DuckFetchResult.kt
@@ -5,16 +5,57 @@ import land.sungbin.androidprojecttemplate.domain.repository.DuckFetchRepository
 /**
  * [DuckFetchRepository] 의 결과를 나타냅니다.
  */
-sealed class DuckFetchResult<T> : DuckApiResult<T> {
+sealed class DuckFetchResult<out T> : DuckApiResult<T> {
     /**
      * [DuckFetchRepository] 에서 값 fetch 에 성공했을 때 반환되는 결과입니다.
      *
      * @param value fetch 된 값
      */
-    data class Success<T>(val value: T) : DuckFetchResult<T>()
+    data class Success<out T>(val value: T) : DuckFetchResult<T>()
 
     /**
      * [DuckFetchRepository] 에서 fetch 된 값이 없을 때 반환되는 결과입니다.
      */
-    class Empty<T> : DuckFetchResult<T>()
+    class Empty<out T> : DuckFetchResult<T>()
+}
+
+/**
+ * [DuckApiResult] 가 [DuckFetchResult.Success] 일 때 주어진
+ * 콜백을 실행하는 확장 함수 입니다.
+ *
+ * @receiver [DuckFetchRepository] 로 부터 받은 결과
+ * @param onSuccess [DuckFetchResult.Success] 일 때 실행할 콜백
+ * @return receiver 로 받는 [DuckApiResult].
+ * method chaining 을 위해 반환합니다.
+ */
+fun <T> DuckApiResult<T>.onSuccess(
+    onSuccess: (value: T) -> Unit,
+): DuckApiResult<T> = when (this) {
+    is DuckFetchResult.Success -> {
+        onSuccess(
+            /* value = */
+            value,
+        )
+        this
+    }
+    else -> this
+}
+
+/**
+ * [DuckApiResult] 가 [DuckFetchResult.Empty] 일 때 주어진
+ * 콜백을 실행하는 확장 함수 입니다.
+ *
+ * @receiver [DuckFetchRepository] 로 부터 받은 결과
+ * @param onEmpty [DuckFetchResult.Empty] 일 때 실행할 콜백
+ * @return receiver 로 받는 [DuckApiResult].
+ * method chaining 을 위해 반환합니다.
+ */
+fun DuckApiResult<*>.onEmpty(
+    onEmpty: () -> Unit,
+): DuckApiResult<*> = when (this) {
+    is DuckFetchResult.Empty -> {
+        onEmpty()
+        this
+    }
+    else -> this
 }
--- domain/src/main/kotlin/land/sungbin/androidprojecttemplate/domain/repository/result/DuckUpsertResult.kt
+++ domain/src/main/kotlin/land/sungbin/androidprojecttemplate/domain/repository/result/DuckUpsertResult.kt
@@ -5,9 +5,28 @@ import land.sungbin.androidprojecttemplate.domain.repository.DuckUpsertRepositor
 /**
  * [DuckUpsertRepository] 의 결과를 나타냅니다.
  */
-sealed class DuckUpsertResult<T> : DuckApiResult<T> {
+sealed class DuckUpsertResult<out T> : DuckApiResult<T> {
     /**
      * [DuckUpsertRepository] 에서 값 upsert 에 성공했을 때 반환되는 결과입니다.
      */
-    class Done<T> : DuckUpsertResult<T>()
+    class Done<out T> : DuckUpsertResult<T>()
+}
+
+/**
+ * [DuckApiResult] 가 [DuckUpsertResult.Done] 일 때 주어진
+ * 콜백을 실행하는 확장 함수 입니다.
+ *
+ * @receiver [DuckUpsertRepository] 로 부터 받은 결과
+ * @param onDone [DuckUpsertResult.Done] 일 때 실행할 콜백
+ * @return receiver 로 받는 [DuckApiResult].
+ * method chaining 을 위해 반환합니다.
+ */
+fun DuckApiResult<*>.onDone(
+    onDone: () -> Unit,
+): DuckApiResult<*> = when (this) {
+    is DuckUpsertResult.Done -> {
+        onDone()
+        this
+    }
+    else -> this
 }
