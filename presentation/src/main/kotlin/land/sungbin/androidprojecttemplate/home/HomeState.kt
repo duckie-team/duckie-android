7dcfa33f0a8390eaba9db1458dd61926a7db98e3
EvergreenTree97 <tkdfhr7497@naver.com>
2022-10-20 17:30:26 +0900

    feat: 뷰모델에 상태 추

--- presentation/src/main/kotlin/land/sungbin/androidprojecttemplate/home/HomeState.kt
+++ presentation/src/main/kotlin/land/sungbin/androidprojecttemplate/home/HomeState.kt
@@ -1,17 +1,20 @@
 package land.sungbin.androidprojecttemplate.home
 
+import kotlinx.collections.immutable.PersistentList
+import kotlinx.collections.immutable.toPersistentList
 import land.sungbin.androidprojecttemplate.common.UiStatus
 import land.sungbin.androidprojecttemplate.domain.model.Comment
 import land.sungbin.androidprojecttemplate.domain.model.Feed
 import land.sungbin.androidprojecttemplate.domain.model.Follow
 import land.sungbin.androidprojecttemplate.domain.model.Heart
 import land.sungbin.androidprojecttemplate.domain.model.User
+import team.duckie.quackquack.ui.component.QuackBottomSheetItem
 
 data class HomeState(
     val status: UiStatus = UiStatus.Loading,
-    val user: User? = null,
-    val follow: Follow? = null,
     val feeds: List<Feed> = emptyList(),
-    val heart: List<Heart> = emptyList(),
-    val comment: List<Comment> = emptyList(),
+    val selectedUser: String = "",
+    val filterBottomSheetItems: List<QuackBottomSheetItem> = emptyList(),
+    val moreBottomSheetItems: List<QuackBottomSheetItem> = emptyList(),
+    val interestedTags: List<String> = emptyList(),
 )
