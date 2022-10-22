f6d468266f6813937087a186e8016c91f061b0e1
jisungbin <ji@sungb.in>
2022-10-23 04:48:12 +0900

    feat: 누락된 필드 ownerId 추가

--- domain/src/main/kotlin/land/sungbin/androidprojecttemplate/domain/model/SaleRequest.kt
+++ domain/src/main/kotlin/land/sungbin/androidprojecttemplate/domain/model/SaleRequest.kt
@@ -9,12 +9,14 @@ import land.sungbin.androidprojecttemplate.domain.model.util.requireInput
  *
  * @param id 고유 아이디
  * @param feedId 판매 요청할 물품이 있는 [피드 아이디][Feed.id]
+ * @param ownerId 판매 요청할 물품을 소유하고 있는 [유저 아이디][User.nickname]
  * @param requesterId 물품 판매를 요청한 [유저 아이디][User.nickname]
  * @param result 판매 요청 수락 여부
  */
 data class SaleRequest(
     @PK val id: String,
     @FK val feedId: String,
+    @FK val ownerId: String,
     @FK val requesterId: String,
     val result: Boolean,
 ) {
@@ -26,6 +28,10 @@ data class SaleRequest(
         requireInput(
             field = "feedId",
             value = feedId,
+        )
+        requireInput(
+            field = "ownerId",
+            value = ownerId,
         )
         requireInput(
             field = "requesterId",
