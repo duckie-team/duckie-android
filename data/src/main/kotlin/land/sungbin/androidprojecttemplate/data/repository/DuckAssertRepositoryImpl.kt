3c07d335cefc9c79f84f2d945805380851d32a35
jisungbin <ji@sungb.in>
2022-10-23 08:13:33 +0900

    feat: DuckAssertRepositoryImpl 구현

---
+++ data/src/main/kotlin/land/sungbin/androidprojecttemplate/data/repository/DuckAssertRepositoryImpl.kt
@@ -0,0 +1,30 @@
+@file:Suppress("KDocFields")
+
+package land.sungbin.androidprojecttemplate.data.repository
+
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
+import kotlin.coroutines.resume
+import kotlin.coroutines.resumeWithException
+import kotlinx.coroutines.suspendCancellableCoroutine
+import land.sungbin.androidprojecttemplate.domain.repository.DuckAssertRepository
+
+class DuckAssertRepositoryImpl : DuckAssertRepository {
+    override suspend fun checkNicknameAvailable(
+        nickname: String,
+    ) = suspendCancellableCoroutine<Boolean> { continuation ->
+        Firebase.firestore.collection("users")
+            .whereEqualTo("nickname", nickname)
+            .get()
+            .addOnSuccessListener { result ->
+                continuation.resume(
+                    value = result.isEmpty,
+                )
+            }
+            .addOnFailureListener { exception ->
+                continuation.resumeWithException(
+                    exception = exception,
+                )
+            }
+    }
+}
