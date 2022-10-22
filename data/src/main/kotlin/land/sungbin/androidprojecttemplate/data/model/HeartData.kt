8bee3491d3388a609f69e9c631e709dad8a8bf86
jisungbin <ji@sungb.in>
2022-10-23 04:46:31 +0900

    feat: HeartTarget 에 따라 CommentHeartData, FeedHeartData 으로 분리

--- data/src/main/kotlin/land/sungbin/androidprojecttemplate/data/model/HeartData.kt
+++ data/src/main/kotlin/land/sungbin/androidprojecttemplate/data/model/HeartData.kt
@@ -1,10 +1,23 @@
 package land.sungbin.androidprojecttemplate.data.model
 
-import land.sungbin.androidprojecttemplate.data.model.util.NewField
+import land.sungbin.androidprojecttemplate.domain.model.constraint.HeartTarget
 
-internal data class HeartData(
-    @NewField val type: Int? = null,
-    val feedId: String? = null,
-    val userId: String? = null,
-)
+internal interface HeartData {
+    val type: HeartTarget
+    val targetId: String?
+    val userId: String?
+}
 
+internal data class CommentHeartData(
+    override val targetId: String? = null,
+    override val userId: String? = null,
+) : HeartData {
+    override val type = HeartTarget.Comment
+}
+
+internal data class FeedHeartData(
+    override val targetId: String? = null,
+    override val userId: String? = null,
+) : HeartData {
+    override val type = HeartTarget.Feed
+}
