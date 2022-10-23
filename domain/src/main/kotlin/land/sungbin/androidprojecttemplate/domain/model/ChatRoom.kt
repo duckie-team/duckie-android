b5cad572938f06e631aec676b45286771173223f
jisungbin <ji@sungb.in>
2022-10-23 15:22:12 +0900

    feat: ChatRoom 에 누락된 필드 추가

--- domain/src/main/kotlin/land/sungbin/androidprojecttemplate/domain/model/ChatRoom.kt
+++ domain/src/main/kotlin/land/sungbin/androidprojecttemplate/domain/model/ChatRoom.kt
@@ -12,18 +12,19 @@ import land.sungbin.androidprojecttemplate.domain.model.util.requireSetting
 /**
  * 채팅방 모델
  *
+ * [categories] 를 포함한 [categories] 이후로 나오는 필드들은
+ * [type] 이 [ChatRoomType.Open] 일 때만 유효합니다.
+ * [type] 이 [ChatRoomType.Open] 이 아니라면 null 을 받습니다.
+ *
  * @param id 고유 아이디
  * @param type 채팅방 타입
  * @param coverImageUrl 채팅방 커버 이미지 주소.
  * null 은 설정된 커버 이미지가 없음을 의미합니다.
  * @param name 채팅방 이름
  * @param ownerId 채팅방 방장 [유저 아이디][User.nickname]
+ * @param joinUsers 채팅방에 참여중인 [유저 아이디][User.nickname]
  * @param categories 채팅방 카테고리.
- * [type] 이 [ChatRoomType.Open] 일 때만 유효합니다.
- * [type] 이 [ChatRoomType.Open] 이 아니라면 null 을 받습니다.
  * @param tags 채팅방 태그.
- * [type] 이 [ChatRoomType.Open] 일 때만 유효합니다.
- * [type] 이 [ChatRoomType.Open] 이 아니라면 null 을 받습니다.
  */
 data class ChatRoom(
     @PK val id: String,
@@ -31,6 +32,7 @@ data class ChatRoom(
     val coverImageUrl: String?,
     val name: String,
     @FK val ownerId: String,
+    @FK val joinUsers: List<String>,
     val categories: Categories?,
     val tags: Tags?,
 ) {
