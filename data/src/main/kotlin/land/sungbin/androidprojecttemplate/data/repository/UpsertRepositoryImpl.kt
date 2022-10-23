37bdc9ddf5abf427eb2c21c7f8378971d5532acd
jisungbin <ji@sungb.in>
2022-10-23 12:42:10 +0900

    refactor: 암시적 it 제거

--- data/src/main/kotlin/land/sungbin/androidprojecttemplate/data/repository/UpsertRepositoryImpl.kt
+++ data/src/main/kotlin/land/sungbin/androidprojecttemplate/data/repository/UpsertRepositoryImpl.kt
@@ -21,68 +21,68 @@ import land.sungbin.androidprojecttemplate.domain.repository.result.DuckApiResul
 class UpsertRepositoryImpl : UpsertRepository {
     override suspend fun upsertUser(
         user: User,
-    ) = suspendCancellableCoroutine<DuckApiResult<Nothing>> {
+    ) = suspendCancellableCoroutine<DuckApiResult<Nothing>> { continuation ->
 
     }
 
     override suspend fun upsertFollow(
         follow: Follow,
-    ) = suspendCancellableCoroutine<DuckApiResult<Nothing>> {
+    ) = suspendCancellableCoroutine<DuckApiResult<Nothing>> { continuation ->
 
     }
 
     override suspend fun upsertChatRoom(
         chatRoom: ChatRoom,
-    ) = suspendCancellableCoroutine<DuckApiResult<Nothing>> {
+    ) = suspendCancellableCoroutine<DuckApiResult<Nothing>> { continuation ->
 
     }
 
     override suspend fun upsertChat(
         chat: Chat,
-    ) = suspendCancellableCoroutine<DuckApiResult<Nothing>> {
+    ) = suspendCancellableCoroutine<DuckApiResult<Nothing>> { continuation ->
 
     }
 
     override suspend fun upsertFeed(
         feed: Feed,
-    ) = suspendCancellableCoroutine<DuckApiResult<Nothing>> {
+    ) = suspendCancellableCoroutine<DuckApiResult<Nothing>> { continuation ->
 
     }
 
     @Unsupported
     override suspend fun upsertChatRead(
         chatRead: ChatRead,
-    ) = suspendCancellableCoroutine<DuckApiResult<Nothing>> {
+    ) = suspendCancellableCoroutine<DuckApiResult<Nothing>> { continuation ->
 
     }
 
     override suspend fun upsertHeart(
         heart: Heart,
-    ) = suspendCancellableCoroutine<DuckApiResult<Nothing>> {
+    ) = suspendCancellableCoroutine<DuckApiResult<Nothing>> { continuation ->
 
     }
 
     override suspend fun upsertComment(
         comment: Comment,
-    ) = suspendCancellableCoroutine<DuckApiResult<Nothing>> {
+    ) = suspendCancellableCoroutine<DuckApiResult<Nothing>> { continuation ->
 
     }
 
     override suspend fun upsertReview(
         review: DealReview,
-    ) = suspendCancellableCoroutine<DuckApiResult<Nothing>> {
+    ) = suspendCancellableCoroutine<DuckApiResult<Nothing>> { continuation ->
 
     }
 
     override suspend fun upsertSaleRequest(
         saleRequest: SaleRequest,
-    ) = suspendCancellableCoroutine<DuckApiResult<Nothing>> {
+    ) = suspendCancellableCoroutine<DuckApiResult<Nothing>> { continuation ->
 
     }
 
     override suspend fun upsertReport(
         report: Report,
-    ) = suspendCancellableCoroutine<DuckApiResult<Nothing>> {
+    ) = suspendCancellableCoroutine<DuckApiResult<Nothing>> { continuation ->
 
     }
 }
