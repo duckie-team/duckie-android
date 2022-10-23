89020ad74570ee5cc0f420122c491ea242cdcc07
jisungbin <ji@sungb.in>
2022-10-23 17:28:26 +0900

    feat: upsertHeart 에 isDeletion 인자 추가

--- domain/src/main/kotlin/land/sungbin/androidprojecttemplate/domain/usecase/upsert/UpsertHeartUseCase.kt
+++ domain/src/main/kotlin/land/sungbin/androidprojecttemplate/domain/usecase/upsert/UpsertHeartUseCase.kt
@@ -21,24 +21,27 @@ class UpsertHeartUseCase(
      * 기존에 등록된 정보를 업데이트합니다.
      *
      * @param type 좋아요 이벤트를 받은 대상 타입
-     * @param commentId 좋아요 이벤트를 받은 대상 아이디.
+     * @param targetId 좋아요 이벤트를 받은 대상 아이디.
      * [피드 아이디][Feed.id] 혹은 [댓글 아이디][Comment.id]가 될 수 있습니다.
      * @param userId 해당 이벤트를 발생시킨 [유저 아이디][User.nickname]
+     * @param isDeletion 좋아요 삭제 이벤트인지 여부
      *
      * @return Upsert 결과.
      * Upsert 결과는 반환 값이 없으므로 [Nothing] 타입의 [DuckApiResult] 를 을 반환합니다.
      */
     suspend operator fun invoke(
         type: HeartTarget,
-        @PK @FK commentId: String,
+        @PK @FK targetId: String,
         @PK @FK userId: String,
+        isDeletion: Boolean,
     ) = runDuckApiCatching {
         repository.upsertHeart(
             heart = Heart(
                 target = type,
-                targetId = commentId,
+                targetId = targetId,
                 userId = userId,
             ),
+            isDeletion = isDeletion,
         )
     }
 }
