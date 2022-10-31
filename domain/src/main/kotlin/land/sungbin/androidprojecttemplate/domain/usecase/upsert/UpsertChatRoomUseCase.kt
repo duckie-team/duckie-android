53e64d9b61d44389c0308ddd69728bcb306d0aef
EvergreenTree97 <tkdfhr7497@naver.com>
2022-11-01 02:38:00 +0900

    fix: domain FK 어노테이션 오류, 빠진 프로퍼티 추가

--- domain/src/main/kotlin/land/sungbin/androidprojecttemplate/domain/usecase/upsert/UpsertChatRoomUseCase.kt
+++ domain/src/main/kotlin/land/sungbin/androidprojecttemplate/domain/usecase/upsert/UpsertChatRoomUseCase.kt
@@ -38,6 +38,7 @@ class UpsertChatRoomUseCase(
                 ownerId = ownerId,
                 categories = null,
                 tags = null,
+                joinUsers = emptyList(),
             ),
         )
     }
