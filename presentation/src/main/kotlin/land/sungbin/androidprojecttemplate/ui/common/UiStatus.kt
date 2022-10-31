745c2a5a9ac825ab3f449c06103b2889f387c8df
EvergreenTree97 <tkdfhr7497@naver.com>
2022-11-01 02:13:43 +0900

    remove: 파일 임시 제거

--- presentation/src/main/kotlin/land/sungbin/androidprojecttemplate/ui/common/UiStatus.kt
+++
@@ -1,7 +0,0 @@
-package land.sungbin.androidprojecttemplate.ui.common
-
-sealed class UiStatus {
-    object Loading : UiStatus()
-    object Success : UiStatus()
-    data class Failed(val message: String = "") : UiStatus()
-}
--- presentation/src/main/kotlin/land/sungbin/androidprojecttemplate/ui/search/SearchScreen.kt
+++
@@ -1,9 +0,0 @@
-package land.sungbin.androidprojecttemplate.ui.search
-
-import androidx.compose.runtime.Composable
-import team.duckie.quackquack.ui.component.QuackTitle2
-
-@Composable
-internal fun SearchScreen() {
-    QuackTitle2(text = "검색")
-}
