4d5f5a3862f7b5659ead6b3332f3ba2be1a3f0ef
EvergreenTree97 <tkdfhr7497@naver.com>
2022-11-06 22:46:12 +0900

    feat: BaseState, BaseViewModel 생성

---
+++ presentation/src/main/kotlin/land/sungbin/androidprojecttemplate/base/BaseState.kt
@@ -0,0 +1,5 @@
+package land.sungbin.androidprojecttemplate.base
+
+interface ViewState
+
+interface ViewSideEffect
---
+++ presentation/src/main/kotlin/land/sungbin/androidprojecttemplate/base/BaseViewModel.kt
@@ -0,0 +1,29 @@
+package land.sungbin.androidprojecttemplate.base
+
+import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.receiveAsFlow
+
+abstract class BaseViewModel<S : ViewState, A : ViewSideEffect>(
+    initialState: S,
+) {
+    private val _state: MutableStateFlow<S> = MutableStateFlow(initialState)
+    val state = _state.asStateFlow()
+
+    private val currentState: S
+        get() = _state.value
+
+    private val _effect: Channel<A> = Channel()
+    val effect = _effect.receiveAsFlow()
+
+    protected fun updateState(reducer: S.() -> S) {
+        val newState = currentState.reducer()
+        _state.value = newState
+    }
+
+    protected suspend fun sendEffect(effect: () -> A) {
+        _effect.send(effect())
+    }
+}
