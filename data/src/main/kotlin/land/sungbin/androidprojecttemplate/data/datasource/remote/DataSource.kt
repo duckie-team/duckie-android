06395105ce576d0074b034eac284b00131cf6b03
EvergreenTree97 <tkdfhr7497@naver.com>
2022-11-08 20:02:34 +0900

    feat: DataSource 구현 WIP

---
+++ data/src/main/kotlin/land/sungbin/androidprojecttemplate/data/datasource/remote/DataSource.kt
@@ -0,0 +1,27 @@
+package land.sungbin.androidprojecttemplate.data.datasource.remote
+
+import io.ktor.client.HttpClient
+import io.ktor.client.call.body
+import io.ktor.client.request.get
+import io.ktor.client.request.url
+import io.ktor.client.statement.bodyAsText
+import land.sungbin.androidprojecttemplate.data.Constants.BaseUrl
+import land.sungbin.androidprojecttemplate.domain.model.constraint.Category
+import javax.inject.Inject
+
+@Suppress("KDocFields")
+class RemoteDataSource(
+   @Inject private val client: HttpClient
+){
+   suspend fun getCategories() : List<Category>{
+      /*val request = client.get {
+         url(BaseUrl+"/router")
+      }
+      val body: List<Category> = request.body()
+      */
+      return listOf(
+         Category.Celebrity,
+         Category.Game,
+      )
+   }
+}
