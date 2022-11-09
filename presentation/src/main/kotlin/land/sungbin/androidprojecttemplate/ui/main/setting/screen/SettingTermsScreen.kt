259e23a4b809b37f1a4319348f45694145d94ab4
limsaehyun <sh00710000@gmail.com>
2022-11-09 10:29:48 +0900

    feat: SettingTermsScreen

---
+++ presentation/src/main/kotlin/land/sungbin/androidprojecttemplate/ui/main/setting/screen/SettingTermsScreen.kt
@@ -0,0 +1,33 @@
+package land.sungbin.androidprojecttemplate.ui.main.setting.screen
+
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import land.sungbin.androidprojecttemplate.R
+import land.sungbin.androidprojecttemplate.ui.component.BackArrowTopAppBar
+import land.sungbin.androidprojecttemplate.ui.main.setting.component.BaseAppSettingLayout
+import team.duckie.quackquack.ui.component.QuackBody1
+
+@Composable
+fun SettingTermsScreen(
+    onClickBack: () -> Unit,
+) {
+    BaseAppSettingLayout(
+        topAppBar = {
+            BackArrowTopAppBar(
+                text = stringResource(
+                    id = R.string.terms,
+                )
+            ) {
+                onClickBack()
+            }
+        },
+    ) {
+        Spacer(modifier = Modifier.height(16.dp))
+
+        QuackBody1(text = "ㅁㄴㅇㅁㄴㅇㅁㄴㅇㅁ")
+    }
+}
