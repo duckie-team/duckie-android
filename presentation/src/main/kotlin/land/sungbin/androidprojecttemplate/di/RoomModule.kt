c6f4a88181202fcb77ae0dd3f00663af8b5ee6ee
limsaehyun <sh00710000@gmail.com>
2022-11-17 00:31:51 +0900

    feat: RoomModule

---
+++ presentation/src/main/kotlin/land/sungbin/androidprojecttemplate/di/RoomModule.kt
@@ -0,0 +1,28 @@
+package land.sungbin.androidprojecttemplate.di
+
+import android.content.Context
+import androidx.room.Room
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import land.sungbin.androidprojecttemplate.data.datasource.local.room.DuckieDataBase
+import land.sungbin.androidprojecttemplate.data.datasource.local.room.SettingDao
+
+@Module
+@InstallIn(SingletonComponent::class)
+object RoomModule {
+
+    @Provides
+    fun provideDuckieDatabase(
+        @ApplicationContext context: Context,
+    ): DuckieDataBase = Room
+        .databaseBuilder(context, DuckieDataBase::class.java, "DuckieDataBase")
+        .build()
+
+    @Provides
+    fun provideSettingDao(
+        duckieDataBase: DuckieDataBase,
+    ): SettingDao = duckieDataBase.settingDao()
+}
