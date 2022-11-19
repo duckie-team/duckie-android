c469d922600dc02a5cffb0953f6be87aca8c0896
limsaehyun <sh00710000@gmail.com>
2022-11-19 20:31:46 +0900

    feat: synchronized room database

--- presentation/src/main/kotlin/land/sungbin/androidprojecttemplate/di/RoomModule.kt
+++ presentation/src/main/kotlin/land/sungbin/androidprojecttemplate/di/RoomModule.kt
@@ -9,18 +9,23 @@ import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
 import land.sungbin.androidprojecttemplate.data.datasource.local.room.DuckieDataBase
 import land.sungbin.androidprojecttemplate.data.datasource.local.room.SettingDao
+import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
 object RoomModule {
-
+    
     @Provides
     fun provideDuckieDatabase(
         @ApplicationContext context: Context,
-    ): DuckieDataBase = Room
-        .databaseBuilder(context, DuckieDataBase::class.java, "DuckieDataBase")
-        .fallbackToDestructiveMigration()
-        .build()
+    ): DuckieDataBase = synchronized(
+        lock = this,
+    ) {
+        Room
+            .databaseBuilder(context, DuckieDataBase::class.java, "DuckieDataBase")
+            .fallbackToDestructiveMigration()
+            .build()
+    }
 
     @Provides
     fun provideSettingDao(
