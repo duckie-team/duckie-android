0a28cece4d886590912f4cdbcd5531cdf8ced6fc
jisungbin <ji@sungb.in>
2022-11-21 00:28:23 +0900

    feat: build-logic 에 저작권 헤더 추가

--- build-logic/src/main/kotlin/team/duckie/app/android/convention/ApplicationConstants.kt
+++ build-logic/src/main/kotlin/team/duckie/app/android/convention/ApplicationConstants.kt
@@ -1,3 +1,10 @@
+/*
+ * Designed and developed by Duckie Team, 2022
+ *
+ * Licensed under the MIT.
+ * Please see full license: https://github.com/duckie-team/duckie-android/blob/develop/LICENSE
+ */
+
 package team.duckie.app.android.convention
 
 import org.gradle.api.JavaVersion
