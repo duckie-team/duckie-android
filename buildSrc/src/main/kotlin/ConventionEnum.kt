59d40bcd067829e40bc9710387e018d9c73a666d
jisungbin <ji@sungb.in>
2022-11-21 00:28:55 +0900

    feat: buildSrc 에 저작권 헤더 추가

--- buildSrc/src/main/kotlin/ConventionEnum.kt
+++ buildSrc/src/main/kotlin/ConventionEnum.kt
@@ -2,7 +2,7 @@
  * Designed and developed by Duckie Team, 2022
  *
  * Licensed under the MIT.
- * Please see full license: https://github.com/duckie-team/quack-quack-android/blob/master/LICENSE
+ * Please see full license: https://github.com/duckie-team/duckie-android/blob/develop/LICENSE
  */
 
 object ConventionEnum {
