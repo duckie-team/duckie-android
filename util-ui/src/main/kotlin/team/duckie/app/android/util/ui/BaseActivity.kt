76aace8adec3b6e858f032286c3d943df302c231
jisungbin <ji@sungb.in>
2022-11-21 18:50:50 +0900

    feat: 본격적인 개발을 위한 유틸 모듈들 추가

---
+++ util-ui/src/main/kotlin/team/duckie/app/android/util/ui/BaseActivity.kt
@@ -0,0 +1,19 @@
+/*
+ * Designed and developed by Duckie Team, 2022
+ *
+ * Licensed under the MIT.
+ * Please see full license: https://github.com/duckie-team/duckie-android/blob/develop/LICENSE
+ */
+
+package team.duckie.app.android.util.ui
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.core.view.WindowCompat
+
+open class BaseActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        WindowCompat.setDecorFitsSystemWindows(window, false)
+        super.onCreate(savedInstanceState)
+    }
+}
