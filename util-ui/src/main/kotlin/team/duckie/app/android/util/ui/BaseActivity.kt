666d81f690b36d28716cce38719c85f41d4ae542
Ji Sungbin <ji@sungb.in>
2022-11-22 00:01:18 +0900

    feat: IntroActivity 구현 (#81)
    * chore: largeHeap 활성화
    
    * feat: IntroActivity 구현
    
    * feat: 문자열 리소스 추가
    
    * feat: 더키 인트로 이미지 추가
    
    * feat: ui 유틸 추가
    
    * chore: 중복되는 의존성 제거
    
    * chore: ui-util 모듈에 AndroidLibraryCompose 플러그인 추가
    
    * feat: BaseActivity 구현 개선

--- util-ui/src/main/kotlin/team/duckie/app/android/util/ui/BaseActivity.kt
+++ util-ui/src/main/kotlin/team/duckie/app/android/util/ui/BaseActivity.kt
@@ -9,11 +9,42 @@ package team.duckie.app.android.util.ui
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
+import androidx.core.content.ContextCompat
 import androidx.core.view.WindowCompat
+import land.sungbin.systemuicontroller.SystemUiController
 
-open class BaseActivity : ComponentActivity() {
+/**
+ * 모든 액티비티의 기본이 되는 Base Activity 를 구현합니다.
+ * [BaseActivity] 는 크게 다음과 같은 일을 진행합니다.
+ *
+ * 1. WindowInsets 의 consume 비활성화
+ * 2. SystemBars 의 색상을 transparent 로 변경
+ *
+ * @param useDarkStatusBarIcons 상태바에 다크모드 아이콘을 사용할지 여부
+ * @param useDarkNavigationBarIcons 네비게이션바에 다크모드 아이콘을 사용할지 여부
+ */
+open class BaseActivity(
+    private val useDarkStatusBarIcons: Boolean = true,
+    private val useDarkNavigationBarIcons: Boolean = true,
+) : ComponentActivity() {
+
+    private val systemUiController by lazy {
+        SystemUiController(window)
+    }
+    private val transparentColor by lazy {
+        ContextCompat.getColor(this, android.R.color.transparent)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         WindowCompat.setDecorFitsSystemWindows(window, false)
+        systemUiController.setStatusBarColor(
+            color = transparentColor,
+            darkIcons = useDarkStatusBarIcons,
+        )
+        systemUiController.setNavigationBarColor(
+            color = transparentColor,
+            darkIcons = useDarkNavigationBarIcons,
+        )
         super.onCreate(savedInstanceState)
     }
 }
---
+++ util-ui/src/main/kotlin/team/duckie/app/android/util/ui/WindowInsets.kt
@@ -0,0 +1,20 @@
+/*
+ * Designed and developed by 2022 Ji Sungbin.
+ *
+ * Licensed under the MIT.
+ * Please see full license: https://github.com/duckie-team/ApiLibrary/blob/trunk/LICENSE
+ */
+
+package team.duckie.app.android.util.ui
+
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.asPaddingValues
+import androidx.compose.foundation.layout.systemBars
+import androidx.compose.runtime.Composable
+
+/**
+ * SystemBars 에 해당하는 WindowInsets 을 [PaddingValues] 로 가져옵니다.
+ */
+val systemBarPaddings
+    @Composable get() = WindowInsets.systemBars.asPaddingValues()
