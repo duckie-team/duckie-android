1122d003cd71135c6429a1df174b14784a73dd8e
jisungbin <ji@sungb.in>
2022-11-26 13:37:58 +0900

    feat: 버튼 중복 클릭 예방 로직 추가

--- feature-ui-onboard/src/main/kotlin/team/duckie/app/android/feature/ui/onboard/viewmodel/OnboardViewModel.kt
+++ feature-ui-onboard/src/main/kotlin/team/duckie/app/android/feature/ui/onboard/viewmodel/OnboardViewModel.kt
@@ -14,6 +14,7 @@ import team.duckie.app.android.util.viewmodel.BaseViewModel
 internal class OnboardViewModel : BaseViewModel<Unit, Unit>(Unit) {
     private val nicknameFilter = Regex("[^가-힣a-zA-Z0-9_.]")
     private val mutableStep = MutableStateFlow(OnboardStep.Login)
+    private var lastestUpdateStepMillis = System.currentTimeMillis()
 
     /**
      * 현재 온보딩이 어느 단계에 있는지 나타냅니다.
@@ -28,10 +29,16 @@ internal class OnboardViewModel : BaseViewModel<Unit, Unit>(Unit) {
 
     /**
      * 온보딩 단계를 업데이트합니다.
+     * 다음 단계로 넘어가기 전에 발생하는 중복 업데이트를 방지하기 위해
+     * 1000 ms 이내에는 업데이트가 되지 않습니다.
      *
      * @param step 새로운 온보딩 단계
      */
     fun updateStep(step: OnboardStep) {
+        if (System.currentTimeMillis() - lastestUpdateStepMillis < 1000) {
+            return
+        }
+        lastestUpdateStepMillis = System.currentTimeMillis()
         mutableStep.value = step
     }
 
