cd9be4e2419b2f69f339f6982824e6176957ed3e
jisungbin <ji@sungb.in>
2022-11-28 06:10:03 +0900

    refactor: 코드 정리

--- build-logic/src/main/kotlin/team/duckie/app/android/convention/ApplicationConstants.kt
+++ build-logic/src/main/kotlin/team/duckie/app/android/convention/ApplicationConstants.kt
@@ -16,7 +16,6 @@ internal object ApplicationConstants {
     const val minSdk = 23
     const val targetSdk = 33
     const val compileSdk = 33
-    const val jvmTarget = "11"
     const val versionCode = 1
     const val versionName = "1"
     val javaVersion = JavaVersion.VERSION_11
