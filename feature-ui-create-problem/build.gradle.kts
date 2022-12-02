1b3a7596615fa035f9176a522a7be87afebaf042
Choi Sang Rok <tkdfhr7497@naver.com>
2022-12-02 19:25:54 +0900

    ui feature 모듈 생성 (#103)
    * chore: setiing.gradle에 모듈 의존성 추가
    
    * feat: 문제 풀기 피쳐 모듈
    
    * feat: 상세 페이지 피쳐 모듈
    
    * feat: 홈 피쳐 모듈
    
    * feat: 문제 풀기 피쳐 모듈
    
    * chore: namespace 이름 변경
    
    * chore: menifest 정렬
    
    * refactor: kotlin패키지 변경
    
    * remove: 사용하지 않는 테스트 폴더 제거
    
    * chore: 중복 모듈 명시 제거
    
    * chore: 사용하지 않는 의존성 제거
    
    * feat: 패키지 변경하고 더미파일 생성
    
    * refactor: solving -> solve로 패키지 명 변경

---
+++ feature-ui-create-problem/build.gradle.kts
@@ -0,0 +1,19 @@
+/*
+ * Designed and developed by Duckie Team, 2022
+ *
+ * Licensed under the MIT.
+ * Please see full license: https://github.com/duckie-team/duckie-android/blob/develop/LICENSE
+ */
+
+plugins {
+    id(ConventionEnum.AndroidLibrary)
+    id(ConventionEnum.AndroidLibraryCompose)
+    id(ConventionEnum.AndroidLibraryComposeUiTest)
+    id(ConventionEnum.AndroidHilt)
+    id(ConventionEnum.JvmJUnit4)
+    id(ConventionEnum.JvmDokka)
+}
+
+android {
+    namespace = "team.duckie.app.android.feature.ui.create.problem"
+}
