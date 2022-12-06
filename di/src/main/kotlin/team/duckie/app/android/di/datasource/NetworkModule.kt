04ca0a8f473ff8ae11e961ef72d263f5d338400f
EvergreenTree97 <tkdfhr7497@naver.com>
2022-12-06 18:27:47 +0900

    feat: network di 정의

---
+++ di/src/main/kotlin/team/duckie/app/android/di/datasource/NetworkModule.kt
@@ -0,0 +1,57 @@
+/*
+ * Designed and developed by Duckie Team, 2022
+ *
+ * Licensed under the MIT.
+ * Please see full license: https://github.com/duckie-team/duckie-android/blob/develop/LICENSE
+ */
+
+package team.duckie.app.android.di.datasource
+
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import io.ktor.client.HttpClient
+import io.ktor.client.engine.cio.CIO
+import io.ktor.client.engine.cio.endpoint
+import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
+import io.ktor.client.plugins.defaultRequest
+import io.ktor.client.plugins.logging.ANDROID
+import io.ktor.client.plugins.logging.LogLevel
+import io.ktor.client.plugins.logging.Logger
+import io.ktor.client.plugins.logging.Logging
+import io.ktor.serialization.jackson.jackson
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+internal class NetworkModule {
+
+    @Provides
+    @Singleton
+    fun provideKtorClient(): HttpClient = HttpClient(CIO) {
+        expectSuccess = true
+        engine {
+            endpoint {
+                connectTimeout = MaxTimeoutMillis
+                connectAttempts = MaxRetryCount
+            }
+        }
+        defaultRequest {
+            url(BaseUrl)
+        }
+        install(plugin = ContentNegotiation) {
+            jackson()
+        }
+        install(plugin = Logging) {
+            logger = Logger.ANDROID
+            level = LogLevel.ALL
+        }
+    }
+
+    private companion object {
+        const val MaxTimeoutMillis = 3000L
+        const val MaxRetryCount = 3
+        const val BaseUrl = "https://api.publicapis.org/entries"
+    }
+}
