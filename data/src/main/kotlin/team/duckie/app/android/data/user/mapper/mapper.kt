cb8b364adca2034ccb0d34d42ab3bbd93f7d9d7a
EvergreenTree97 <tkdfhr7497@naver.com>
2022-12-06 23:00:20 +0900

    feat: mapper 작성

---
+++ data/src/main/kotlin/team/duckie/app/android/data/user/mapper/mapper.kt
@@ -0,0 +1,30 @@
+/*
+ * Designed and developed by Duckie Team, 2022
+ *
+ * Licensed under the MIT.
+ * Please see full license: https://github.com/duckie-team/duckie-android/blob/develop/LICENSE
+ */
+
+/*
+ * Designed and developed by Duckie Team, 2022
+ *
+ * Licensed under the MIT.
+ * Please see full license: https://github.com/duckie-team/duckie-android/blob/develop/LICENSE
+ */
+
+package team.duckie.app.android.data.user.mapper
+
+import team.duckie.app.android.data.user.model.UserData
+import team.duckie.app.android.domain.user.model.User
+
+fun UserData.toDomain() = User(
+    id = id,
+    nickName = nickName,
+    accountEnabled = accountEnabled,
+    profileUrl = profileUrl,
+    tier = tier,
+    createdAt = createdAt,
+    updatedAt = updatedAt,
+    deletedAt = deletedAt,
+    bannedAt = bannedAt
+)
