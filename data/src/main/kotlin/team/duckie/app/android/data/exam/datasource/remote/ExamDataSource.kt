fc01d6c6180394c9ec8cf5594ca43be6d8607d59
EvergreenTree97 <tkdfhr7497@naver.com>
2022-12-06 22:59:45 +0900

    feat: DataSource dummy 데이터 제공

--- data/src/main/kotlin/team/duckie/app/android/data/exam/datasource/remote/ExamDataSource.kt
+++ data/src/main/kotlin/team/duckie/app/android/data/exam/datasource/remote/ExamDataSource.kt
@@ -8,19 +8,63 @@
 package team.duckie.app.android.data.exam.datasource.remote
 
 import io.ktor.client.HttpClient
+import io.ktor.client.call.body
 import io.ktor.client.request.header
 import io.ktor.client.request.post
+import io.ktor.client.request.request
 import io.ktor.client.request.setBody
+import io.ktor.client.request.url
+import team.duckie.app.android.data.exam.mapper.toDomain
+import team.duckie.app.android.data.exam.model.ExamRequest
+import team.duckie.app.android.data.exam.model.ExamResponse
+import team.duckie.app.android.data.exam.model.TagData
+import team.duckie.app.android.data.user.model.UserData
+import java.util.Date
 import javax.inject.Inject
 
 class ExamDataSource @Inject constructor(
     private val client: HttpClient
 ) {
-
-    suspend fun postExams(){
-        val request = client.post {
-            setBody()
-            header("authorization", "accessToken")
-        }
+    suspend fun postExams(examRequest: ExamRequest): ExamResponse {
+        val request = client
+            .post {
+                url("/exams")
+                setBody(examRequest)
+                header("authorization", "AT") //TODO [Evergreen] access token 자동화 방안 마련 필요
+            }
+        val body: ExamResponse = request.body()
+        //return body
+        return dummyResponse
     }
 }
+
+val dummyResponse = ExamResponse(
+    title = "Test Title 2",
+    description = "Test Description 2",
+    thumbnailUrl = "Test Image Url",
+    certifyingStatement = "Test 필적 확인 문구1",
+    buttonTitle = "Test Button Title 1",
+    isPublic = true,
+    tags = listOf(
+        TagData(0, "tag1"),
+        TagData(1, "tag2")
+    ),
+    UserData(
+        id = 1,
+        nickName = "goddoro",
+        accountEnabled = true,
+        profileUrl = "asdf",
+        tier = 1,
+        createdAt = Date(),
+        updatedAt = Date(),
+        deletedAt = null,
+        bannedAt = null,
+    ),
+    deletedAt = null,
+    id = 11,
+    createdAt = Date(),
+    updatedAt = Date(),
+    solvedCount = 0,
+    answerRate = 0.0f,
+    canRetry = true
+)
