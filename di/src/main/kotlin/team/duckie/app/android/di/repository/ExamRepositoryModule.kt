c63029f316a0822a79cb777a77d86bec76086448
EvergreenTree97 <tkdfhr7497@naver.com>
2022-12-08 00:30:36 +0900

    refactor: file supress newline

--- di/src/main/kotlin/team/duckie/app/android/di/repository/ExamRepositoryModule.kt
+++ di/src/main/kotlin/team/duckie/app/android/di/repository/ExamRepositoryModule.kt
@@ -4,6 +4,7 @@
  * Licensed under the MIT.
  * Please see full license: https://github.com/duckie-team/duckie-android/blob/develop/LICENSE
  */
+
 @file:Suppress("UnnecessaryAbstractClass")
 
 package team.duckie.app.android.di.repository
