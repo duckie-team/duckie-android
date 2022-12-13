db0963a234abc42db9f7240225d63e41963bc122
EvergreenTree97 <tkdfhr7497@naver.com>
2022-12-14 01:39:03 +0900

    feat: ViewModel에 비즈니스 로직 작성

--- feature-ui-create-problem/src/main/kotlin/team/duckie/app/android/feature/ui/create/problem/viewmodel/CreateProblemViewModel.kt
+++ feature-ui-create-problem/src/main/kotlin/team/duckie/app/android/feature/ui/create/problem/viewmodel/CreateProblemViewModel.kt
@@ -9,58 +9,83 @@ package team.duckie.app.android.feature.ui.create.problem.viewmodel
 
 import android.util.Log
 import kotlinx.collections.immutable.persistentListOf
+import kotlinx.collections.immutable.toImmutableList
 import team.duckie.app.android.domain.exam.model.Answer
 import team.duckie.app.android.domain.exam.model.ExamParam
 import team.duckie.app.android.domain.exam.model.ProblemItem
 import team.duckie.app.android.domain.exam.model.Question
 import team.duckie.app.android.domain.exam.model.Tag
 import team.duckie.app.android.domain.exam.usecase.MakeExamUseCase
+import team.duckie.app.android.feature.ui.create.problem.viewmodel.sideeffect.CreateProblemSideEffect
+import team.duckie.app.android.feature.ui.create.problem.viewmodel.state.CreateProblemState
+import team.duckie.app.android.util.kotlin.copy
+import team.duckie.app.android.util.viewmodel.BaseViewModel
 import javax.inject.Inject
 import javax.inject.Singleton
 
 @Singleton
 class CreateProblemViewModel @Inject constructor(
     private val makeExamUseCase: MakeExamUseCase
-) {
+) : BaseViewModel<CreateProblemState, CreateProblemSideEffect>(CreateProblemState()) {
     private val TAG = CreateProblemViewModel::class.simpleName
 
-    suspend fun makeExam() {
+    /*suspend fun makeExam() {
         makeExamUseCase(dummyParam).onSuccess { exam ->
             Log.d(TAG, exam.toString() + "성공")
         }
+    }*/
+
+    fun onClickCategory(
+        index: Int,
+    ) {
+        updateState { prevState ->
+            CreateProblemState(
+                examInformation = CreateProblemState.ExamInformation(
+                    categoriesSelection = prevState.examInformation.categoriesSelection.copy {
+                        this[index] = !this[index]
+                    }.toImmutableList()
+                ),
+            )
+        }
+    }
+
+    fun setExamArea(examArea: String) {
+        updateState {
+            CreateProblemState(
+                examInformation = CreateProblemState.ExamInformation(
+                    examArea = examArea,
+                ),
+            )
+        }
+    }
+
+    fun setExamTitle(examTitle: String) {
+        updateState {
+            CreateProblemState(
+                examInformation = CreateProblemState.ExamInformation(
+                    examArea = examTitle,
+                ),
+            )
+        }
+    }
+
+    fun setExamDescription(examDescription: String) {
+        updateState {
+            CreateProblemState(
+                examInformation = CreateProblemState.ExamInformation(
+                    examDescription = examDescription,
+                ),
+            )
+        }
+    }
+
+    fun setCertifyingStatement(certifyingStatement: String){
+        updateState {
+            CreateProblemState(
+                examInformation = CreateProblemState.ExamInformation(
+                    certifyingStatement = certifyingStatement,
+                ),
+            )
+        }
     }
 }
-
-private val dummyParam = ExamParam(
-    title = "Test Title 2",
-    description = "Test Description 2",
-    mainTag = Tag(
-        id = 1,
-        "1"
-    ),
-    subTag = persistentListOf(
-        Tag(
-            2,
-            "2",
-        )
-    ),
-    thumbnailImageUrl = "Test Image Url",
-    userId = 1,
-    certifyingStatement = "Test 필적 확인 문구1",
-    thumbnailType = "image",
-    buttonTitle = "Test Button Title 1",
-    isPublic = true,
-    problems = ProblemItem(
-        questionObject = Question.Text(
-            text = "",
-            type = "",
-        ),
-        answerObject = Answer.ShortAnswer(
-            shortAnswer = "바보",
-            type = "",
-        ),
-        memo = "test memo 1",
-        hint = "test hint 1",
-        correctAnswer = "3",
-    )
-)
