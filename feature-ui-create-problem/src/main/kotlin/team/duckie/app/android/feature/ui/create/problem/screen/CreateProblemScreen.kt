31fbfd8d714378d3fa86b1309d27b4154932f5f9
EvergreenTree97 <tkdfhr7497@naver.com>
2022-12-14 01:39:24 +0900

    feat: Screen에 viewModel 상태 뿌리기

--- feature-ui-create-problem/src/main/kotlin/team/duckie/app/android/feature/ui/create/problem/screen/CreateProblemScreen.kt
+++ feature-ui-create-problem/src/main/kotlin/team/duckie/app/android/feature/ui/create/problem/screen/CreateProblemScreen.kt
@@ -5,54 +5,40 @@
  * Please see full license: https://github.com/duckie-team/duckie-android/blob/develop/LICENSE
  */
 
+@file:OptIn(ExperimentalLifecycleComposeApi::class)
+
 package team.duckie.app.android.feature.ui.create.problem.screen
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.Scaffold
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.Stable
-import androidx.compose.runtime.mutableStateListOf
-import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.DpSize
 import androidx.compose.ui.unit.dp
-import kotlinx.collections.immutable.persistentListOf
+import androidx.lifecycle.compose.ExperimentalLifecycleComposeApi
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import team.duckie.app.android.feature.ui.create.problem.common.CreateProblemTopAppBar
 import team.duckie.app.android.feature.ui.create.problem.common.TitleAndComponent
 import team.duckie.app.android.feature.ui.create.problem.viewmodel.CreateProblemViewModel
+import team.duckie.app.android.util.compose.LocalViewModel
 import team.duckie.app.android.util.compose.component.DuckieGridLayout
 import team.duckie.quackquack.ui.color.QuackColor
-import team.duckie.quackquack.ui.component.QuackBasic2TextField
 import team.duckie.quackquack.ui.component.QuackBasicTextArea
 import team.duckie.quackquack.ui.component.QuackBasicTextField
 import team.duckie.quackquack.ui.component.QuackMediumToggleButton
+import team.duckie.quackquack.ui.component.QuackReviewTextArea
 import team.duckie.quackquack.ui.icon.QuackIcon
 
-private val categories = persistentListOf("연예인", "영화", "만화/애니", "웹툰", "게임", "밀리터리")
-
-@Stable
-private val CategoryButtonSize = DpSize(
-    width = 102.dp,
-    height = 40.dp,
-)
-
 @Composable
-internal fun CreateProblemScreen(
-    viewModel: CreateProblemViewModel,
-) {
-    val categoriesSelectedIndex = remember {
-        mutableStateListOf(
-            elements = Array(
-                size = categories.size,
-                init = { false },
-            )
-        )
-    }
+internal fun CreateProblemScreen() {
+    val viewModel = LocalViewModel.current as CreateProblemViewModel
+    val state = viewModel.state.collectAsStateWithLifecycle().value.examInformation
+
     Scaffold(
         modifier = Modifier
             .statusBarsPadding()
@@ -71,16 +57,17 @@ internal fun CreateProblemScreen(
             verticalArrangement = Arrangement.spacedBy(space = 48.dp),
         ) {
             TitleAndComponent(title = "카테고리") {
-                DuckieGridLayout(items = categories) { index, item ->
+                DuckieGridLayout(items = state.categories) { index, item ->
+                    //TODO [EvergreenTree97] 컴포넌트 필요
                     QuackMediumToggleButton(
                         modifier = Modifier.size(
                             width = 102.dp,
                             height = 40.dp,
                         ),
                         text = item,
-                        selected = categoriesSelectedIndex[index],
+                        selected = state.categoriesSelection[index],
                         onClick = {
-                            categoriesSelectedIndex[index] = !categoriesSelectedIndex[index]
+                            viewModel.onClickCategory(index)
                         },
                     )
                 }
@@ -88,31 +75,34 @@ internal fun CreateProblemScreen(
             TitleAndComponent(title = "시험 영역") {
                 QuackBasicTextField(
                     leadingIcon = QuackIcon.Search,
-                    text = "",
-                    onTextChanged = { text -> text },
+                    text = state.examArea,
+                    onTextChanged = viewModel::setExamArea,
                     placeholderText = "시험 영역 찾기",
                 )
             }
 
             TitleAndComponent(title = "시험 제목") {
                 QuackBasicTextField(
-                    text = "",
-                    onTextChanged = { text -> text },
+                    text = state.examTitle,
+                    onTextChanged = viewModel::setExamTitle,
                     placeholderText = "시험 제목을 입력해 주세요",
                 )
             }
 
             TitleAndComponent(title = "설명") {
-                QuackBasicTextArea(
-                    text = "",
-                    onTextChanged = {},
+                QuackReviewTextArea(
+                    modifier = Modifier.heightIn(140.dp),
+                    text = state.examDescription,
+                    onTextChanged = viewModel::setExamDescription,
                     placeholderText = "시험에 대한 설명을 입력해 주세요",
                 )
             }
             TitleAndComponent(title = "필적 확인 문구") {
-                QuackBasic2TextField(
-                    text = "",
-                    onTextChanged = {},
+                //TODO [EvergreenTree97] 컴포넌트 필요
+                QuackBasicTextArea(
+                    text = state.certifyingStatement,
+                    onTextChanged = viewModel::setCertifyingStatement,
+                    placeholderText = "필적확인 문구를 입력해 주세요"
                 )
             }
         }
