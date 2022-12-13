7426812c2a98964f9428f824b35604234e97b88e
EvergreenTree97 <tkdfhr7497@naver.com>
2022-12-14 01:38:48 +0900

    feat: SideEffect 클래스 작성

---
+++ feature-ui-create-problem/src/main/kotlin/team/duckie/app/android/feature/ui/create/problem/viewmodel/sideeffect/CreateProblemSideEffect.kt
@@ -0,0 +1,12 @@
+/*
+ * Designed and developed by Duckie Team, 2022
+ *
+ * Licensed under the MIT.
+ * Please see full license: https://github.com/duckie-team/duckie-android/blob/develop/LICENSE
+ */
+
+package team.duckie.app.android.feature.ui.create.problem.viewmodel.sideeffect
+
+sealed class CreateProblemSideEffect{
+
+}
