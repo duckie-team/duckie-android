d6f19b5f9d2862dc09f970f5fc62000f0f10c704
limsaehyun <sh00710000@gmail.com>
2022-12-14 23:32:57 +0900

    chore: home dependencies 설정

--- feature-ui-home/build.gradle.kts
+++ feature-ui-home/build.gradle.kts
@@ -5,6 +5,8 @@
  * Please see full license: https://github.com/duckie-team/duckie-android/blob/develop/LICENSE
  */
 
+import DependencyHandler.Extensions.implementations
+
 plugins {
     id(ConventionEnum.AndroidLibrary)
     id(ConventionEnum.AndroidLibraryCompose)
@@ -16,4 +18,21 @@ plugins {
 
 android {
     namespace = "team.duckie.app.android.feature.ui.home"
+}
+
+dependencies {
+    implementations(
+        projects.di,
+        projects.domain,
+        projects.utilUi,
+        projects.utilKotlin,
+        projects.utilCompose,
+        projects.utilViewmodel,
+        libs.ktx.lifecycle,
+        libs.compose.ktx.lifecycle,
+        libs.quack.ui.components,
+        libs.compose.coil,
+        libs.ui.pager,
+        libs.ui.pager.indicators,
+    )
 }
