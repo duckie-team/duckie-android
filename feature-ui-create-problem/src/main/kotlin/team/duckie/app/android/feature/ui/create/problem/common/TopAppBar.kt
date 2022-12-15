d8e4b0566092244ac642b0a957df294810abaea1
EvergreenTree97 <tkdfhr7497@naver.com>
2022-12-15 15:31:05 +0900

    feat: 종료 버튼 있는 topappbar 구현

--- feature-ui-create-problem/src/main/kotlin/team/duckie/app/android/feature/ui/create/problem/common/TopAppBar.kt
+++ feature-ui-create-problem/src/main/kotlin/team/duckie/app/android/feature/ui/create/problem/common/TopAppBar.kt
@@ -7,9 +7,18 @@
 
 package team.duckie.app.android.feature.ui.create.problem.common
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
 import team.duckie.app.android.feature.ui.create.problem.R
+import team.duckie.quackquack.ui.component.QuackHeadLine2
+import team.duckie.quackquack.ui.component.QuackImage
 import team.duckie.quackquack.ui.component.QuackTopAppBar
 import team.duckie.quackquack.ui.icon.QuackIcon
 
@@ -32,3 +41,24 @@ internal fun PrevAndNextTopAppBar(
     )
 }
 
+//TODO [EvergreenTree] QuackTopAppBar에 leadingIcon이 nullable로 변경됨이 필요
+@Composable
+internal fun ExitAppBar(
+    modifier: Modifier = Modifier,
+    leadingText: String,
+    onTrailingIconClick: () -> Unit,
+) {
+    Row(
+        modifier = modifier
+            .fillMaxWidth()
+            .padding(vertical = 12.dp, horizontal = 16.dp),
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.SpaceBetween,
+    ) {
+        QuackHeadLine2(text = leadingText)
+        QuackImage(
+            src = QuackIcon.Close,
+            onClick = onTrailingIconClick,
+        )
+    }
+}
