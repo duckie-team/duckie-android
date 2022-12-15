7695e863b57cc0753b3f2c7bb96be9b5b0ce87b4
EvergreenTree97 <tkdfhr7497@naver.com>
2022-12-15 15:31:21 +0900

    feat: activity에서 navigate가 가능하도록 변경

--- feature-ui-create-problem/src/main/kotlin/team/duckie/app/android/feature/ui/create/problem/CreateProblemActivity.kt
+++ feature-ui-create-problem/src/main/kotlin/team/duckie/app/android/feature/ui/create/problem/CreateProblemActivity.kt
@@ -5,16 +5,27 @@
  * Please see full license: https://github.com/duckie-team/duckie-android/blob/develop/LICENSE
  */
 
+@file:OptIn(ExperimentalLifecycleComposeApi::class)
+
 package team.duckie.app.android.feature.ui.create.problem
 
 import android.os.Bundle
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.ui.Modifier
+import androidx.lifecycle.compose.ExperimentalLifecycleComposeApi
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import dagger.hilt.android.AndroidEntryPoint
-import team.duckie.app.android.feature.ui.create.problem.screen.CreateProblemScreen
+import team.duckie.app.android.feature.ui.create.problem.screen.ExamInformationScreen
+import team.duckie.app.android.feature.ui.create.problem.screen.FindExamAreaScreen
 import team.duckie.app.android.feature.ui.create.problem.viewmodel.CreateProblemViewModel
+import team.duckie.app.android.feature.ui.create.problem.viewmodel.state.CreateProblemStep
 import team.duckie.app.android.util.compose.LocalViewModel
 import team.duckie.app.android.util.ui.BaseActivity
+import team.duckie.quackquack.ui.animation.QuackAnimatedContent
+import team.duckie.quackquack.ui.color.QuackColor
 import javax.inject.Inject
 
 @AndroidEntryPoint
@@ -27,7 +38,21 @@ class CreateProblemActivity : BaseActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             CompositionLocalProvider(LocalViewModel provides viewModel) {
-                CreateProblemScreen()
+                val createProblemStep = viewModel.state.collectAsStateWithLifecycle().value.createProblemStep
+
+                QuackAnimatedContent(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .background(color = QuackColor.White.composeColor),
+                    targetState = createProblemStep,
+                ) { step: CreateProblemStep ->
+                    when (step) {
+                        CreateProblemStep.ExamInformation -> ExamInformationScreen()
+                        CreateProblemStep.FindExamArea -> FindExamAreaScreen()
+                        CreateProblemStep.CreateProblem -> {}
+                        CreateProblemStep.AdditionalInformation -> {}
+                    }
+                }
             }
         }
     }
