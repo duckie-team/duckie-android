792ea083a24b70dc7bc20857b36f0596a71976b5
EvergreenTree97 <tkdfhr7497@naver.com>
2022-12-17 17:02:27 +0900

    refactor: onNext에서 stable하지 않을 수 있음

--- feature-ui-create-problem/src/main/kotlin/team/duckie/app/android/feature/ui/create/problem/common/TextfieldOptions.kt
+++ feature-ui-create-problem/src/main/kotlin/team/duckie/app/android/feature/ui/create/problem/common/TextfieldOptions.kt
@@ -17,7 +17,6 @@ import androidx.compose.ui.text.input.ImeAction
 @Stable
 internal val ImeActionNext = KeyboardOptions(imeAction = ImeAction.Next)
 
-@Stable
 internal fun moveDownFocus(
     focusManager: FocusManager,
 ) = KeyboardActions(
