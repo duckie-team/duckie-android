e16369f12465700afd2d6e8f2dee45a3c5171462
ricky_0_k <ridickle7@gmail.com>
2022-12-18 16:32:17 +0900

    feat: Composable 분리 (이제 UI 구조가 확정되었으므로 분리함)

--- feature-ui-detail/src/main/kotlin/team/duckie/app/android/feature/ui/detail/screen/DetailScreen.kt
+++ feature-ui-detail/src/main/kotlin/team/duckie/app/android/feature/ui/detail/screen/DetailScreen.kt
@@ -24,8 +24,10 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.layout.Layout
 import androidx.compose.ui.layout.layoutId
+import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.DpSize
@@ -82,7 +84,7 @@ internal fun DetailScreen(modifier: Modifier = Modifier) = CoroutineScopeContent
     Layout(
         modifier = modifier,
         content = {
-            // 상단 탭바
+            // 상단 탭바 Layout
             // TODO(riflockle7): trailingIcon 의 경우, 추후 커스텀 composable 을 넣을 수 있게 하면 어떨지...?
             QuackTopAppBar(
                 modifier = Modifier.layoutId(DetailScreenTopAppBarLayoutId),
@@ -91,139 +93,9 @@ internal fun DetailScreen(modifier: Modifier = Modifier) = CoroutineScopeContent
                 onLeadingIconClick = { activity.finish() },
                 onTrailingIconClick = {},
             )
-            Column(
-                modifier = Modifier
-                    .layoutId(DetailScreenContentLayoutId)
-                    .fillMaxWidth()
-                    .verticalScroll(rememberScrollState()),
-            ) {
-                // 그림
-                QuackImage(
-                    modifier = Modifier.padding(
-                        top = 16.dp,
-                        start = 16.dp,
-                        end = 16.dp
-                    ),
-                    src = R.drawable.img_duckie_detail_image_dummy,
-                    size = DpSize(328.dp, 240.dp)
-                )
-                // 공백
-                Spacer(modifier = Modifier.height(12.dp))
-                // 제목
-                QuackHeadLine2(
-                    modifier = Modifier.padding(horizontal = 16.dp),
-                    text = "제 1회 도로 패션영역"
-                )
-                // 공백
-                Spacer(modifier = Modifier.height(8.dp))
-                // 내용
-                QuackBody2(
-                    modifier = Modifier.padding(horizontal = 16.dp),
-                    text = "아 저 근데 너무 재밌을거 같아요 내 시험 최고"
-                )
-                // 공백
-                Spacer(modifier = Modifier.height(12.dp))
-                QuackSingeLazyRowTag(
-                    contentPadding = PaddingValues(horizontal = 16.dp, vertical = 4.dp),
-                    horizontalSpace = 4.dp,
-                    items = persistentListOf(
-                        "도로", "패션", "도로패션", "도로로", "Doro Driven Design"
-                    ),
-                    tagType = QuackTagType.Grayscale(""),
-                    onClick = {},
-                )
-                // 태그 목록
-                // 공백
-                Spacer(modifier = Modifier.height(24.dp))
-                // 구분선
-                QuackDivider()
-                // 프로필 Layout
-                Row(
-                    modifier = Modifier.padding(
-                        horizontal = 16.dp,
-                        vertical = 12.dp,
-                    ),
-                    verticalAlignment = Alignment.CenterVertically
-                ) {
-                    // 작성자 프로필 이미지
-                    QuackImage(
-                        src = team.duckie.quackquack.ui.R.drawable.quack_ic_profile_24,
-                        size = DpSize(32.dp, 32.dp)
-                    )
-
-                    // 공백
-                    Spacer(modifier = Modifier.width(8.dp))
-
-                    Column {
-                        Row(verticalAlignment = Alignment.CenterVertically) {
-                            // 댓글 작성자 닉네임
-                            QuackBody3(
-                                text = "닉네임",
-                                onClick = {},
-                                color = QuackColor.Black,
-                            )
-
-                            // 공백
-                            Spacer(modifier = Modifier.width(4.dp))
-                        }
-
-                        // 공백
-                        Spacer(modifier = Modifier.height(2.dp))
-
-                        // 응시자, 일자
-                        QuackBody3(
-                            text = stringResource(R.string.detail_num_date, "20", "1일 전"),
-                            color = QuackColor.Gray2
-                        )
-                    }
-
-                    // 공백
-                    Spacer(modifier = Modifier.weight(1f))
-
-                    // TODO(riflockle7): 추후 팔로우 완료 시에 대한 처리 필요
-                    // 팔로우 버튼
-                    QuackBody2(
-                        padding = PaddingValues(
-                            top = 8.dp,
-                            bottom = 8.dp,
-                        ),
-                        text = activity.getString(R.string.detail_follow),
-                        color = QuackColor.DuckieOrange,
-                        onClick = { },
-                    )
-                }
-                // 구분선
-                QuackDivider()
-                // 공백
-                Spacer(modifier = Modifier.height(24.dp))
-                // 점수 분포도 제목 Layout
-                Row(
-                    modifier = Modifier.padding(horizontal = 16.dp),
-                    verticalAlignment = Alignment.CenterVertically
-                ) {
-                    // 제목
-                    QuackText(text = "점수 분포도", style = QuackTextStyle.Title2)
-
-                    // 공백
-                    Spacer(modifier = Modifier.weight(1f))
-
-                    // 정답률 텍스트
-                    QuackText(
-                        text = activity.getString(R.string.detail_right_percent, "80%"),
-                        style = QuackTextStyle.Body2
-                    )
-                }
-                // 공백
-                Spacer(modifier = Modifier.height(8.dp))
-                // 분포도 레퍼
-                QuackText(
-                    modifier = Modifier.padding(horizontal = 16.dp),
-                    text = "분포도 레퍼 필요",
-                    style = QuackTextStyle.Body2
-                )
-                // 하단 공백
-                Spacer(modifier = Modifier.height(53.dp))
-            }
+            // content Layout
+            DetailContentLayout()
+            // 최하단 Layout
             DetailBottomLayout(
                 modifier = Modifier
                     .layoutId(DetailScreenBottomBarLayoutId)
@@ -277,6 +149,158 @@ internal fun DetailScreen(modifier: Modifier = Modifier) = CoroutineScopeContent
     }
 }
 
+/** 상세 화면 컨텐츠 Layout */
+@Composable
+private fun DetailContentLayout() {
+    val configuration = LocalConfiguration.current
+    val screenWidthDp = configuration.screenWidthDp.dp
+    val detailImageWidthDp = screenWidthDp - 32.dp
+
+    Column(
+        modifier = Modifier
+            .layoutId(DetailScreenContentLayoutId)
+            .fillMaxWidth()
+            .verticalScroll(rememberScrollState()),
+    ) {
+        // 그림
+        QuackImage(
+            size = DpSize(detailImageWidthDp, detailImageWidthDp * 240 / 328),
+            padding = PaddingValues(
+                top = 16.dp,
+                start = 16.dp,
+                end = 16.dp
+            ),
+            contentScale = ContentScale.FillWidth,
+            src = R.drawable.img_duckie_detail_image_dummy,
+        )
+        // 공백
+        Spacer(modifier = Modifier.height(12.dp))
+        // 제목
+        QuackHeadLine2(
+            modifier = Modifier.padding(horizontal = 16.dp),
+            text = "제 1회 도로 패션영역"
+        )
+        // 공백
+        Spacer(modifier = Modifier.height(8.dp))
+        // 내용
+        QuackBody2(
+            modifier = Modifier.padding(horizontal = 16.dp),
+            text = "아 저 근데 너무 재밌을거 같아요 내 시험 최고"
+        )
+        // 공백
+        Spacer(modifier = Modifier.height(12.dp))
+        // 태그 목록
+        QuackSingeLazyRowTag(
+            contentPadding = PaddingValues(horizontal = 16.dp, vertical = 4.dp),
+            horizontalSpace = 4.dp,
+            items = persistentListOf(
+                "도로", "패션", "도로패션", "도로로", "Doro Driven Design"
+            ),
+            tagType = QuackTagType.Grayscale(""),
+            onClick = {},
+        )
+        // 공백
+        Spacer(modifier = Modifier.height(24.dp))
+        // 구분선
+        QuackDivider()
+        // 프로필 Layout
+        DetailProfileLayout()
+        // 구분선
+        QuackDivider()
+        // 공백
+        Spacer(modifier = Modifier.height(24.dp))
+        // 점수 분포도 Layout
+        DetailScoreDistributionLayout()
+    }
+}
+
+/**
+ * 상세 화면 프로필 Layout
+ * TODO(riflockle7): 추후 공통화하기
+ */
+@Composable
+private fun DetailProfileLayout() {
+    Row(
+        modifier = Modifier.padding(
+            horizontal = 16.dp,
+            vertical = 12.dp,
+        ),
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        // 작성자 프로필 이미지
+        QuackImage(
+            src = team.duckie.quackquack.ui.R.drawable.quack_ic_profile_24,
+            size = DpSize(32.dp, 32.dp)
+        )
+        // 공백
+        Spacer(modifier = Modifier.width(8.dp))
+        // 닉네임, 응시자, 일자 Layout
+        Column {
+            Row(verticalAlignment = Alignment.CenterVertically) {
+                // 댓글 작성자 닉네임
+                QuackBody3(
+                    text = "닉네임",
+                    onClick = {},
+                    color = QuackColor.Black,
+                )
+
+                // 공백
+                Spacer(modifier = Modifier.width(4.dp))
+            }
+
+            // 공백
+            Spacer(modifier = Modifier.height(2.dp))
+
+            // 응시자, 일자
+            QuackBody3(
+                text = stringResource(R.string.detail_num_date, "20", "1일 전"),
+                color = QuackColor.Gray2
+            )
+        }
+        // 공백
+        Spacer(modifier = Modifier.weight(1f))
+        // TODO(riflockle7): 추후 팔로우 완료 시에 대한 처리 필요
+        // 팔로우 버튼
+        QuackBody2(
+            padding = PaddingValues(
+                top = 8.dp,
+                bottom = 8.dp,
+            ),
+            text = stringResource(R.string.detail_follow),
+            color = QuackColor.DuckieOrange,
+            onClick = { },
+        )
+    }
+}
+
+/** 상세 화면 점수 분포도 Layout */
+@Composable
+private fun DetailScoreDistributionLayout() {
+    // 제목 Layout
+    Row(
+        modifier = Modifier.padding(horizontal = 16.dp),
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        // 제목
+        QuackText(text = "점수 분포도", style = QuackTextStyle.Title2)
+        // 공백
+        Spacer(modifier = Modifier.weight(1f))
+        // 정답률 텍스트
+        QuackText(
+            text = stringResource(R.string.detail_right_percent, "80%"),
+            style = QuackTextStyle.Body2
+        )
+    }
+    // 공백
+    Spacer(modifier = Modifier.height(8.dp))
+    // 분포도 레퍼
+    QuackText(
+        modifier = Modifier.padding(horizontal = 16.dp),
+        text = "분포도 레퍼 필요",
+        style = QuackTextStyle.Body2
+    )
+}
+
 /** 상세 화면 최하단 Layout */
 @Composable
 private fun DetailBottomLayout(
@@ -287,7 +311,7 @@ private fun DetailBottomLayout(
     Column(modifier = modifier) {
         // 구분선
         QuackDivider()
-
+        // 버튼 모음 Layout
         // TODO(riflockle7): 추후 Layout 을 활용해 처리하기
         Row(
             modifier = Modifier
