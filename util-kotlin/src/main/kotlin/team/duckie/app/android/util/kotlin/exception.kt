d0fd6f7c098f80d02191b511228f59e11e3460a9
jisungbin <ji@sungb.in>
2022-12-26 00:32:21 +0900

    feat: DuckieApiException 에 toString 정의

--- util-kotlin/src/main/kotlin/team/duckie/app/android/util/kotlin/exception.kt
+++ util-kotlin/src/main/kotlin/team/duckie/app/android/util/kotlin/exception.kt
@@ -5,17 +5,21 @@
  * Please see full license: https://github.com/duckie-team/duckie-android/blob/develop/LICENSE
  */
 
+@file:Suppress("MemberVisibilityCanBePrivate")
+
 package team.duckie.app.android.util.kotlin
 
 open class DuckieApiException(
     override val message: String,
     val code: String? = null,
     val errors: List<String>? = null,
-) : Exception(message)
+) : Exception(message) {
+    override fun toString(): String {
+        return "DuckieApiException(message=$message, code=$code, errors=$errors)"
+    }
+}
 
-class DuckieResponseNPE(
-    requiredFieldName: String
-) : DuckieApiException(
+class DuckieResponseNPE(requiredFieldName: String) : DuckieApiException(
     code = "400",
-    message = "필수 필드가 response에 누락되었습니다. 누락된 필드명: $requiredFieldName"
+    message = "필수 필드가 response에 누락되었습니다. 누락된 필드명: $requiredFieldName",
 )
