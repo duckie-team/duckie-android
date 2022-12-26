76fffb25387ae6fdefb28a3379c9798dd4ddf61e
limsaehyun <sh00710000@gmail.com>
2022-12-26 16:42:33 +0900

    docs: home data class KDoc

--- feature-ui-home/src/main/kotlin/team/duckie/app/android/feature/ui/home/viewmodel/state/HomeState.kt
+++ feature-ui-home/src/main/kotlin/team/duckie/app/android/feature/ui/home/viewmodel/state/HomeState.kt
@@ -11,19 +11,28 @@ import kotlinx.collections.immutable.ImmutableList
 import kotlinx.collections.immutable.PersistentList
 import kotlinx.collections.immutable.persistentListOf
 import team.duckie.app.android.feature.ui.home.constants.HomeStep
-import team.duckie.app.android.feature.ui.home.screen.BottomNavigationStep
+import team.duckie.app.android.feature.ui.home.constants.BottomNavigationStep
 
 data class HomeState(
     val step: BottomNavigationStep = BottomNavigationStep.HomeScreen,
     val selectedTabIndex: HomeStep = HomeStep.HomeRecommendScreen,
 
     val jumbotrons: PersistentList<HomeRecommendJumbotron> = persistentListOf(),
-    val recommendTopics: PersistentList<TopicRecommendItem> = persistentListOf(),
+    val recommendTopics: PersistentList<RecommendTopic> = persistentListOf(),
 
     val recommendFollowing: PersistentList<RecommendUserByTopic> = persistentListOf(),
-    val recommendFollowingTest: PersistentList<TestMaker> = persistentListOf(),
+    val recommendFollowingTest: PersistentList<FollowingTest> = persistentListOf(),
 ) {
-    data class TestMaker(
+    /**
+     * 팔로잉의 덕질고사 추천 피드 data class [FollowingTest]
+     *
+     * @param coverUrl 덕질고사 커버 이미지 url
+     * @param title 덕질고사 제목
+     * @param examineeNumber 덕질고사 응시자 정보
+     * @param createAt 덕질고사가 만들어진 날짜
+     * @param owner 덕질고사 제작자 정보
+     */
+    data class FollowingTest(
         val coverUrl: String,
         val title: String,
         val examineeNumber: Int,
@@ -36,32 +45,53 @@ data class HomeState(
         )
     }
 
+    /**
+     * 주제에 관련한 유저 추천 data class [RecommendUserByTopic]
+     *
+     * @param topic 덕질고사 주제
+     * @param users 추천하는 유저들
+     */
     data class RecommendUserByTopic(
         val topic: String,
-        val users: ImmutableList<RecommendUser>,
-    )
+        val users: ImmutableList<User>,
+    ) {
+        data class User(
+            val userId: Int,
+            val profile: String,
+            val name: String,
+            val examineeNumber: Int,
+            val createAt: String,
+        )
+    }
 
-    data class RecommendUser(
-        val userId: Int,
-        val profile: String,
-        val name: String,
-        val examineeNumber: Int,
-        val createAt: String,
-    )
-
+    /**
+     * 덕질고사 jumbotron 추천 피드 data class
+     *
+     * @param coverUrl 덕질고사 커버 이미지 url
+     * @param title 덕질고사 제목
+     * @param content 덕질고사 설명
+     * @param buttonContent 시작 버튼 content
+     */
     data class HomeRecommendJumbotron(
-        val image: String,
+        val coverUrl: String,
         val title: String,
         val content: String,
         val buttonContent: String,
     )
 
-    data class TopicRecommendItem(
+    /**
+     * 덕질고사 주제별 추천 피드 data class
+     *
+     * @param title 덕질고사 제목
+     * @param tag 덕질고사 태그
+     * @param items 추천하는 덕질고사 정보
+     */
+    data class RecommendTopic(
         val title: String,
         val tag: String,
-        val items: PersistentList<DuckTest>,
+        val items: PersistentList<Test>,
     ) {
-        data class DuckTest(
+        data class Test(
             val coverImg: String,
             val nickname: String,
             val title: String,
