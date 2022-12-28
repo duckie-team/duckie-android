882d1e28246d8a2c2677058accf94efa762b5caf
jisungbin <ji@sungb.in>
2022-12-29 01:15:21 +0900

    feat: 백엔드 에러 처리 로직 추가

---
+++ data/src/main/kotlin/team/duckie/app/android/data/_exception/model/ExceptionBody.kt
@@ -0,0 +1,33 @@
+/*
+ * Designed and developed by Duckie Team, 2022
+ *
+ * Licensed under the MIT.
+ * Please see full license: https://github.com/duckie-team/duckie-android/blob/develop/LICENSE
+ */
+
+@file:Suppress("PackageNaming")
+
+package team.duckie.app.android.data._exception.model
+
+import com.fasterxml.jackson.annotation.JsonProperty
+import team.duckie.app.android.util.kotlin.duckieResponseException
+import team.duckie.app.android.util.kotlin.duckieResponseFieldNpe
+
+internal data class ExceptionBody(
+    @field:JsonProperty("code")
+    val code: String? = null,
+
+    @field:JsonProperty("message")
+    val message: String? = null,
+
+    @field:JsonProperty("errors")
+    val errors: List<String?>? = null,
+)
+
+internal fun ExceptionBody.throwing(): Nothing {
+    duckieResponseException(
+        message = message,
+        code = code ?: duckieResponseFieldNpe("code"),
+        errors = errors?.filterNotNull(),
+    )
+}
---
+++ data/src/main/kotlin/team/duckie/app/android/data/_exception/util/responseCatching.kt
@@ -0,0 +1,29 @@
+/*
+ * Designed and developed by Duckie Team, 2022
+ *
+ * Licensed under the MIT.
+ * Please see full license: https://github.com/duckie-team/duckie-android/blob/develop/LICENSE
+ */
+
+@file:Suppress("PackageNaming")
+
+package team.duckie.app.android.data._exception.util
+
+import io.ktor.client.call.body
+import io.ktor.client.statement.HttpResponse
+import team.duckie.app.android.data._exception.model.ExceptionBody
+import team.duckie.app.android.data._exception.model.throwing
+
+@Suppress("TooGenericExceptionCaught")
+internal suspend inline fun <reified DataModel, DomainModel> responseCatching(
+    response: HttpResponse,
+    parse: (body: DataModel) -> DomainModel,
+): DomainModel {
+    try {
+        val body: DataModel = response.body()
+        return parse(body)
+    } catch (_: Throwable) {
+        val errorBody: ExceptionBody = response.body()
+        errorBody.throwing()
+    }
+}
