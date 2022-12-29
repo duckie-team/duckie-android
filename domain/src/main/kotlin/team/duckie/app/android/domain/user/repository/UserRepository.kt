d322bc1aebb761c9242fe2427a1c428a9bfdd167
jisungbin <ji@sungb.in>
2022-12-30 08:59:41 +0900

    feat: user api 추가

---
+++ domain/src/main/kotlin/team/duckie/app/android/domain/user/repository/UserRepository.kt
@@ -0,0 +1,24 @@
+/*
+ * Designed and developed by Duckie Team, 2022
+ *
+ * Licensed under the MIT.
+ * Please see full license: https://github.com/duckie-team/duckie-android/blob/develop/LICENSE
+ */
+
+package team.duckie.app.android.domain.user.repository
+
+import androidx.compose.runtime.Immutable
+import team.duckie.app.android.domain.category.model.Category
+import team.duckie.app.android.domain.tag.model.Tag
+import team.duckie.app.android.domain.user.model.User
+
+@Immutable
+interface UserRepository {
+    suspend fun update(
+        id: Int,
+        nickname: String?,
+        profileImageUrl: String?,
+        favoriteCategories: List<Category>?,
+        favoriteTags: List<Tag>?,
+    ): User
+}
