aaad64eef26a16cd269172056e921bb164467456
jisungbin <ji@sungb.in>
2022-12-31 03:50:03 +0900

    refactor: 코드 정리

--- data/src/main/kotlin/team/duckie/app/android/data/file/repository/FileRepositoryImpl.kt
+++ data/src/main/kotlin/team/duckie/app/android/data/file/repository/FileRepositoryImpl.kt
@@ -7,7 +7,6 @@
 
 package team.duckie.app.android.data.file.repository
 
-import com.fasterxml.jackson.core.type.TypeReference
 import io.ktor.client.HttpClient
 import io.ktor.client.call.body
 import io.ktor.client.plugins.onUpload
@@ -18,7 +17,7 @@ import io.ktor.client.request.setBody
 import java.io.File
 import javax.inject.Inject
 import team.duckie.app.android.data._exception.util.responseCatching
-import team.duckie.app.android.data._util.jsonMapper
+import team.duckie.app.android.data._util.toStringJsonMap
 import team.duckie.app.android.domain.file.repository.FileRepository
 import team.duckie.app.android.util.kotlin.duckieResponseFieldNpe
 
@@ -40,7 +39,7 @@ class FileRepositoryImpl @Inject constructor(
             }
         }
         return responseCatching<String, String>(response.body()) { body ->
-            val json = jsonMapper.readValue(body, object : TypeReference<Map<String?, String?>>() {})
+            val json = body.toStringJsonMap()
             json["url"] ?: duckieResponseFieldNpe("url")
         }
     }
