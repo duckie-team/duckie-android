e23bd555f2a5374db7d89534bf4e069c4baaf00a
jisungbin <ji@sungb.in>
2022-12-31 03:49:48 +0900

    feat: json mapping 확장함수 추가

--- data/src/main/kotlin/team/duckie/app/android/data/_util/json.kt
+++ data/src/main/kotlin/team/duckie/app/android/data/_util/json.kt
@@ -7,6 +7,15 @@
 
 package team.duckie.app.android.data._util
 
+import com.fasterxml.jackson.core.type.TypeReference
 import com.fasterxml.jackson.databind.ObjectMapper
 
 internal val jsonMapper = ObjectMapper()
+
+/**
+ * 모든 필드가 String 일 때만 사용해야 합니다.
+ * 그렇지 않으면 역직렬화 에러가 발생합니다.
+ */
+internal fun String.toStringJsonMap(): Map<String, String?> {
+    return jsonMapper.readValue(this, object : TypeReference<Map<String, String?>>() {})
+}
