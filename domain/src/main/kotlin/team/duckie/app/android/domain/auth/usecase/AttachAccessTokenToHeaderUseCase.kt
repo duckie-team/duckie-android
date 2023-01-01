ae653733b32b2eb771c502cdf09a0e7e1f9ac51b
jisungbin <ji@sungb.in>
2023-01-01 09:42:45 +0900

    feat: AttachAccessTokenToHeaderUseCase 추가

---
+++ domain/src/main/kotlin/team/duckie/app/android/domain/auth/usecase/AttachAccessTokenToHeaderUseCase.kt
@@ -0,0 +1,19 @@
+/*
+ * Designed and developed by Duckie Team, 2022
+ *
+ * Licensed under the MIT.
+ * Please see full license: https://github.com/duckie-team/duckie-android/blob/develop/LICENSE
+ */
+
+package team.duckie.app.android.domain.auth.usecase
+
+import javax.inject.Inject
+import team.duckie.app.android.domain.auth.repository.AuthRepository
+
+class AttachAccessTokenToHeaderUseCase @Inject constructor(
+    private val repository: AuthRepository,
+) {
+    operator fun invoke(accessToken: String) {
+        repository.attachAccessTokenToHeader(accessToken)
+    }
+}
