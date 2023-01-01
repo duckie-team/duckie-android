ecda8e3ae24808001f40bce7455b8d0a7586c22a
jisungbin <ji@sungb.in>
2023-01-01 10:29:52 +0900

    refactor: 상단 저작권 헤더 정리

--- build-logic/src/main/kotlin/AppVersionNameProvider.kt
+++ build-logic/src/main/kotlin/AppVersionNameProvider.kt
@@ -1,13 +1,13 @@
-import org.gradle.api.Plugin
-import org.gradle.api.Project
-import team.duckie.app.android.convention.ApplicationConstants
-
 /*
  * Designed and developed by Duckie Team, 2022
  *
  * Licensed under the MIT.
  * Please see full license: https://github.com/duckie-team/duckie-android/blob/develop/LICENSE
  */
+
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+import team.duckie.app.android.convention.ApplicationConstants
 
 class AppVersionNameProvider : Plugin<Project> {
     override fun apply(target: Project) = Unit
