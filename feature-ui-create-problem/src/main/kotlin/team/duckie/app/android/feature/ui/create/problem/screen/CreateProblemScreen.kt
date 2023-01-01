fbce95571b6c08c2024f04a6abb524bf7f949af0
ricky_0_k <ridickle7@gmail.com>
2023-01-02 01:17:59 +0900

    feat: Lazy 하지 않은 Grid 빌더 컴포저블 생성 (NoLazyGridItems)

--- feature-ui-create-problem/src/main/kotlin/team/duckie/app/android/feature/ui/create/problem/screen/CreateProblemScreen.kt
+++ feature-ui-create-problem/src/main/kotlin/team/duckie/app/android/feature/ui/create/problem/screen/CreateProblemScreen.kt
@@ -691,3 +691,76 @@ private val imagePermission
 private fun String.check(context: Context): Boolean {
     return ContextCompat.checkSelfPermission(context, this) == PackageManager.PERMISSION_GRANTED
 }
+
+/**
+ * Lazy 하지 않은 그리드 목록 빌더 (개수로 계산)
+ * (https://stackoverflow.com/questions/69336555/fixed-grid-inside-lazycolumn-in-jetpack-compose)
+ *
+ * @param count 전체 아이템 개수
+ * @param nColumns 한 행의 개수
+ * @param paddingValues 그리드 목록 패딩
+ * @param horizontalArrangement 정렬 방향
+ * @param itemContent index 기반으로 만들어지는 아이템 컨텐츠 빌더
+ */
+@Composable
+fun NoLazyGridItems(
+    count: Int,
+    nColumns: Int,
+    paddingValues: PaddingValues = PaddingValues(0.dp),
+    horizontalArrangement: Arrangement.Horizontal = Arrangement.Start,
+    itemContent: @Composable BoxScope.(Int) -> Unit,
+) {
+    NoLazyGridItems(
+        data = List(count) { it },
+        nColumns = nColumns,
+        paddingValues = paddingValues,
+        horizontalArrangement = horizontalArrangement,
+        itemContent = itemContent,
+    )
+}
+
+/**
+ * Lazy 하지 않은 그리드 목록 빌더 (개수로 계산)
+ * (https://stackoverflow.com/questions/69336555/fixed-grid-inside-lazycolumn-in-jetpack-compose)
+ *
+ * @param data 전체 아이템
+ * @param nColumns 한 행의 개수
+ * @param paddingValues 그리드 목록 패딩
+ * @param horizontalArrangement 정렬 방향
+ * @param key 사전 실행 로직 함수?
+ * @param itemContent data[`index`] 기반으로 만들어지는 아이템 컨텐츠 빌더
+ */
+@Composable
+fun <T> NoLazyGridItems(
+    data: List<T>,
+    nColumns: Int,
+    paddingValues: PaddingValues = PaddingValues(0.dp),
+    horizontalArrangement: Arrangement.Horizontal = Arrangement.Start,
+    key: ((item: T) -> Any)? = null,
+    itemContent: @Composable BoxScope.(T) -> Unit,
+) {
+    val rows = if (data.isEmpty()) 0 else 1 + (data.count() - 1) / nColumns
+    for (rowIndex in 0 until rows) {
+        Row(
+            modifier = Modifier.padding(paddingValues),
+            horizontalArrangement = horizontalArrangement
+        ) {
+            for (columnIndex in 0 until nColumns) {
+                val itemIndex = rowIndex * nColumns + columnIndex
+                if (itemIndex < data.count()) {
+                    val item = data[itemIndex]
+                    androidx.compose.runtime.key(key?.invoke(item)) {
+                        Box(
+                            modifier = Modifier.weight(1f, fill = true),
+                            propagateMinConstraints = true
+                        ) {
+                            itemContent.invoke(this, item)
+                        }
+                    }
+                } else {
+                    Spacer(Modifier.weight(1f, fill = true))
+                }
+            }
+        }
+    }
+}
