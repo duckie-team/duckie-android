58851fa7ef226de3a6559d35c0182c27a84feb6c
jisungbin <ji@sungb.in>
2023-01-01 09:40:14 +0900

    feat: HttpClient#update 확장 함수 추가

--- data/src/main/kotlin/team/duckie/app/android/data/_util/KtorExtensions.kt
+++ data/src/main/kotlin/team/duckie/app/android/data/_util/KtorExtensions.kt
@@ -7,9 +7,19 @@
 
 package team.duckie.app.android.data._util
 
+import io.ktor.client.HttpClient
+import io.ktor.client.HttpClientConfig
 import io.ktor.client.request.HttpRequestBuilder
 import io.ktor.client.request.setBody
+import team.duckie.app.android.data._datasource.updateClient
 
 internal inline fun HttpRequestBuilder.jsonBody(builder: JsonBuilder.() -> Unit) {
     setBody(buildJson(builder))
+}
+
+internal inline fun HttpClient.update(
+    noinline updateLogic: HttpClientConfig<*>.() -> Unit = {},
+    updateField: (client: HttpClient) -> Unit = ::updateClient,
+) {
+    updateField(config(updateLogic))
 }
