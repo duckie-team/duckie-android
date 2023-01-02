5a33c46a18b644a9e7754256bc9acf8fc441741e
jisungbin <ji@sungb.in>
2023-01-02 20:59:32 +0900

    feat: Map 타입을 직접 정의할 수 있는 String.toJsonMap 유틸 추가

--- data/src/test/kotlin/team/duckie/app/android/data/JsonMappingTest.kt
+++ data/src/test/kotlin/team/duckie/app/android/data/JsonMappingTest.kt
@@ -11,15 +11,26 @@ import org.junit.Test
 import strikt.api.expectThat
 import strikt.assertions.isEqualTo
 import strikt.assertions.isNotNull
+import team.duckie.app.android.data._util.toJsonMap
 import team.duckie.app.android.data._util.toStringJsonMap
 
 class JsonMappingTest {
     @Test
-    fun mapping2map() {
+    fun mapping_to_string_map() {
         val rawJson = "{\"name\":\"duckie\"}"
         val actual = rawJson.toStringJsonMap()
 
         val expected = "duckie"
+
+        expectThat(actual["name"]).isNotNull().isEqualTo(expected)
+    }
+
+    @Test
+    fun mapping_to_list_map() {
+        val rawJson = "{\"name\":[\"duckie\"]}"
+        val actual: Map<String, List<String>> = rawJson.toJsonMap()
+
+        val expected = listOf("duckie")
 
         expectThat(actual["name"]).isNotNull().isEqualTo(expected)
     }
