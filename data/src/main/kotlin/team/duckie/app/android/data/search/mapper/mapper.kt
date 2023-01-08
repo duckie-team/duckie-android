d411db949e859d3c8d1bd50bcf8df84c8cdedd6e
ricky_0_k <ridickle7@gmail.com>
2023-01-08 14:49:42 +0900

    feat: GET /search 구현

---
+++ data/src/main/kotlin/team/duckie/app/android/data/search/mapper/mapper.kt
@@ -0,0 +1,34 @@
+/*
+ * Designed and developed by Duckie Team, 2022
+ *
+ * Licensed under the MIT.
+ * Please see full license: https://github.com/duckie-team/duckie-android/blob/develop/LICENSE
+ */
+
+package team.duckie.app.android.data.search.mapper
+
+import team.duckie.app.android.data.exam.mapper.toDomain
+import team.duckie.app.android.data.exam.model.ExamData
+import team.duckie.app.android.data.search.model.SearchData
+import team.duckie.app.android.data.user.mapper.toDomain
+import team.duckie.app.android.data.user.model.UserResponse
+import team.duckie.app.android.domain.search.model.Search
+import team.duckie.app.android.util.kotlin.OutOfDateApi
+import team.duckie.app.android.util.kotlin.duckieResponseFieldNpe
+import team.duckie.app.android.util.kotlin.fastMap
+
+@OptIn(OutOfDateApi::class)
+internal fun SearchData.toDomain() = when (this) {
+    is SearchData.ExamSearchData -> Search.ExamSearch(
+        exams = exams?.fastMap(ExamData::toDomain)
+            ?: duckieResponseFieldNpe("${this::class.java.simpleName}.exams"),
+        page = page ?: duckieResponseFieldNpe("${this::class.java.simpleName}.page")
+    )
+
+    is SearchData.UserSearchData -> Search.UserSearch(
+        users = users?.fastMap(UserResponse::toDomain)
+            ?: duckieResponseFieldNpe("${this::class.java.simpleName}.users"),
+        page = page ?: duckieResponseFieldNpe("${this::class.java.simpleName}.page")
+    )
+}
+
---
+++ data/src/main/kotlin/team/duckie/app/android/data/search/repository/SearchRepositoryImpl.kt
@@ -0,0 +1,30 @@
+/*
+ * Designed and developed by Duckie Team, 2022
+ *
+ * Licensed under the MIT.
+ * Please see full license: https://github.com/duckie-team/duckie-android/blob/develop/LICENSE
+ */
+
+package team.duckie.app.android.data.search.repository
+
+import io.ktor.client.request.get
+import io.ktor.client.request.url
+import io.ktor.client.statement.bodyAsText
+import team.duckie.app.android.data._datasource.client
+import team.duckie.app.android.data._exception.util.responseCatching
+import team.duckie.app.android.data._util.toJsonObject
+import team.duckie.app.android.data.search.mapper.toDomain
+import team.duckie.app.android.data.search.model.SearchData
+import team.duckie.app.android.domain.search.model.Search
+import team.duckie.app.android.domain.search.repository.SearchRepository
+
+class SearchRepositoryImpl : SearchRepository {
+    override suspend fun getSearch(query: String, page: Int, type: String): Search {
+        val response = client.get {
+            url("/search")
+        }
+        return responseCatching(response.bodyAsText()) { body ->
+            body.toJsonObject<SearchData>().toDomain()
+        }
+    }
+}
