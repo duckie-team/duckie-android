da6152ba314f5af761df54a2f43048e115e973a0
jisungbin <ji@sungb.in>
2023-01-10 10:22:08 +0900

    chore: orbit-compose 의존성 추가

--- feature-ui-onboard/build.gradle.kts
+++ feature-ui-onboard/build.gradle.kts
@@ -31,6 +31,7 @@ dependencies {
         projects.sharedUiCompose,
         libs.apache.commons.io,
         libs.orbit.viewmodel,
+        libs.orbit.compose,
         libs.ktx.lifecycle.runtime,
         libs.compose.ui.material, // needs for ModalBottomSheet
         libs.compose.lifecycle.runtime,
