feefcd714940ceff9a23a727213aa8ba25cea7a3
EvergreenTree97 <tkdfhr7497@naver.com>
2023-01-15 01:52:40 +0900

    feat: 단답형 문제 폼

---
+++ feature-ui-solve-problem/src/main/kotlin/team/duckie/app/android/feature/ui/solve/problem/screen/ShortAnswerForm.kt
@@ -0,0 +1,59 @@
+/*
+ * Designed and developed by Duckie Team, 2022
+ *
+ * Licensed under the MIT.
+ * Please see full license: https://github.com/duckie-team/duckie-android/blob/develop/LICENSE
+ */
+
+package team.duckie.app.android.feature.ui.solve.problem.screen
+
+import android.util.Log
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.ExperimentalComposeUiApi
+import androidx.compose.ui.text.TextRange
+import androidx.compose.ui.text.input.TextFieldValue
+import team.duckie.app.android.domain.exam.model.Answer
+import team.duckie.app.android.feature.ui.solve.problem.common.EachCharTextField
+
+internal const val WhiteSpace = '*'
+
+@Composable
+internal fun ShortAnswerForm(
+    answer: Answer,
+) {
+    val shortAnswer = (answer as Answer.Short).answer.text
+    var value by remember { mutableStateOf(TextFieldValue()) } // replace '*' to " " 필요
+
+    EachCharTextField(
+        value = value,
+        onValueChanged = { newValue, prevLength ->
+            value = newValue
+            if (
+                newValue.text.isNotEmpty() &&
+                newValue.text.lastIndex < shortAnswer.length
+                && shortAnswer[newValue.text.lastIndex].isWhitespace()
+                && newValue.text.length > prevLength
+            ) {
+                val lastChar = newValue.text.last()
+                value = value.copy(
+                    text = newValue.text.dropLast(1).plus(WhiteSpace) + lastChar,
+                    selection = TextRange(value.selection.end + 1)
+                )
+            } else if (
+                newValue.text.lastIndex > 0
+                && shortAnswer[newValue.text.lastIndex].isWhitespace()
+                && newValue.text.length < prevLength
+            ) {
+                value = value.copy(
+                    text = newValue.text.dropLast(1),
+                    selection = TextRange(value.selection.end - 1)
+                )
+            }
+        },
+        answer = shortAnswer,
+    )
+}
