61eb88da6a0dfa76edcea3f43a7fe39c9157be32
jisungbin <ji@sungb.in>
2023-01-20 20:20:35 +0900

    feat: HttpClient fuel 초기화 및 provides 설정

---
+++ data/src/main/kotlin/team/duckie/app/android/data/_datasource/AuthorizationClient.kt
@@ -0,0 +1,59 @@
+/*
+ * Designed and developed by Duckie Team, 2022
+ *
+ * Licensed under the MIT.
+ * Please see full license: https://github.com/duckie-team/duckie-android/blob/develop/LICENSE
+ */
+
+package team.duckie.app.android.data._datasource
+
+import android.os.Build
+import com.github.kittinunf.fuel.Fuel
+import com.github.kittinunf.fuel.core.FuelManager
+import com.github.kittinunf.fuel.core.interceptors.LogRequestInterceptor
+import com.github.kittinunf.fuel.core.interceptors.LogResponseInterceptor
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import team.duckie.app.android.data.BuildConfig
+import team.duckie.app.android.util.kotlin.seconds
+
+private object HttpHeaders {
+    const val ContentType = "Content-Type"
+
+    object ContentTypeValues {
+        const val ApplicationJson = "application/json"
+    }
+}
+
+@Suppress("unused")
+@Module
+@InstallIn(SingletonComponent::class)
+internal object AuthorizationClient {
+    private val MaxTimeoutMillis = 3.seconds
+    private const val BaseUrl = "http://api-staging.goose-duckie.com:3000"
+
+    private var DeviceName = Build.MODEL
+    private const val ClientName = "android"
+
+    private operator fun invoke(): Fuel {
+        with(FuelManager.instance) {
+            basePath = BaseUrl
+            timeoutInMillisecond = MaxTimeoutMillis.toInt()
+            timeoutReadInMillisecond = MaxTimeoutMillis.toInt()
+            baseHeaders = mapOf(
+                DuckieHttpHeaders.Client to ClientName,
+                DuckieHttpHeaders.DeviceName to DeviceName,
+                DuckieHttpHeaders.Version to BuildConfig.APP_VERSION_NAME,
+                HttpHeaders.ContentType to HttpHeaders.ContentTypeValues.ApplicationJson,
+            )
+            addRequestInterceptor { LogRequestInterceptor }
+            addResponseInterceptor { LogResponseInterceptor }
+        }
+        return Fuel
+    }
+
+    @Provides
+    fun provideHttpClient(): Fuel = this()
+}
