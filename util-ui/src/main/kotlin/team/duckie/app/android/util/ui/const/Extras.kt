f1358c10d15122215e11459010380d0152ab4043
ricky_0_k <ridickle7@gmail.com>
2023-01-23 03:32:31 +0900

    feat: Intent.Extra Key 값을 관리하는 상수 모음 object 생성

---
+++ util-ui/src/main/kotlin/team/duckie/app/android/util/ui/const/Extras.kt
@@ -0,0 +1,14 @@
+/*
+ * Designed and developed by Duckie Team, 2022
+ *
+ * Licensed under the MIT.
+ * Please see full license: https://github.com/duckie-team/duckie-android/blob/develop/LICENSE
+ */
+
+package team.duckie.app.android.util.ui.const
+
+/** Intent.Extra Key 값 모음 */
+object Extras {
+    const val ExamId = "ExtraExamId"
+    const val CertifyingStatement = "ExtraCertifyingStatement"
+}
