2f8428fac734ebaddfce155bf59ff499c9c43905
EvergreenTree97 <tkdfhr7497@naver.com>
2023-01-23 23:42:33 +0900

    feat: 문제 섹션

--- feature-ui-solve-problem/src/main/kotlin/team/duckie/app/android/feature/ui/solve/problem/question/QuestionSection.kt
+++ feature-ui-solve-problem/src/main/kotlin/team/duckie/app/android/feature/ui/solve/problem/question/QuestionSection.kt
@@ -7,3 +7,55 @@
 
 package team.duckie.app.android.feature.ui.solve.problem.question
 
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyListScope
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import team.duckie.app.android.domain.exam.model.Question
+import team.duckie.app.android.feature.ui.solve.problem.question.audio.AudioPlayer
+import team.duckie.app.android.feature.ui.solve.problem.question.video.VideoPlayer
+import team.duckie.quackquack.ui.component.QuackHeadLine2
+import team.duckie.quackquack.ui.component.QuackImage
+import team.duckie.quackquack.ui.util.DpSize
+
+private val HorizontalPadding = PaddingValues(horizontal = 16.dp)
+internal fun LazyListScope.questionSection(
+    page: Int,
+    question: Question,
+) {
+    item {
+        QuackHeadLine2(
+            text = "${page + 1}. ${question.text}",
+            padding = HorizontalPadding
+        )
+    }
+    when (question) {
+        is Question.Text -> {}
+        is Question.Image -> {
+            item {
+                QuackImage(
+                    src = question.imageUrl,
+                    size = DpSize(all = 200.dp),
+                    padding = HorizontalPadding
+                )
+            }
+        }
+
+        is Question.Audio -> {
+            item {
+                AudioPlayer(
+                    modifier = Modifier.padding(paddingValues = HorizontalPadding),
+                    url = question.audioUrl
+                )
+            }
+        }
+
+        is Question.Video -> {
+            item {
+                VideoPlayer(url = question.videoUrl)
+            }
+        }
+    }
+
+}
