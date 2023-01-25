c169f238bf9ba9166e5a9d3d877db4d17442fc58
jisungbin <ji@sungb.in>
2023-01-25 22:44:19 +0900

    feat: PATCH 문제로 fuel -> ktor 변경

--- data/src/main/kotlin/team/duckie/app/android/data/user/repository/UserRepositoryImpl.kt
+++ data/src/main/kotlin/team/duckie/app/android/data/user/repository/UserRepositoryImpl.kt
@@ -7,16 +7,14 @@
 
 package team.duckie.app.android.data.user.repository
 
-import com.github.kittinunf.fuel.Fuel
-import com.github.kittinunf.fuel.coroutines.awaitObject
-import com.github.kittinunf.fuel.jackson.jacksonDeserializerOf
 import io.ktor.client.call.body
 import io.ktor.client.request.get
+import io.ktor.client.request.patch
 import io.ktor.client.statement.bodyAsText
 import javax.inject.Inject
 import team.duckie.app.android.data._datasource.client
 import team.duckie.app.android.data._exception.util.responseCatching
-import team.duckie.app.android.data._util.buildJson
+import team.duckie.app.android.data._util.jsonBody
 import team.duckie.app.android.data._util.toStringJsonMap
 import team.duckie.app.android.data.user.mapper.toDomain
 import team.duckie.app.android.data.user.model.UserResponse
@@ -27,7 +25,7 @@ import team.duckie.app.android.domain.user.repository.UserRepository
 import team.duckie.app.android.util.kotlin.duckieResponseFieldNpe
 import team.duckie.app.android.util.kotlin.runtimeCheck
 
-class UserRepositoryImpl @Inject constructor(private val fuel: Fuel) : UserRepository {
+class UserRepositoryImpl @Inject constructor() : UserRepository {
     override suspend fun get(id: Int): User {
         val response = client.get("/users/$id")
 
@@ -50,18 +48,18 @@ class UserRepositoryImpl @Inject constructor(private val fuel: Fuel) : UserRepos
         ) {
             "At least one of the parameters must be non-null"
         }
-        val request = fuel
-            .patch("/users/$id")
-            .body(
-                buildJson {
-                    nickname?.let { "nickname" withString nickname }
-                    profileImageUrl?.let { "profileImageUrl" withString profileImageUrl }
-                    favoriteCategories?.let { "favoriteCategories" withPojos favoriteCategories }
-                    favoriteTags?.let { "favoriteTags" withPojos favoriteTags }
-                }
-            )
-        val response = request.awaitObject<UserResponse>(jacksonDeserializerOf())
-        return response.toDomain()
+        val response = client.patch("/users/$id") {
+            jsonBody {
+                nickname?.let { "nickname" withString nickname }
+                profileImageUrl?.let { "profileImageUrl" withString profileImageUrl }
+                favoriteCategories?.let { "favoriteCategories" withPojos favoriteCategories }
+                favoriteTags?.let { "favoriteTags" withPojos favoriteTags }
+            }
+        }
+        return responseCatching(
+            response = response.body(),
+            parse = UserResponse::toDomain,
+        )
     }
 
     override suspend fun nicknameValidateCheck(nickname: String): Boolean {
