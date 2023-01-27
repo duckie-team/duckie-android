8872e18e027ce29d7480c51ffac5014d0126e470
ricky_0_k <ridickle7@gmail.com>
2023-01-28 03:40:37 +0900

    feat: 로딩 화면, 에러 화면 구축 & CreateProblemStep 순서, 숫자 변경

---
+++ feature-ui-create-problem/src/main/kotlin/team/duckie/app/android/feature/ui/create/problem/screen/ErrorScreen.kt
@@ -0,0 +1,48 @@
+/*
+ * Designed and developed by Duckie Team, 2022
+ *
+ * Licensed under the MIT.
+ * Please see full license: https://github.com/duckie-team/duckie-android/blob/develop/LICENSE
+ */
+
+package team.duckie.app.android.feature.ui.create.problem.screen
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material.CircularProgressIndicator
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import kotlinx.coroutines.launch
+import org.orbitmvi.orbit.compose.collectAsState
+import team.duckie.app.android.feature.ui.create.problem.R
+import team.duckie.app.android.feature.ui.create.problem.viewmodel.CreateProblemViewModel
+import team.duckie.app.android.util.compose.activityViewModel
+import team.duckie.quackquack.ui.color.QuackColor
+import team.duckie.quackquack.ui.component.QuackTitle1
+
+@Composable
+internal fun ErrorScreen(
+    viewModel: CreateProblemViewModel = activityViewModel(),
+    modifier: Modifier,
+) {
+    Column(
+        modifier = modifier,
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center,
+    ) {
+        val isEditMode = viewModel.collectAsState().value.isEditMode
+        QuackTitle1(
+            text = "에러입니다\n\n$viewModel\n\n${
+                if (isEditMode) {
+                    stringResource(id = R.string.get_exam_authorized_error)
+                } else {
+                    ""
+                }
+            }",
+        )
+    }
+}
