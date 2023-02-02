96e8e96c2cf3fed9d053eef20ec26e4d877e21bc
jisungbin <ji@sungb.in>
2023-02-02 18:10:43 +0900

    feat: ToastWrapper 에 StringRes 지원 추가

--- util-compose/src/main/kotlin/team/duckie/app/android/util/compose/ToastWrapper.kt
+++ util-compose/src/main/kotlin/team/duckie/app/android/util/compose/ToastWrapper.kt
@@ -9,6 +9,7 @@ package team.duckie.app.android.util.compose
 
 import android.content.Context
 import android.widget.Toast
+import androidx.annotation.StringRes
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
 import androidx.compose.ui.platform.LocalContext
@@ -22,6 +23,18 @@ class ToastWrapper(context: Context) {
     operator fun invoke(message: Any, length: Int = Toast.LENGTH_SHORT) {
         toast.run {
             setText(message.toString())
+            duration = length
+            show()
+        }
+    }
+
+    operator fun invoke(
+        @StringRes stringRes: Int,
+        vararg args: Any,
+        length: Int = Toast.LENGTH_SHORT,
+    ) {
+        toast.run {
+            setText(context.getString(stringRes, *args))
             duration = length
             show()
         }
