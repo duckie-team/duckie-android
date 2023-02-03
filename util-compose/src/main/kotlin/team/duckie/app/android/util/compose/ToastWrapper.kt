e08fc4087fe44673df9e4ce86e43e5b3a649c72e
Ji Sungbin <ji@sungb.in>
2023-02-04 00:21:13 +0900

    태그 생성 실패 대응 (#204)

--- util-compose/src/main/kotlin/team/duckie/app/android/util/compose/ToastWrapper.kt
+++ util-compose/src/main/kotlin/team/duckie/app/android/util/compose/ToastWrapper.kt
@@ -9,6 +9,7 @@ package team.duckie.app.android.util.compose
 
 import android.content.Context
 import android.widget.Toast
+import androidx.annotation.StringRes
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
 import androidx.compose.ui.platform.LocalContext
@@ -17,11 +18,25 @@ import java.lang.ref.WeakReference
 class ToastWrapper(context: Context) {
     private val _context = WeakReference(context)
     private val context get() = _context.get()!!
-    private val toast = Toast.makeText(this.context, "", Toast.LENGTH_SHORT)
+
+    private val _toast = WeakReference(Toast.makeText(this.context, "", Toast.LENGTH_SHORT))
+    private val toast get() = _toast.get()!!
 
     operator fun invoke(message: Any, length: Int = Toast.LENGTH_SHORT) {
         toast.run {
             setText(message.toString())
+            duration = length
+            show()
+        }
+    }
+
+    operator fun invoke(
+        @StringRes stringRes: Int,
+        vararg args: Any,
+        length: Int = Toast.LENGTH_SHORT,
+    ) {
+        toast.run {
+            setText(context.getString(stringRes, *args))
             duration = length
             show()
         }
