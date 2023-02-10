f3ef91c772b6dec01205beb670e2d134c19e4150
EvergreenTree97 <tkdfhr7497@naver.com>
2023-02-11 01:11:57 +0900

    chore: 문제 풀기에서 필요없는 의존성 제거하고, 시험 시작 화면에서 문제 풀기 화면 의존성 추가

--- feature-ui-solve-problem/build.gradle.kts
+++ feature-ui-solve-problem/build.gradle.kts
@@ -26,7 +26,6 @@ dependencies {
         projects.utilKotlin,
         projects.utilCompose,
         projects.sharedUiCompose,
-        projects.featureUiHome,
         libs.orbit.viewmodel,
         libs.orbit.compose,
         libs.ktx.lifecycle.runtime,
