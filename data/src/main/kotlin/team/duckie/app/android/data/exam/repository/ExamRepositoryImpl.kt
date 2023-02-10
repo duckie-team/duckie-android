3537991b7e8fe6477fec540b0079566eb44c09b2
ricky_0_k <ridickle7@gmail.com>
2023-02-11 02:22:20 +0900

    feat: GET /exams/me/following API Mock 제거

--- data/src/main/kotlin/team/duckie/app/android/data/exam/repository/ExamRepositoryImpl.kt
+++ data/src/main/kotlin/team/duckie/app/android/data/exam/repository/ExamRepositoryImpl.kt
@@ -7,6 +7,7 @@
 
 package team.duckie.app.android.data.exam.repository
 
+import com.github.kittinunf.fuel.Fuel
 import io.ktor.client.request.get
 import io.ktor.client.request.post
 import io.ktor.client.request.setBody
@@ -16,14 +17,12 @@ import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
 import team.duckie.app.android.data._datasource.client
 import team.duckie.app.android.data._exception.util.responseCatching
+import team.duckie.app.android.data._exception.util.responseCatchingFuel
 import team.duckie.app.android.data._util.toStringJsonMap
-import team.duckie.app.android.data.category.model.CategoryData
 import team.duckie.app.android.data.exam.mapper.toData
 import team.duckie.app.android.data.exam.mapper.toDomain
 import team.duckie.app.android.data.exam.model.ExamData
-import team.duckie.app.android.data.tag.model.TagData
-import team.duckie.app.android.data.user.model.DuckPowerResponse
-import team.duckie.app.android.data.user.model.UserResponse
+import team.duckie.app.android.data.exam.model.ExamMeFollowingResponseData
 import team.duckie.app.android.domain.exam.model.Exam
 import team.duckie.app.android.domain.exam.model.ExamBody
 import team.duckie.app.android.domain.exam.model.ExamThumbnailBody
@@ -31,11 +30,10 @@ import team.duckie.app.android.domain.exam.repository.ExamRepository
 import team.duckie.app.android.util.kotlin.AllowMagicNumber
 import team.duckie.app.android.util.kotlin.OutOfDateApi
 import team.duckie.app.android.util.kotlin.duckieResponseFieldNpe
-import team.duckie.app.android.util.kotlin.fastMap
 import javax.inject.Inject
 
 // TODO(riflockle7): GET /exams/me/following API commit
-class ExamRepositoryImpl @Inject constructor(/*private val fuel: Fuel*/) : ExamRepository {
+class ExamRepositoryImpl @Inject constructor(private val fuel: Fuel) : ExamRepository {
     @OutOfDateApi
     override suspend fun makeExam(exam: ExamBody): Boolean {
         val response = client.post {
@@ -68,54 +66,22 @@ class ExamRepositoryImpl @Inject constructor(/*private val fuel: Fuel*/) : ExamR
         }
     }
 
-    // TODO(limsaehyun): API 불안정하여 Mock 으로 구현
     // TODO(riflockle7): GET /exams/me/following API commit
     @AllowMagicNumber
     @OutOfDateApi
     override suspend fun getExamFollowing(page: Int): List<Exam> = withContext(Dispatchers.IO) {
-//        val (_, response) = fuel
-//            .post(
-//                "/exams/me/following",
-//                listOf("page" to page),
-//            )
-//            .responseString()
-//
-//        val examMeFollowingResponse = responseCatchingFuel(
-//            response = response,
-//            parse = ExamMeFollowingResponseData::toDomain,
-//        )
-//
-//        return@withContext examMeFollowingResponse.exams
-
-        val response = (0..6).map { exam ->
-            ExamData(
-                id = exam,
-                title = "제 ${exam}회 도로 패션 영역",
-                description = "설명",
-                user = UserResponse(
-                    id = 1,
-                    nickName = "faker",
-                    profileImageUrl = "https://img.freepik.com/free-icon/user_318-804790.jpg?w=2000",
-                    duckPower = DuckPowerResponse(
-                        id = 1,
-                        tier = "덕력 20%",
-                        tag = TagData(
-                            id = 1,
-                            name = "도로패션",
-                        ),
-                    ),
-                ),
-                thumbnailUrl = "https://user-images.githubusercontent.com/80076029/206901501-8d8a97ea-b7d8-4f18-84e7-ba593b4c824b.png",
-                buttonTitle = "열심히 풀어주세요",
-                certifyingStatement = "이것은 content 인비다",
-                solvedCount = 1,
-                answerRate = 0.17f,
-                category = CategoryData(1, "카테고리"),
-                mainTag = TagData(1, "방탄소년단"),
-                type = "text",
+        val (_, response) = fuel
+            .post(
+                "/exams/me/following",
+                listOf("page" to page),
             )
-        }
+            .responseString()
 
-        return@withContext response.fastMap(ExamData::toDomain)
+        val examMeFollowingResponse = responseCatchingFuel(
+            response = response,
+            parse = ExamMeFollowingResponseData::toDomain,
+        )
+
+        return@withContext examMeFollowingResponse.exams
     }
 }
