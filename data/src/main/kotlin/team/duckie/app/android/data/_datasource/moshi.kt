4e4531c310149deac7545f7b4e2c93c93bf6008d
EvergreenTree97 <tkdfhr7497@naver.com>
2023-02-14 00:14:40 +0900

    feat: question, answer에 대한 moshiFactory 추가

--- data/src/main/kotlin/team/duckie/app/android/data/_datasource/moshi.kt
+++ data/src/main/kotlin/team/duckie/app/android/data/_datasource/moshi.kt
@@ -10,11 +10,15 @@ package team.duckie.app.android.data._datasource
 import com.squareup.moshi.Moshi
 import com.squareup.moshi.adapters.PolymorphicJsonAdapterFactory
 import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
+import team.duckie.app.android.data.exam.model.AnswerData
+import team.duckie.app.android.data.exam.model.QuestionData
 import team.duckie.app.android.data.search.model.SearchData
 import team.duckie.app.android.domain.search.model.Search
 
 internal val MoshiBuilder = Moshi.Builder()
     .add(searchDataFactory)
+    .add(questionDataFactory)
+    .add(answerDataFactory)
     .addLast(KotlinJsonAdapterFactory())
     .build()
 
@@ -23,3 +27,16 @@ internal val searchDataFactory
         .withSubtype(SearchData.ExamSearchData::class.java, Search.Exam)
         .withSubtype(SearchData.TagSearchData::class.java, Search.Tags)
         .withSubtype(SearchData.UserSearchData::class.java, Search.User)
+
+internal val questionDataFactory
+    get() = PolymorphicJsonAdapterFactory.of(QuestionData::class.java, "type")
+        .withSubtype(QuestionData.Text::class.java, "text")
+        .withSubtype(QuestionData.Image::class.java, "image")
+        .withSubtype(QuestionData.Audio::class.java, "audio")
+        .withSubtype(QuestionData.Video::class.java, "video")
+
+internal val answerDataFactory
+    get() = PolymorphicJsonAdapterFactory.of(AnswerData::class.java, "type")
+        .withSubtype(AnswerData.ShortAnswer::class.java, "shortAnswer")
+        .withSubtype(AnswerData.Choice::class.java, "choice")
+        .withSubtype(AnswerData.ImageChoice::class.java, "imageChoice")
