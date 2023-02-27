0009a730c3091a418f4e4104b8e9664beff6194d
Choi Sang Rok <tkdfhr7497@naver.com>
2023-02-27 20:50:11 +0900

    reslove #255 (#275)
    * refactor: gradlew build
    
    * refactor: Follwing thumbnail roundcorner 적용

--- feature-ui-home/src/main/kotlin/team/duckie/app/android/feature/ui/home/screen/HomeFollowingScreen.kt
+++ feature-ui-home/src/main/kotlin/team/duckie/app/android/feature/ui/home/screen/HomeFollowingScreen.kt
@@ -13,6 +13,7 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
@@ -21,10 +22,12 @@ import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.lazy.itemsIndexed
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
@@ -194,6 +197,8 @@ private fun HomeFollowingInitialRecommendUsers(
     }
 }
 
+internal const val ThumbnailRatio = 4f / 3f
+
 @Composable
 private fun TestCoverWithMaker(
     modifier: Modifier = Modifier,
@@ -211,13 +216,15 @@ private fun TestCoverWithMaker(
     ) {
         AsyncImage(
             modifier = Modifier
+                .aspectRatio(ThumbnailRatio)
                 .fillMaxWidth()
+                .clip(RoundedCornerShape(8.dp))
                 .quackClickable {
                     onClickTestCover()
                 },
             model = cover,
             contentDescription = null,
-            contentScale = ContentScale.FillWidth,
+            contentScale = ContentScale.FillBounds,
         )
         TestMakerLayout(
             modifier = Modifier.padding(top = 12.dp),
