b4c8c8f2728e2bede4ec90f19e25e69cbd1489f0
Choi Sang Rok <tkdfhr7497@naver.com>
2023-02-27 22:47:38 +0900

    resolve #247 (#274)
    * refactor: thumbnail들 roundcorner 적용
    
    * refactor: ratio 를 4:3으로 변경

--- feature-ui-home/src/main/kotlin/team/duckie/app/android/feature/ui/home/screen/HomeRecommendScreen.kt
+++ feature-ui-home/src/main/kotlin/team/duckie/app/android/feature/ui/home/screen/HomeRecommendScreen.kt
@@ -25,9 +25,11 @@ import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.pager.HorizontalPager
 import androidx.compose.foundation.pager.rememberPagerState
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
@@ -146,7 +148,8 @@ private fun HomeRecommendJumbotronLayout(
         AsyncImage(
             modifier = Modifier
                 .fillMaxWidth()
-                .aspectRatio(ratio = ThumbnailRatio),
+                .aspectRatio(ratio = ThumbnailRatio)
+                .clip(RoundedCornerShape(8.dp)),
             model = recommendItem.coverUrl,
             contentDescription = null,
             contentScale = ContentScale.FillBounds,
