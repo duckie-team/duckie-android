ab8c8d2cb390c701bce2eca4c4278a317f20b3cd
Choi Sang Rok <tkdfhr7497@naver.com>
2023-02-28 15:07:03 +0900

    resolve #249 (#276)
    * feat: String Builder 함수 제공, 해시태그 추가하는 함수
    
    * refactor: 해시태그 까지 hightlight 하도록 변경
    
    * feat: 임시로 사용할 annotatedText
    
    * refactor: 강조 표시된 부분만 클릭할 수 있도록 변경
    
    * refactor: unused import

--- feature-ui-home/src/main/kotlin/team/duckie/app/android/feature/ui/home/screen/HomeRecommendScreen.kt
+++ feature-ui-home/src/main/kotlin/team/duckie/app/android/feature/ui/home/screen/HomeRecommendScreen.kt
@@ -51,13 +51,14 @@ import team.duckie.app.android.feature.ui.home.viewmodel.state.HomeState
 import team.duckie.app.android.shared.ui.compose.DuckTestCoverItem
 import team.duckie.app.android.shared.ui.compose.DuckTestSmallCover
 import team.duckie.app.android.shared.ui.compose.DuckieHorizontalPagerIndicator
+import team.duckie.app.android.shared.ui.compose.QuackAnnotatedText
 import team.duckie.app.android.util.compose.activityViewModel
+import team.duckie.app.android.util.kotlin.addHashTag
 import team.duckie.quackquack.ui.component.QuackBody1
 import team.duckie.quackquack.ui.component.QuackBody3
 import team.duckie.quackquack.ui.component.QuackLarge1
 import team.duckie.quackquack.ui.component.QuackLargeButton
 import team.duckie.quackquack.ui.component.QuackLargeButtonType
-import team.duckie.quackquack.ui.component.QuackUnderlineHeadLine2
 
 private val HomeHorizontalPadding = PaddingValues(horizontal = 16.dp)
 
@@ -199,13 +200,12 @@ private fun HomeTopicRecommendLayout(
     ) {
         // TODO(limsaehyun): QuackAnnotatedHeadLine2로 교체 필요
         // https://github.com/duckie-team/quack-quack-android/issues/442
-        QuackUnderlineHeadLine2(
+        QuackAnnotatedText(
             modifier = Modifier.padding(HomeHorizontalPadding),
             text = title,
-            underlineTexts = persistentListOf(tag),
-            onClick = {
-                onTagClicked(tag)
-            },
+            highlightTextPairs = persistentListOf(
+                tag.addHashTag() to { onTagClicked(tag) },
+            ),
         )
         Spacer(modifier = Modifier.height(16.dp))
         LazyRow(
