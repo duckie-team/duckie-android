9a8c993fd9fa86e121af06b7716d94ed3907ec36
EvergreenTree97 <tkdfhr7497@naver.com>
2023-03-01 21:55:31 +0900

    feat: domain moel

---
+++ domain/src/main/kotlin/team/duckie/app/android/domain/exam/model/ExamInfo.kt
@@ -0,0 +1,18 @@
+/*
+ * Designed and developed by Duckie Team, 2022
+ *
+ * Licensed under the MIT.
+ * Please see full license: https://github.com/duckie-team/duckie-android/blob/develop/LICENSE
+ */
+
+package team.duckie.app.android.domain.exam.model
+
+import androidx.compose.runtime.Immutable
+
+@Immutable
+data class ExamInfo(
+    val id: Int,
+    val title: String,
+    val thumbnailUrl: String,
+    val solvedCount: Int?,
+)
