2842e30a31642e5164578807b264662ea869309e
EvergreenTree97 <tkdfhr7497@naver.com>
2023-03-01 21:53:25 +0900

    feat : dao 작성

---
+++ data/src/main/kotlin/team/duckie/app/android/data/exam/dao/MadeExamsDao.kt
@@ -0,0 +1,22 @@
+/*
+ * Designed and developed by Duckie Team, 2022
+ *
+ * Licensed under the MIT.
+ * Please see full license: https://github.com/duckie-team/duckie-android/blob/develop/LICENSE
+ */
+
+package team.duckie.app.android.data.exam.dao
+
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import team.duckie.app.android.data._local.BaseDao
+import team.duckie.app.android.data.exam.model.ExamInfoEntity
+
+@Dao
+interface MadeExamsDao: BaseDao<ExamInfoEntity> {
+
+    @Query("SELECT * FROM exam_infos")
+    suspend fun getAll(): List<ExamInfoEntity>
+}
