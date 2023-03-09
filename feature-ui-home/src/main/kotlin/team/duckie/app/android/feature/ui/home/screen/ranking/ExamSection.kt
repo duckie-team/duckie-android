5357e6c387918fec5b460b594878f6cf0708ac41
EvergreenTree97 <tkdfhr7497@naver.com>
2023-03-09 12:34:56 +0900

    feat: 덕력고사 탭 WIP

---
+++ feature-ui-home/src/main/kotlin/team/duckie/app/android/feature/ui/home/screen/ranking/ExamSection.kt
@@ -0,0 +1,16 @@
+/*
+ * Designed and developed by Duckie Team, 2022
+ *
+ * Licensed under the MIT.
+ * Please see full license: https://github.com/duckie-team/duckie-android/blob/develop/LICENSE
+ */
+
+package team.duckie.app.android.feature.ui.home.screen.ranking
+
+import androidx.compose.runtime.Composable
+import team.duckie.quackquack.ui.component.QuackHeadLine2
+
+@Composable
+internal fun ExamSection() {
+    QuackHeadLine2(text = "덕력고사")
+}
