be706bdc326bb92611dfea965193b65cf851b82b
EvergreenTree97 <tkdfhr7497@naver.com>
2023-03-20 17:22:57 +0900

    refactor: gradlew build

--- feature-ui-home/src/main/kotlin/team/duckie/app/android/feature/ui/home/screen/ranking/ExamineeSection.kt
+++ feature-ui-home/src/main/kotlin/team/duckie/app/android/feature/ui/home/screen/ranking/ExamineeSection.kt
@@ -20,7 +20,6 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyListState
 import androidx.compose.material.Divider
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
