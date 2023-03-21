2dbb5b2ca5d46767f54fa351ca273b1f0c143b05
limsaehyun <sh00710000@gmail.com>
2023-03-21 15:25:23 +0900

    chore: setting 에 필요한 dependency 추가

--- feature-ui-setting/build.gradle.kts
+++ feature-ui-setting/build.gradle.kts
@@ -5,11 +5,32 @@
  * Please see full license: https://github.com/duckie-team/duckie-android/blob/develop/LICENSE
  */
 
+import DependencyHandler.Extensions.implementations
+
 plugins {
     id(ConventionEnum.AndroidLibrary)
     id(ConventionEnum.AndroidLibraryCompose)
+    id(ConventionEnum.AndroidHilt)
 }
 
 android {
     namespace = "team.duckie.app.android.feature.setting"
+}
+
+dependencies {
+    implementations(
+        platform(libs.firebase.bom),
+        projects.di,
+        projects.domain,
+        projects.navigator,
+        projects.utilUi,
+        projects.utilKotlin,
+        projects.utilCompose,
+        projects.sharedUiCompose,
+        libs.orbit.viewmodel,
+        libs.orbit.compose,
+        libs.quack.ui.components,
+        libs.compose.lifecycle.runtime,
+        libs.firebase.crashlytics,
+    )
 }
