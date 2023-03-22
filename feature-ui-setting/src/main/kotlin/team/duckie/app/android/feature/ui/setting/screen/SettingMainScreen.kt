456a5fe39e0bb3c9b34e39dac1a06bf399b99f04
limsaehyun <sh00710000@gmail.com>
2023-03-22 09:53:22 +0900

    refactor: SettingContentLayout 재사용 할 수 있도록 분리

--- feature-ui-setting/src/main/kotlin/team/duckie/app/android/feature/ui/setting/screen/SettingMainScreen.kt
+++ feature-ui-setting/src/main/kotlin/team/duckie/app/android/feature/ui/setting/screen/SettingMainScreen.kt
@@ -7,30 +7,22 @@
 
 package team.duckie.app.android.feature.ui.setting.screen
 
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.unit.dp
+import team.duckie.app.android.feature.ui.setting.component.SettingContentLayout
 import team.duckie.app.android.feature.ui.setting.constans.SettingType
 import team.duckie.app.android.feature.ui.setting.viewmodel.SettingViewModel
-import team.duckie.quackquack.ui.color.QuackColor
-import team.duckie.quackquack.ui.component.QuackBody1
-import team.duckie.quackquack.ui.component.QuackTitle2
-import team.duckie.quackquack.ui.modifier.quackClickable
 
 @Composable
 fun SettingMainScreen(
     vm: SettingViewModel,
     version: String,
 ) {
-    val settingItems = SettingType.values()
+    val settingItems = SettingType
+        .values()
+        .filter { it !in listOf(SettingType.Main) }
 
     LazyColumn {
         items(settingItems) { item ->
@@ -40,35 +32,7 @@ fun SettingMainScreen(
                 onClick = {
                     vm.navigateStep(item)
                 },
-            )
-        }
-    }
-}
-
-@Composable
-private fun SettingContentLayout(
-    title: String,
-    trailingText: String? = null,
-    onClick: () -> Unit,
-) {
-    Row(
-        modifier = Modifier
-            .fillMaxWidth()
-            .height(44.dp)
-            .quackClickable {
-                onClick()
-            },
-        verticalAlignment = Alignment.CenterVertically,
-        horizontalArrangement = Arrangement.SpaceBetween,
-    ) {
-        QuackTitle2(
-            text = title,
-            color = QuackColor.Black,
-        )
-        if (trailingText != null) {
-            QuackBody1(
-                text = trailingText,
-                color = QuackColor.Gray1,
+                isBold = true,
             )
         }
     }
