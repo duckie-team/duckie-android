40689b73ee6c2657a04ca156db5e2685d0993ffa
limsaehyun <sh00710000@gmail.com>
2023-03-28 20:05:30 +0900

    style: 기본 프로필 아이콘 추가

---
+++ shared-ui-compose/src/main/kotlin/team/duckie/app/android/shared/ui/compose/DuckieIcon.kt
@@ -0,0 +1,14 @@
+/*
+ * Designed and developed by Duckie Team, 2022
+ *
+ * Licensed under the MIT.
+ * Please see full license: https://github.com/duckie-team/duckie-android/blob/develop/LICENSE
+ */
+
+package team.duckie.app.android.shared.ui.compose
+
+object DuckieIcon {
+
+    /** 기본 프로필 이미지 */
+    val DefaultProfile = R.drawable.ic_default_profile
+}
