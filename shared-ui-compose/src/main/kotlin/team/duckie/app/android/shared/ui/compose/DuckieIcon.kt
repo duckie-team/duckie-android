b4d467d06f0ac24f11744e67e16227d75e23eca6
임세현 <sh00710000@gmail.com>
2023-04-01 22:05:55 +0900

    Resolve #343 (#359)
    * style: 기본 프로필 아이콘 추가
    
    * feat: 기본 프로필 이미지 적용
    
    * fix: QuackIcon을 사용하도록 변경

---
+++ shared-ui-compose/src/main/kotlin/team/duckie/app/android/shared/ui/compose/DuckieIcon.kt
@@ -0,0 +1,12 @@
+/*
+ * Designed and developed by Duckie Team, 2022
+ *
+ * Licensed under the MIT.
+ * Please see full license: https://github.com/duckie-team/duckie-android/blob/develop/LICENSE
+ */
+
+package team.duckie.app.android.shared.ui.compose
+
+import team.duckie.quackquack.ui.icon.QuackIcon
+
+val QuackIcon.Companion.DefaultProfile get() = R.drawable.ic_default_profile
--- shared-ui-compose/src/main/kotlin/team/duckie/app/android/shared/ui/compose/UserFollowingLayout.kt
+++ shared-ui-compose/src/main/kotlin/team/duckie/app/android/shared/ui/compose/UserFollowingLayout.kt
@@ -7,8 +7,7 @@
 
 package team.duckie.app.android.shared.ui.compose
 
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
@@ -16,8 +15,10 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.layout.Layout
 import androidx.compose.ui.layout.layoutId
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.DpSize
 import androidx.compose.ui.unit.dp
@@ -29,6 +30,7 @@ import team.duckie.quackquack.ui.component.QuackBody2
 import team.duckie.quackquack.ui.component.QuackBody3
 import team.duckie.quackquack.ui.component.QuackImage
 import team.duckie.quackquack.ui.component.QuackSubtitle2
+import team.duckie.quackquack.ui.icon.QuackIcon
 import team.duckie.quackquack.ui.modifier.quackClickable
 import team.duckie.quackquack.ui.shape.SquircleShape
 import team.duckie.quackquack.ui.util.DpSize
@@ -78,20 +80,18 @@ fun UserFollowingLayout(
             ),
         content = {
             if (profileImgUrl.isEmpty()) {
-                // TODO(limsaehyun): 추후에 기본 프로필 이미지로 변경해야 함
-                Box(
+                Image(
                     modifier = Modifier
                         .layoutId(UserInfoBlockUserProfileLayoutId)
                         .size(HomeProfileSize)
-                        .background(
-                            color = QuackColor.Gray2.composeColor,
-                            shape = SquircleShape,
-                        )
+                        .clip(SquircleShape)
                         .quackClickable {
                             if (onClickUserProfile != null) {
                                 onClickUserProfile(userId)
                             }
                         },
+                    painter = painterResource(id = QuackIcon.DefaultProfile),
+                    contentDescription = null,
                 )
             } else {
                 QuackImage(
