3b12f7953880319db7de19fe33725f390b115e9d
EvergreenTree97 <tkdfhr7497@naver.com>
2023-04-13 15:28:43 +0900

    feat: onResume에 이벤트가 발생할 수 있도록 변경

---
+++ util-compose/src/main/kotlin/team/duckie/app/android/util/compose/Lifecycle.kt
@@ -0,0 +1,36 @@
+/*
+ * Designed and developed by Duckie Team, 2022
+ *
+ * Licensed under the MIT.
+ * Please see full license: https://github.com/duckie-team/duckie-android/blob/develop/LICENSE
+ */
+
+package team.duckie.app.android.util.compose
+
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.DisposableEffect
+import androidx.compose.ui.platform.LocalLifecycleOwner
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.LifecycleEventObserver
+
+@Composable
+fun LaunchOnLifecycle(
+    lifecycleEvent: Lifecycle.Event = Lifecycle.Event.ON_RESUME,
+    block: () -> Unit,
+) {
+    val lifecycleOwner = LocalLifecycleOwner.current
+    DisposableEffect(lifecycleOwner) {
+        val observer = LifecycleEventObserver { _, event ->
+            if (event == lifecycleEvent) {
+                block()
+            }
+        }
+
+        lifecycleOwner.lifecycle.addObserver(observer)
+
+        onDispose {
+            lifecycleOwner.lifecycle.removeObserver(observer)
+        }
+    }
+
+}
