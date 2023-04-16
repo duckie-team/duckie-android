c046877c51b54830eb00a92a2d6c8c2e900e6cf6
Choi Sang Rok <tkdfhr7497@naver.com>
2023-04-16 22:09:18 +0900

    Release/1.0.1 (#402)
    * chore: version 1.0.1 업그레이드
    
    * chore: versioncode upgrade

--- build-logic/src/main/kotlin/team/duckie/app/android/convention/ApplicationConstants.kt
+++ build-logic/src/main/kotlin/team/duckie/app/android/convention/ApplicationConstants.kt
@@ -16,7 +16,7 @@ internal object ApplicationConstants {
     const val minSdk = 23
     const val targetSdk = 33
     const val compileSdk = 33
-    const val versionCode = 3
+    const val versionCode = 4
     const val versionName = "MVP-1.0.1"
     val javaVersion = JavaVersion.VERSION_17
 }
