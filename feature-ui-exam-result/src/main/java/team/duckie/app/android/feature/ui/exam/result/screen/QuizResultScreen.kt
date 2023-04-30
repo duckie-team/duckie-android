b7e5333528be5ac43dead3f6d55e8e5b84e0e082
EvergreenTree97 <tkdfhr7497@naver.com>
2023-04-30 20:56:01 +0900

    remove: 추후 quizScreen 구현을 위해 제거

--- feature-ui-exam-result/src/main/java/team/duckie/app/android/feature/ui/exam/result/screen/QuizResultScreen.kt
+++ feature-ui-exam-result/src/main/java/team/duckie/app/android/feature/ui/exam/result/screen/QuizResultScreen.kt
@@ -7,113 +7,13 @@
 
 package team.duckie.app.android.feature.ui.exam.result.screen
 
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.heightIn
-import androidx.compose.foundation.layout.navigationBarsPadding
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.statusBarsPadding
-import androidx.compose.material.Scaffold
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.getValue
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.unit.dp
-import androidx.lifecycle.compose.collectAsStateWithLifecycle
-import team.duckie.app.android.feature.ui.exam.result.ExamResultActivity
-import team.duckie.app.android.feature.ui.exam.result.R
 import team.duckie.app.android.feature.ui.exam.result.viewmodel.ExamResultViewModel
-import team.duckie.app.android.shared.ui.compose.quack.QuackCrossfade
 import team.duckie.app.android.util.compose.activityViewModel
-import team.duckie.quackquack.ui.component.QuackImage
-import team.duckie.quackquack.ui.component.QuackSmallButton
-import team.duckie.quackquack.ui.component.QuackSmallButtonType
-import team.duckie.quackquack.ui.component.QuackTopAppBar
-import team.duckie.quackquack.ui.icon.QuackIcon
 
 @Composable
 internal fun QuizResultScreen(
     viewModel: ExamResultViewModel = activityViewModel(),
 ) {
-    val state by viewModel.container.stateFlow.collectAsStateWithLifecycle()
-    val activity = LocalContext.current as ExamResultActivity
-
-    LaunchedEffect(Unit) {
-        viewModel.initState()
-    }
-
-    Scaffold(
-        modifier = Modifier
-            .statusBarsPadding()
-            .navigationBarsPadding(),
-        topBar = {
-            QuackTopAppBar(
-                modifier = Modifier
-                    .padding(vertical = 12.dp)
-                    .padding(horizontal = 16.dp),
-                leadingIcon = QuackIcon.Close,
-                onLeadingIconClick = viewModel::exitExam,
-            )
-        },
-        bottomBar = {
-            Row(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(
-                        horizontal = 20.dp,
-                        vertical = 12.dp,
-                    ),
-                verticalAlignment = Alignment.CenterVertically,
-                horizontalArrangement = Arrangement.spacedBy(space = 8.dp),
-            ) {
-                GrayBorderSmallButton(
-                    modifier = Modifier
-                        .heightIn(min = 44.dp)
-                        .weight(1f),
-                    text = stringResource(id = R.string.solve_retry),
-                    onClick = { // TODO(EvergreenTree97) 해당 시험 본 적 있는지 플래그 생기면 enabled 설정 해야함
-                        viewModel.clickRetry(activity.getString(R.string.feature_prepare))
-                    },
-                )
-                QuackSmallButton(
-                    modifier = Modifier
-                        .heightIn(44.dp)
-                        .weight(1f),
-                    type = QuackSmallButtonType.Fill,
-                    text = stringResource(id = R.string.exit_exam),
-                    enabled = true,
-                    onClick = viewModel::exitExam,
-                )
-            }
-        },
-    ) { padding ->
-        QuackCrossfade(targetState = state.isReportLoading) {
-            when (it) {
-                true -> {
-                    LoadingIndicator()
-                }
-
-                false -> {
-                    Box(
-                        modifier = Modifier
-                            .fillMaxSize()
-                            .padding(padding)
-                            .padding(all = 16.dp),
-                        contentAlignment = Alignment.Center,
-                    ) {
-                        QuackImage(
-                            modifier = Modifier.fillMaxSize(),
-                            src = state.reportUrl,
-                        )
-                    }
-                }
-            }
-        }
-    }
+    
 }
