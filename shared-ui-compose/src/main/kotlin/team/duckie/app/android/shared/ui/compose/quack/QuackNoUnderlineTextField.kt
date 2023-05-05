fca40f4a122e56bc06b6d376614e7f82d97ef483
limsaehyun <sh00710000@gmail.com>
2023-05-05 17:40:24 +0900

    refactor: QuackNoUnderlineTextField 직접 구현
    잘못된 padding 값 수정

--- shared-ui-compose/src/main/kotlin/team/duckie/app/android/shared/ui/compose/quack/QuackNoUnderlineTextField.kt
+++ shared-ui-compose/src/main/kotlin/team/duckie/app/android/shared/ui/compose/quack/QuackNoUnderlineTextField.kt
@@ -7,47 +7,139 @@
 
 package team.duckie.app.android.shared.ui.compose.quack
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.BasicTextField
 import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.SolidColor
 import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.DpSize
 import androidx.compose.ui.unit.dp
-import team.duckie.quackquack.ui.component.QuackBasic2TextField
+import team.duckie.app.android.util.kotlin.runIf
+import team.duckie.quackquack.ui.color.QuackColor
+import team.duckie.quackquack.ui.component.QuackImage
 import team.duckie.quackquack.ui.icon.QuackIcon
+import team.duckie.quackquack.ui.textstyle.QuackTextStyle
 
-/**
- * [QuackBasic2TextField] 를 변형해서 구현한 underline이 없는 TextField
- */
 @Composable
 fun QuackNoUnderlineTextField(
     modifier: Modifier = Modifier,
     text: String,
     onTextChanged: (text: String) -> Unit,
     placeholderText: String? = null,
-    leadingStartPadding: Dp = 0.dp,
-    leadingIcon: QuackIcon? = null,
-    leadingIconOnClick: (() -> Unit)? = null,
     trailingEndPadding: Dp = 0.dp,
     trailingIcon: QuackIcon? = null,
     trailingIconOnClick: (() -> Unit)? = null,
     keyboardOptions: KeyboardOptions = KeyboardOptions(imeAction = ImeAction.Done),
     keyboardActions: KeyboardActions = KeyboardActions(),
 ) {
-    QuackBasic2TextField(
-        modifier = modifier,
-        text = text,
-        onTextChanged = onTextChanged,
-        placeholderText = placeholderText,
-        leadingStartPadding = leadingStartPadding,
-        leadingIcon = leadingIcon,
-        leadingIconOnClick = leadingIconOnClick,
-        trailingEndPadding = trailingEndPadding,
-        trailingIcon = trailingIcon,
-        trailingIconOnClick = trailingIconOnClick,
+    val quackTextFieldColors = QuackColor.DuckieOrange.composeColor
+    val isPlaceholder = text.isEmpty()
+
+    val inputTypography = remember(
+        key1 = isPlaceholder,
+    ) {
+        QuackTextStyle.Subtitle.runIf(
+            condition = isPlaceholder,
+        ) {
+            change(
+                color = QuackColor.Gray2,
+            )
+        }.asComposeStyle()
+    }
+
+    BasicTextField(
+        modifier = modifier
+            .fillMaxWidth()
+            .background(
+                color = QuackColor.White.composeColor,
+            )
+            .padding(
+                paddingValues = PaddingValues(
+                    top = 8.dp,
+                    bottom = 8.dp,
+                    start = 12.dp,
+                ),
+            ),
+        value = text,
+        onValueChange = onTextChanged,
+        textStyle = inputTypography,
         keyboardOptions = keyboardOptions,
         keyboardActions = keyboardActions,
-        showIndicatorLine = false,
+        singleLine = true,
+        cursorBrush = SolidColor(
+            value = quackTextFieldColors,
+        ),
+        decorationBox = { textField ->
+            TextFieldDecoration(
+                textField = textField,
+                isPlaceholder = isPlaceholder,
+                placeholderText = placeholderText,
+                trailingIcon = trailingIcon,
+                trailingEndPadding = trailingEndPadding,
+                trailingIconOnClick = trailingIconOnClick,
+            )
+        },
     )
+}
+
+@Composable
+private fun TextFieldDecoration(
+    textField: @Composable () -> Unit,
+    isPlaceholder: Boolean,
+    placeholderText: String?,
+    trailingIcon: QuackIcon?,
+    trailingEndPadding: Dp = 0.dp,
+    trailingIconOnClick: (() -> Unit)?,
+) {
+    Box(
+        modifier = Modifier.fillMaxWidth(),
+        contentAlignment = Alignment.CenterStart,
+    ) {
+        textField()
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+        ) {
+            if (isPlaceholder && placeholderText != null) {
+                Box(
+                    propagateMinConstraints = true,
+                ) {
+                    Text(
+                        text = placeholderText,
+                        style = QuackTextStyle.Body1.asComposeStyle().copy(
+                            color = QuackColor.Gray2.composeColor,
+                        ),
+                        maxLines = 1,
+                        softWrap = false,
+                        overflow = TextOverflow.Ellipsis,
+                    )
+                }
+            } else {
+                null
+            }
+            Spacer(modifier = Modifier.weight(1f))
+            if (trailingIcon != null) {
+                QuackImage(
+                    src = trailingIcon,
+                    modifier = Modifier.padding(end = trailingEndPadding),
+                    onClick = trailingIconOnClick,
+                    size = DpSize(24.dp, 24.dp),
+                    rippleEnabled = false,
+                )
+            }
+        }
+    }
 }
