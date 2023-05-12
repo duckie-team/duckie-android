e51fd74c14f6b2bf0737805a23d3e1e8eb284290
EvergreenTree97 <tkdfhr7497@naver.com>
2023-05-12 22:13:55 +0900

    feat: time HH:MM:SS 형식으로 보여주는 함수 구현

--- util-kotlin/src/main/kotlin/team/duckie/app/android/util/kotlin/times.kt
+++ util-kotlin/src/main/kotlin/team/duckie/app/android/util/kotlin/times.kt
@@ -21,3 +21,13 @@ inline val Double.seconds: Long get() = (this * 1000).toLong()
  * 주어진 분을 MS 로 반환합니다.
  */
 inline val Int.minutes: Long get() = this.seconds * 60
+
+/**
+ * 주어진 초를 HH:MM:SS 형식 으로 변환합니다.
+ */
+fun Int.toHourMinuteSecond(): String {
+    val hours = this / 3600
+    val minutes = (this % 3600) / 60
+    val seconds = this % 60
+    return "%02d:%02d:%02d".format(hours, minutes, seconds)
+}
