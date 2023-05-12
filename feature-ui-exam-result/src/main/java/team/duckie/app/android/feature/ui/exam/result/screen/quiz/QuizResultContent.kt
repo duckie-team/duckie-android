64e8890c5354567a98bde5883e3695673bf180d5
Choi Sang Rok <tkdfhr7497@naver.com>
2023-05-12 23:27:26 +0900

    덕퀴즈, 시험 결과 화면 분기해서 보여줄 수 있도록 변경 (#476)
    * feat: Exam Result 분기 처리하여 보여주도록 변경
    
    * feat: time HH:MM:SS 형식으로 보여주는 함수 구현
    
    * test: times test 추가
    
    * refactor: 하드코딩 제거
    
    * refactor: ./gradlew build

---
+++ feature-ui-exam-result/src/main/java/team/duckie/app/android/feature/ui/exam/result/screen/quiz/QuizResultContent.kt
@@ -0,0 +1,81 @@
+/*
+ * Designed and developed by Duckie Team, 2022
+ *
+ * Licensed under the MIT.
+ * Please see full license: https://github.com/duckie-team/duckie-android/blob/develop/LICENSE
+ */
+
+package team.duckie.app.android.feature.ui.exam.result.screen.quiz
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import kotlinx.collections.immutable.persistentListOf
+import team.duckie.app.android.feature.ui.exam.result.R
+import team.duckie.app.android.shared.ui.compose.QuackAnnotatedText
+import team.duckie.app.android.shared.ui.compose.Spacer
+import team.duckie.app.android.util.kotlin.toHourMinuteSecond
+import team.duckie.quackquack.ui.component.QuackBody1
+import team.duckie.quackquack.ui.component.QuackDivider
+import team.duckie.quackquack.ui.component.QuackImage
+import team.duckie.quackquack.ui.textstyle.QuackTextStyle
+
+@Composable
+internal fun QuizResultContent(
+    resultImageUrl: String,
+    time: Int,
+    correctProblemCount: Int,
+    mainTag: String,
+    rank: Int,
+) {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(all = 16.dp),
+    ) {
+        QuackImage(
+            modifier = Modifier.fillMaxWidth(),
+            src = resultImageUrl,
+        )
+        Spacer(space = 28.dp)
+        QuackDivider()
+        Spacer(space = 20.dp)
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.SpaceBetween,
+        ) {
+            QuackBody1(text = stringResource(id = R.string.total_time))
+            QuackBody1(text = time.toHourMinuteSecond())
+        }
+        Spacer(space = 8.dp)
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.SpaceBetween,
+        ) {
+            QuackBody1(text = stringResource(id = R.string.correct_problem))
+            QuackBody1(
+                text = stringResource(
+                    id = R.string.correct_problem_unit,
+                    correctProblemCount.toString(),
+                ),
+            )
+        }
+        Spacer(space = 28.dp)
+        QuackAnnotatedText(
+            text = stringResource(id = R.string.rank_by_tag, mainTag, rank.toString()),
+            highlightTextPairs = persistentListOf(
+                mainTag to null,
+                rank.toString() to null,
+            ),
+            style = QuackTextStyle.HeadLine1,
+        )
+        Spacer(space = 38.dp)
+    }
+}
--- util-kotlin/src/main/kotlin/team/duckie/app/android/util/kotlin/times.kt
+++ util-kotlin/src/main/kotlin/team/duckie/app/android/util/kotlin/times.kt
@@ -4,6 +4,7 @@
  * Licensed under the MIT.
  * Please see full license: https://github.com/duckie-team/duckie-android/blob/develop/LICENSE
  */
+@file:Suppress("MagicNumber")
 
 package team.duckie.app.android.util.kotlin
 
@@ -21,3 +22,13 @@ inline val Double.seconds: Long get() = (this * 1000).toLong()
  * 주어진 분을 MS 로 반환합니다.
  */
 inline val Int.minutes: Long get() = this.seconds * 60
+
+/**
+ * 주어진 초를 HH:MM:SS 형식 으로 변환합니다.
+ */
+fun Int.toHourMinuteSecond(): String {
+    val hours = this / 3600
+    val minutes = (this % 3600) / 60
+    val seconds = this % 60
+    return "%02d:%02d:%02d".format(hours, minutes, seconds)
+}
