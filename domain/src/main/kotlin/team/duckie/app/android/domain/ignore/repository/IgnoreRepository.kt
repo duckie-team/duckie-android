44264d4d7f08805f6511303da4db23fbc4763575
limsaehyun <sh00710000@gmail.com>
2023-05-18 11:19:50 +0900

    feat: 차단하기 domain layer 구현

---
+++ domain/src/main/kotlin/team/duckie/app/android/domain/ignore/repository/IgnoreRepository.kt
@@ -0,0 +1,17 @@
+/*
+ * Designed and developed by Duckie Team, 2022
+ *
+ * Licensed under the MIT.
+ * Please see full license: https://github.com/duckie-team/duckie-android/blob/develop/LICENSE
+ */
+
+
+package team.duckie.app.android.domain.ignore.repository
+
+import androidx.compose.runtime.Immutable
+
+@Immutable
+interface IgnoreRepository {
+    suspend fun ignoreUser(targetId: Int)
+    suspend fun deleteIgnoreUser(targetId: Int)
+}
---
+++ domain/src/main/kotlin/team/duckie/app/android/domain/ignore/usecase/CancelUserIgnoreUseCase.kt
@@ -0,0 +1,22 @@
+/*
+ * Designed and developed by Duckie Team, 2022
+ *
+ * Licensed under the MIT.
+ * Please see full license: https://github.com/duckie-team/duckie-android/blob/develop/LICENSE
+ */
+
+package team.duckie.app.android.domain.ignore.usecase
+
+import androidx.compose.runtime.Immutable
+import team.duckie.app.android.domain.ignore.repository.IgnoreRepository
+import javax.inject.Inject
+
+@Immutable
+class CancelUserIgnoreUseCase @Inject constructor(
+    private val ignoreRepository: IgnoreRepository,
+) {
+
+     suspend operator fun invoke(targetId: Int) = runCatching {
+         ignoreRepository.deleteIgnoreUser(targetId)
+     }
+}
