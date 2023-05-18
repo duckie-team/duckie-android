02ff121cb88791b5d914c6ecd6cd34958e1df514
limsaehyun <sh00710000@gmail.com>
2023-05-18 11:19:34 +0900

    feat: 차단하기 data layer 구현

---
+++ data/src/main/kotlin/team/duckie/app/android/data/ignore/datasource/IgnoreRemoteDataSourceImpl.kt
@@ -0,0 +1,45 @@
+/*
+ * Designed and developed by Duckie Team, 2022
+ *
+ * Licensed under the MIT.
+ * Please see full license: https://github.com/duckie-team/duckie-android/blob/develop/LICENSE
+ */
+
+package team.duckie.app.android.data.ignore.datasource
+
+import io.ktor.client.request.delete
+import io.ktor.client.request.post
+import io.ktor.client.request.setBody
+import io.ktor.client.request.url
+import io.ktor.client.statement.bodyAsText
+import team.duckie.app.android.data._datasource.client
+import team.duckie.app.android.data._exception.util.responseCatching
+import team.duckie.app.android.data._util.toStringJsonMap
+import team.duckie.app.android.util.kotlin.exception.duckieResponseFieldNpe
+import javax.inject.Inject
+
+class IgnoreRemoteDataSourceImpl @Inject constructor(): IgnoreRemoteDataSource {
+    override suspend fun ignoreUser(targetId: Int) {
+        val response = client.post {
+            url("/user-block")
+            setBody("targetId" to targetId)
+        }
+
+        return responseCatching(response.status.value, response.bodyAsText()) { body ->
+            val json = body.toStringJsonMap()
+            json["success"]?.toBoolean() ?: duckieResponseFieldNpe("success")
+        }
+    }
+
+    override suspend fun deleteIgnoreUser(targetId: Int) {
+        val response = client.delete {
+            url("/user-block")
+            setBody("targetId" to targetId)
+        }
+
+        return responseCatching(response.status.value, response.bodyAsText()) { body ->
+            val json = body.toStringJsonMap()
+            json["success"]?.toBoolean() ?: duckieResponseFieldNpe("success")
+        }
+    }
+}
