8714bfeb214db618358a66dee678e8a4ae1c6b79
ricky_0_k <ridickle7@gmail.com>
2023-05-21 19:39:05 +0900

    검색, 명예의 전당, 마이페이지 화면 swipeRefresh 기능 추가 (검색은 현재 동작 안됨)

--- feature/home/src/main/kotlin/team/duckie/app/android/feature/home/screen/search/SearchMainScreen.kt
+++ feature/home/src/main/kotlin/team/duckie/app/android/feature/home/screen/search/SearchMainScreen.kt
@@ -5,6 +5,8 @@
  * Please see full license: https://github.com/duckie-team/duckie-android/blob/develop/LICENSE
  */
 
+@file:OptIn(ExperimentalMaterialApi::class)
+
 package team.duckie.app.android.feature.home.screen.search
 
 import androidx.compose.foundation.background
@@ -18,12 +20,23 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.ExperimentalMaterialApi
+import androidx.compose.material.pullrefresh.PullRefreshIndicator
+import androidx.compose.material.pullrefresh.pullRefresh
+import androidx.compose.material.pullrefresh.rememberPullRefreshState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 import org.orbitmvi.orbit.compose.collectAsState
 import team.duckie.app.android.feature.home.R
 import team.duckie.app.android.feature.home.viewmodel.MainViewModel
@@ -48,72 +61,94 @@ internal fun SearchMainScreen(
     vm: MainViewModel = activityViewModel(),
 ) {
     val state = vm.collectAsState().value
+    var isPullRefresh by remember { mutableStateOf(false) }
+    val coroutineScope = rememberCoroutineScope()
 
     LaunchedEffect(Unit) {
         initState(MainScreenType.Search) { vm.fetchPopularTags() }
     }
 
-    Column(
-        modifier = modifier
-            .fillMaxSize()
-            .background(
-                color = QuackColor.White.composeColor,
-            ),
-    ) {
-        Row(
-            modifier = Modifier
-                .fillMaxWidth()
-                .height(48.dp)
-                .padding(
-                    horizontal = 16.dp,
-                    vertical = 6.dp,
-                ),
-            verticalAlignment = Alignment.CenterVertically,
+    val pullRefreshState = rememberPullRefreshState(
+        refreshing = isPullRefresh,
+        onRefresh = {
+            isPullRefresh = true
+            vm.fetchPopularTags()
+            coroutineScope.launch {
+                delay(1000L)
+                isPullRefresh = false
+            }
+        },
+    )
+
+    Box(Modifier.pullRefresh(pullRefreshState)) {
+        Column(
+            modifier = modifier
+                .fillMaxSize()
+                .background(
+                    color = QuackColor.White.composeColor,
+                )
         ) {
-            QuackImage(
-                src = QuackIcon.Search,
-                size = DpSize(24.dp),
-            )
-            Spacer(modifier = Modifier.width(8.dp))
-            Box(
+            Row(
                 modifier = Modifier
                     .fillMaxWidth()
-                    .height(36.dp)
-                    .background(
-                        color = QuackColor.Gray4.composeColor,
-                        shape = RoundedCornerShape(
-                            size = 8.dp,
-                        ),
-                    )
-                    .quackClickable(
-                        rippleEnabled = false,
-                    ) {
-                        vm.navigateToSearch()
-                    }
-                    .padding(start = 12.dp),
-                contentAlignment = Alignment.CenterStart,
+                    .height(48.dp)
+                    .padding(
+                        horizontal = 16.dp,
+                        vertical = 6.dp,
+                    ),
+                verticalAlignment = Alignment.CenterVertically,
             ) {
-                QuackBody1(
-                    text = stringResource(id = R.string.try_search),
-                    color = QuackColor.Gray2,
+                QuackImage(
+                    src = QuackIcon.Search,
+                    size = DpSize(24.dp),
                 )
+                Spacer(modifier = Modifier.width(8.dp))
+                Box(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .height(36.dp)
+                        .background(
+                            color = QuackColor.Gray4.composeColor,
+                            shape = RoundedCornerShape(
+                                size = 8.dp,
+                            ),
+                        )
+                        .quackClickable(
+                            rippleEnabled = false,
+                        ) {
+                            vm.navigateToSearch()
+                        }
+                        .padding(start = 12.dp),
+                    contentAlignment = Alignment.CenterStart,
+                ) {
+                    QuackBody1(
+                        text = stringResource(id = R.string.try_search),
+                        color = QuackColor.Gray2,
+                    )
+                }
             }
+            Spacer(modifier = Modifier.height(16.dp))
+            QuackTitle2(
+                modifier = Modifier.padding(start = SearchScreenHorizontalPaddingDp),
+                text = stringResource(id = R.string.popular_tag),
+            )
+            Spacer(modifier = Modifier.height(8.dp))
+            // TODO(limsaehyun): 추후 꽥꽥에서, 전체 너비만큼 태그 Composable 을 넣을 수 있는 Composable 적용 필요
+            QuackLazyVerticalGridTag(
+                modifier = Modifier.padding(horizontal = SearchScreenHorizontalPaddingDp),
+                items = state.popularTags.map { it.name },
+                tagType = QuackTagType.Round,
+                onClick = { index ->
+                    vm.navigateToSearch(searchTag = state.popularTags[index].name)
+                },
+                itemChunkedSize = 5,
+            )
         }
-        Spacer(modifier = Modifier.height(16.dp))
-        QuackTitle2(
-            modifier = Modifier.padding(start = SearchScreenHorizontalPaddingDp),
-            text = stringResource(id = R.string.popular_tag),
-        )
-        Spacer(modifier = Modifier.height(8.dp))
-        // TODO(limsaehyun): 추후 꽥꽥에서, 전체 너비만큼 태그 Composable 을 넣을 수 있는 Composable 적용 필요
-        QuackLazyVerticalGridTag(
-            modifier = Modifier.padding(horizontal = SearchScreenHorizontalPaddingDp),
-            items = state.popularTags.map { it.name },
-            tagType = QuackTagType.Round,
-            onClick = { index ->
-                vm.navigateToSearch(searchTag = state.popularTags[index].name)
-            },
-            itemChunkedSize = 5,
+
+        PullRefreshIndicator(
+            modifier = Modifier.align(Alignment.TopCenter),
+            refreshing = isPullRefresh,
+            state = pullRefreshState,
         )
     }
 }
