f78167daa936bc8d093ba295016ab7b90613be0b
limsaehyun <sh00710000@gmail.com>
2023-05-28 20:44:26 +0900

    refactor: DuckieFitImage 재사용 할 수 있도록 추상화

--- feature/solve-problem/src/main/kotlin/team/duckie/app/android/feature/solve/problem/question/QuestionSection.kt
+++ feature/solve-problem/src/main/kotlin/team/duckie/app/android/feature/solve/problem/question/QuestionSection.kt
@@ -7,25 +7,15 @@
 
 package team.duckie.app.android.feature.solve.problem.question
 
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.aspectRatio
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyListScope
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.unit.dp
-import team.duckie.app.android.common.compose.CoverImageRatio
+import team.duckie.app.android.common.compose.DuckieFitImage
 import team.duckie.app.android.domain.exam.model.Question
 import team.duckie.app.android.feature.solve.problem.question.audio.AudioPlayer
 import team.duckie.app.android.feature.solve.problem.question.video.VideoPlayer
-import team.duckie.quackquack.material.QuackColor
-import team.duckie.quackquack.ui.QuackImage
 import team.duckie.quackquack.ui.component.QuackHeadLine2
 
 private val HorizontalPadding = PaddingValues(horizontal = 16.dp)
@@ -43,23 +33,9 @@ internal fun LazyListScope.questionSection(
         is Question.Text -> {}
         is Question.Image -> {
             item {
-                Box(
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .padding(horizontal = 16.dp)
-                        .background(
-                            color = QuackColor.Black.value,
-                            shape = RoundedCornerShape(8.dp),
-                        )
-                        .aspectRatio(CoverImageRatio),
-                    contentAlignment = Alignment.Center,
-                ) {
-                    QuackImage(
-                        modifier = Modifier.fillMaxSize(),
-                        src = question.imageUrl,
-                        contentScale = ContentScale.Fit,
-                    )
-                }
+                DuckieFitImage(
+                    imageUrl = question.imageUrl,
+                )
             }
         }
 
