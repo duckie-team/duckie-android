ed8a9a7a28c0739efee079b1070d37e1824e8fe9
EvergreenTree97 <tkdfhr7497@naver.com>
2023-06-01 11:56:15 +0900

    feat: 덕력고사 성적표 화면에 꽉차도록 수정

--- feature/exam-result/src/main/java/team/duckie/app/android/feature/exam/result/screen/exam/ExamResultContent.kt
+++ feature/exam-result/src/main/java/team/duckie/app/android/feature/exam/result/screen/exam/ExamResultContent.kt
@@ -7,28 +7,19 @@
 
 package team.duckie.app.android.feature.exam.result.screen.exam
 
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.dp
-import team.duckie.quackquack.ui.component.QuackImage
+import androidx.compose.ui.layout.ContentScale
+import team.duckie.quackquack.ui.QuackImage
 
 @Composable
 internal fun ExamResultContent(
     resultImageUrl: String,
 ) {
-    Box(
-        modifier = Modifier
-            .fillMaxSize()
-            .padding(all = 16.dp),
-        contentAlignment = Alignment.Center,
-    ) {
-        QuackImage(
-            modifier = Modifier.fillMaxSize(),
-            src = resultImageUrl,
-        )
-    }
+    QuackImage(
+        modifier = Modifier.fillMaxSize(),
+        src = resultImageUrl,
+        contentScale = ContentScale.FillBounds,
+    )
 }
