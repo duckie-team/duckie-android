d3d4d67827f52bc8bf5c5bef6c040e8cb5fd969e
EvergreenTree97 <tkdfhr7497@naver.com>
2023-06-02 15:54:39 +0900

    feat: 덕력고사 정보 보여주기

--- feature/detail/src/main/kotlin/team/duckie/app/android/feature/detail/screen/exam/ExamDetailScreen.kt
+++ feature/detail/src/main/kotlin/team/duckie/app/android/feature/detail/screen/exam/ExamDetailScreen.kt
@@ -9,11 +9,27 @@
 
 package team.duckie.app.android.feature.detail.screen.exam
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import kotlinx.collections.immutable.persistentListOf
+import team.duckie.app.android.common.compose.ui.icon.v2.Paper
+import team.duckie.app.android.common.kotlin.AllowMagicNumber
+import team.duckie.app.android.feature.detail.R
 import team.duckie.app.android.feature.detail.common.DetailContentLayout
 import team.duckie.app.android.feature.detail.viewmodel.state.DetailState
-import team.duckie.app.android.common.kotlin.AllowMagicNumber
+import team.duckie.quackquack.material.QuackIcon
+import team.duckie.quackquack.ui.QuackIcon
+import team.duckie.quackquack.ui.sugar.QuackBody2
+import team.duckie.quackquack.ui.sugar.QuackTitle2
 
 @Composable
 internal fun ExamDetailContentLayout(
@@ -24,6 +40,9 @@ internal fun ExamDetailContentLayout(
     followButtonClick: () -> Unit,
     profileClick: (Int) -> Unit,
 ) {
+    val totalExamCount = remember { (state.exam.problems ?: persistentListOf()).size }
+    val solvedCount = remember { state.exam.solvedCount ?: 0 }
+
     DetailContentLayout(
         modifier = modifier,
         state = state,
@@ -32,7 +51,50 @@ internal fun ExamDetailContentLayout(
         followButtonClick = followButtonClick,
         profileClick = profileClick,
         additionalInfo = {
-            // TODO(EvergreenTree97) 시험 정보 추후 삽입
+            MetadataSection(
+                totalExamCount = totalExamCount,
+                totalExaminee = solvedCount,
+            )
         },
     )
+}
+
+@Composable
+private fun MetadataSection(
+    totalExamCount: Int,
+    totalExaminee: Int,
+) {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(horizontal = 16.dp)
+            .padding(top = 24.dp),
+        verticalArrangement = Arrangement.spacedBy(8.dp),
+    ) {
+        QuackTitle2(text = stringResource(id = R.string.detail_exam_info))
+        IconText(
+            icon = QuackIcon.Paper,
+            text = stringResource(id = R.string.detail_total_exam_count, totalExamCount),
+        )
+        IconText(
+            icon = QuackIcon.Profile,
+            text = stringResource(id = R.string.detail_total_examinee, totalExaminee),
+        )
+    }
+}
+
+@Composable
+private fun IconText(
+    modifier: Modifier = Modifier,
+    icon: QuackIcon,
+    text: String,
+) {
+    Row(
+        modifier = modifier,
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.spacedBy(2.dp),
+    ) {
+        QuackIcon(icon = icon)
+        QuackBody2(text = text)
+    }
 }
