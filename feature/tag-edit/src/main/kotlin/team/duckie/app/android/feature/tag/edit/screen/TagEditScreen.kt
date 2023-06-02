a1679846e108edd70b6b52c357b8ae92fb6409b4
ricky_0_k <ridickle7@gmail.com>
2023-06-03 00:58:31 +0900

    feat: 태그 편집 화면에 DuckieTagAddBottomSheet 적용

--- feature/tag-edit/src/main/kotlin/team/duckie/app/android/feature/tag/edit/screen/TagEditScreen.kt
+++ feature/tag-edit/src/main/kotlin/team/duckie/app/android/feature/tag/edit/screen/TagEditScreen.kt
@@ -5,6 +5,8 @@
  * Please see full license: https://github.com/duckie-team/duckie-android/blob/develop/LICENSE
  */
 
+@file:OptIn(ExperimentalMaterialApi::class)
+
 package team.duckie.app.android.feature.tag.edit.screen
 
 import android.app.Activity
@@ -12,22 +14,27 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.ExperimentalMaterialApi
+import androidx.compose.material.ModalBottomSheetValue
+import androidx.compose.material.rememberModalBottomSheetState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
-import kotlinx.collections.immutable.toImmutableList
 import kotlinx.collections.immutable.toPersistentList
+import kotlinx.coroutines.launch
 import org.orbitmvi.orbit.compose.collectAsState
-import team.duckie.app.android.feature.tag.edit.R
-import team.duckie.app.android.feature.tag.edit.viewmodel.TagEditState
-import team.duckie.app.android.feature.tag.edit.viewmodel.TagEditViewModel
+import team.duckie.app.android.common.compose.activityViewModel
 import team.duckie.app.android.common.compose.ui.ErrorScreen
 import team.duckie.app.android.common.compose.ui.FavoriteTagSection
 import team.duckie.app.android.common.compose.ui.LoadingScreen
-import team.duckie.app.android.common.compose.ui.screen.SearchTagScreen
-import team.duckie.app.android.common.compose.activityViewModel
+import team.duckie.app.android.common.compose.ui.domain.DuckieTagAddBottomSheet
+import team.duckie.app.android.domain.tag.model.Tag
+import team.duckie.app.android.feature.tag.edit.R
+import team.duckie.app.android.feature.tag.edit.viewmodel.TagEditState
+import team.duckie.app.android.feature.tag.edit.viewmodel.TagEditViewModel
 import team.duckie.quackquack.ui.color.QuackColor
 import team.duckie.quackquack.ui.component.QuackSubtitle
 import team.duckie.quackquack.ui.component.QuackTopAppBar
@@ -50,22 +57,9 @@ internal fun TagEditScreen(
             state = state,
             onEditFinishClick = vm::onEditFinishClick,
             onTrailingClick = vm::onTrailingClick,
-            onAddTagClick = vm::onAddTagClick,
-            onTagClick = vm::onTagClick,
-        )
-
-        is TagEditState.AddTag -> SearchTagScreen(
-            modifier = modifier,
-            title = stringResource(id = R.string.add_tag_title),
-            placeholderText = stringResource(id = R.string.add_tag_placeholder),
-            multiSelectMode = false,
-            onCloseClick = vm::onAddFinishClick,
-            onBackPressed = vm::onAddFinishClick,
-            onClickSearchListHeader = vm::onSearchTagHeaderClick,
-            onClickSearchList = { index -> vm.onSearchTagClick(index) },
-            onTextChanged = { newSearchTextValue -> vm.onSearchTextChanged(newSearchTextValue) },
-            onSearchTextValidate = { searchTextValue -> vm.onSearchTextValidate(searchTextValue) },
-            searchResults = state.searchResults.map { it.name }.toImmutableList(),
+            addNewTags = vm::addNewTags,
+            requestAddTag = vm::requestNewTag,
+            onTagClick = vm::onTrailingClick,
         )
 
         is TagEditState.Error -> ErrorScreen(
@@ -81,50 +75,71 @@ fun TagEditSuccessScreen(
     state: TagEditState.Success,
     onEditFinishClick: () -> Unit,
     onTrailingClick: (Int) -> Unit,
-    onAddTagClick: () -> Unit,
+    addNewTags: (List<Tag>) -> Unit,
+    requestAddTag: suspend (String) -> Tag?,
     onTagClick: (Int) -> Unit,
 ) {
     val activity = LocalContext.current as Activity
+    val coroutineScope = rememberCoroutineScope()
+    val sheetState = rememberModalBottomSheetState(
+        ModalBottomSheetValue.Hidden,
+        skipHalfExpanded = true,
+    )
 
-    Column(modifier = modifier) {
-        // 상단 탭바
-        QuackTopAppBar(
-            leadingIcon = QuackIcon.ArrowBack,
-            leadingText = stringResource(R.string.title),
-            onLeadingIconClick = activity::finish,
-            trailingContent = {
-                QuackSubtitle(
-                    modifier = Modifier
-                        .then(Modifier) // prevent Modifier.Companion
-                        .quackClickable(
-                            rippleEnabled = false,
-                            onClick = onEditFinishClick,
+    DuckieTagAddBottomSheet(
+        sheetState = sheetState,
+        onDismissRequest = { newAddedTags, clearAction ->
+            coroutineScope.launch {
+                addNewTags(newAddedTags)
+                clearAction()
+                sheetState.hide()
+            }
+        },
+        requestAddTag = requestAddTag,
+        content = {
+            Column(modifier = modifier) {
+                // 상단 탭바
+                QuackTopAppBar(
+                    leadingIcon = QuackIcon.ArrowBack,
+                    leadingText = stringResource(R.string.title),
+                    onLeadingIconClick = activity::finish,
+                    trailingContent = {
+                        QuackSubtitle(
+                            modifier = Modifier
+                                .then(Modifier) // prevent Modifier.Companion
+                                .quackClickable(
+                                    rippleEnabled = false,
+                                    onClick = onEditFinishClick,
+                                )
+                                .padding(
+                                    vertical = 4.dp,
+                                    horizontal = 16.dp,
+                                ),
+                            text = stringResource(R.string.edit_finish),
+                            color = QuackColor.DuckieOrange,
+                            singleLine = true,
                         )
-                        .padding(
-                            vertical = 4.dp,
-                            horizontal = 16.dp,
-                        ),
-                    text = stringResource(R.string.edit_finish),
-                    color = QuackColor.DuckieOrange,
-                    singleLine = true,
+                    },
                 )
-            },
-        )
 
-        // 내 관심 태그 영역
-        FavoriteTagSection(
-            modifier = Modifier.padding(vertical = 20.dp),
-            title = stringResource(id = R.string.my_favorite_tag),
-            horizontalPadding = PaddingValues(horizontal = 16.dp),
-            verticalArrangement = Arrangement.spacedBy(12.dp),
-            trailingIcon = QuackIcon.Close,
-            onTrailingClick = onTrailingClick,
-            tags = state.myTags.map { it.name }.toPersistentList(),
-            emptySection = {},
-            singleLine = false,
-            onTagClick = onTagClick,
-            addButtonTitle = stringResource(id = R.string.tag_edit_add_favorite_tag),
-            onAddTagClick = onAddTagClick,
-        )
-    }
+                // 내 관심 태그 영역
+                FavoriteTagSection(
+                    modifier = Modifier.padding(vertical = 20.dp),
+                    title = stringResource(id = R.string.my_favorite_tag),
+                    horizontalPadding = PaddingValues(horizontal = 16.dp),
+                    verticalArrangement = Arrangement.spacedBy(12.dp),
+                    trailingIcon = QuackIcon.Close,
+                    onTrailingClick = onTrailingClick,
+                    tags = state.myTags.map { it.name }.toPersistentList(),
+                    emptySection = {},
+                    singleLine = false,
+                    onTagClick = onTagClick,
+                    addButtonTitle = stringResource(id = R.string.tag_edit_add_favorite_tag),
+                    onAddTagClick = {
+                        coroutineScope.launch { sheetState.show() }
+                    },
+                )
+            }
+        },
+    )
 }
--- feature/tag-edit/src/main/kotlin/team/duckie/app/android/feature/tag/edit/viewmodel/TagEditViewModel.kt
+++ feature/tag-edit/src/main/kotlin/team/duckie/app/android/feature/tag/edit/viewmodel/TagEditViewModel.kt
@@ -8,26 +8,16 @@
 package team.duckie.app.android.feature.tag.edit.viewmodel
 
 import androidx.lifecycle.ViewModel
-import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.collections.immutable.ImmutableList
-import kotlinx.collections.immutable.PersistentList
 import kotlinx.collections.immutable.persistentListOf
 import kotlinx.collections.immutable.toImmutableList
 import kotlinx.collections.immutable.toPersistentList
-import kotlinx.coroutines.FlowPreview
-import kotlinx.coroutines.channels.BufferOverflow
-import kotlinx.coroutines.flow.MutableSharedFlow
-import kotlinx.coroutines.flow.collectLatest
-import kotlinx.coroutines.flow.debounce
-import kotlinx.coroutines.launch
 import org.orbitmvi.orbit.ContainerHost
 import org.orbitmvi.orbit.syntax.simple.intent
 import org.orbitmvi.orbit.syntax.simple.postSideEffect
 import org.orbitmvi.orbit.syntax.simple.reduce
 import org.orbitmvi.orbit.viewmodel.container
-import team.duckie.app.android.domain.recommendation.model.SearchType
-import team.duckie.app.android.domain.search.model.Search
 import team.duckie.app.android.domain.search.usecase.GetSearchUseCase
 import team.duckie.app.android.domain.tag.model.Tag
 import team.duckie.app.android.domain.tag.usecase.TagCreateUseCase
@@ -35,8 +25,6 @@ import team.duckie.app.android.domain.user.model.User
 import team.duckie.app.android.domain.user.usecase.GetMeUseCase
 import team.duckie.app.android.domain.user.usecase.SetMeUseCase
 import team.duckie.app.android.domain.user.usecase.UserUpdateUseCase
-import team.duckie.app.android.common.kotlin.fastMap
-import team.duckie.app.android.common.android.ui.const.Debounce
 import javax.inject.Inject
 
 private const val TagsMaximumCount = 10
@@ -56,34 +44,6 @@ internal class TagEditViewModel @Inject constructor(
     private val myTags: ImmutableList<Tag>
         get() = me.favoriteTags?.toImmutableList() ?: persistentListOf()
 
-    /** tags 검색 flow. 실질 동작 로직은 apply 내에 명세되어 있다. */
-    @OptIn(FlowPreview::class)
-    private val _getSearchTagsFlow: MutableSharedFlow<String> = MutableSharedFlow<String>(
-        replay = 0,
-        extraBufferCapacity = 1,
-        onBufferOverflow = BufferOverflow.DROP_OLDEST,
-    ).apply {
-        viewModelScope.launch {
-            this@apply.debounce(team.duckie.app.android.common.android.ui.const.Debounce.SearchSecond).collectLatest { query ->
-                intent {
-                    getSearchUseCase(query = query, page = 1, type = SearchType.Tags)
-                        .onSuccess {
-                            val searchResults = (it as Search.TagSearch).tags
-                                .take(TagsMaximumCount)
-                                .toImmutableList()
-
-                            reduce {
-                                TagEditState.AddTag(searchResults = searchResults)
-                            }
-                        }
-                        .onFailure {
-                            postSideEffect(TagEditSideEffect.ReportError(it))
-                        }
-                }
-            }
-        }
-    }
-
     fun initState() = intent {
         getMeUseCase()
             .onSuccess { meResponse ->
@@ -96,88 +56,43 @@ internal class TagEditViewModel @Inject constructor(
     }
 
     /** 수정완료 버튼 누를 시 동작 */
-    fun onEditFinishClick() {
-        intent { postSideEffect(TagEditSideEffect.FinishTagEdit(true)) }
-    }
-
-    /** + 직접 태그 추가하기 버튼 클릭 시 동작 */
-    fun onAddTagClick() {
-        intent {
-            reduce { TagEditState.AddTag() }
-        }
-    }
+    fun onEditFinishClick() = intent { updateMe() }
 
     /** 각 태그 항목의 x 버튼 클릭 시 동작 */
-    fun onTrailingClick(index: Int) = viewModelScope.launch {
+    fun onTrailingClick(index: Int) = intent {
         val newTags = myTags.toMutableList().apply { removeAt(index) }.toPersistentList()
-        updateMe(newTags)
+        reduce { TagEditState.Success(myTags = newTags) }
     }
 
-    /**
-     * 태그 편집 화면에서, 각 태그 항목 클릭 시 동작
-     * // TODO(riflockle7): 본래는 [onTrailingClick] 에 들어갈 내용이며 추후 개선 필요
-     */
-    fun onTagClick(index: Int) = viewModelScope.launch {
-        val newTags = myTags.toMutableList().apply { removeAt(index) }.toPersistentList()
-        updateMe(newTags)
+    /** 바텀 시트에서 오른쪽 방향 화살표를 눌러 태그 추가를 완료한다. */
+    fun addNewTags(newTag: List<Tag>) {
+        val newTags = myTags.toMutableList().apply { addAll(newTag) }.toPersistentList()
+        intent { reduce { TagEditState.Success(myTags = newTags) } }
     }
 
-    /** 태그 추가 화면 종료 시 동작 */
-    fun onAddFinishClick() {
-        intent { reduce { TagEditState.Success(myTags) } }
-    }
-
-    /** 태그 추가 화면에서, 검색된 항목 클릭 시 동작 */
-    fun onSearchTagHeaderClick(tag: String) = viewModelScope.launch {
-        require(container.stateFlow.value is TagEditState.AddTag)
-        tagCreateUseCase(tag)
-            .onSuccess { tag -> addMyTag(tag) }
-            .onFailure {
-                intent { postSideEffect(TagEditSideEffect.ReportError(it)) }
-            }
-    }
-
-    /** 태그 추가 화면에서, 검색된 항목 클릭 시 동작 */
-    fun onSearchTagClick(index: Int) = viewModelScope.launch {
-        require(container.stateFlow.value is TagEditState.AddTag)
-        val searchResults = (container.stateFlow.value as TagEditState.AddTag).searchResults
-        addMyTag(searchResults[index])
-    }
-
-    /** 태그 추가 화면에서, 텍스트 내용 변경 시 동작 */
-    fun onSearchTextChanged(newSearchTextValue: String) = viewModelScope.launch {
-        _getSearchTagsFlow.emit(newSearchTextValue)
-    }
-
-    /** 태그 추가 화면에서, 텍스트 유효성 체크 */
-    fun onSearchTextValidate(searchTextValue: String): Boolean {
-        val state = container.stateFlow.value
-        require(state is TagEditState.AddTag)
-
-        return searchTextValue.isNotEmpty() &&
-                !state.searchResults.fastMap { it.name }.contains(searchTextValue)
-    }
-
-    /** 태그 추가 화면에서, 태그 추가한 뒤 화면을 종료한다. */
-    private suspend fun addMyTag(newTag: Tag) {
-        val newTags = myTags.toMutableList().apply { add(newTag) }.toPersistentList()
-        updateMe(newTags)
+    /** 새로운 태그를 요청한다. */
+    suspend fun requestNewTag(tag: String): Tag? {
+        require(container.stateFlow.value is TagEditState.Success)
+        return tagCreateUseCase(tag).getOrNull()
     }
 
     /** 태그 추가 화면에서, 태그 추가한 새로운 me 정보를 갱신하고 [TagEditViewModel] 내 정보 또한 갱신한다. */
-    private suspend fun updateMe(newTags: PersistentList<Tag>) {
+    private suspend fun updateMe() {
+        val tagSuccessState = container.stateFlow.value;
+        require(tagSuccessState is TagEditState.Success)
+
         userUpdateUseCase(
             id = me.id,
             profileImageUrl = null,
             categories = null,
-            tags = newTags,
+            tags = tagSuccessState.myTags,
             status = null,
             nickname = null,
             introduction = null,
         ).onSuccess { newMe ->
             setMeUseCase(newMe)
             me = newMe
-            intent { reduce { TagEditState.Success(myTags) } }
+            intent { postSideEffect(TagEditSideEffect.FinishTagEdit(true)) }
         }.onFailure {
             intent { postSideEffect(TagEditSideEffect.ReportError(it)) }
         }
