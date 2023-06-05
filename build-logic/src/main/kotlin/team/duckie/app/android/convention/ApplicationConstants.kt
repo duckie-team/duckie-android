b97e3fa14bebc0dbe619ca98ca95c22e73d699d4
ricky_0_k <ridickle7@gmail.com>
2023-06-05 13:27:49 +0000

    veresionCode 12 으로 올리기

--- build-logic/src/main/kotlin/team/duckie/app/android/convention/ApplicationConstants.kt
+++ build-logic/src/main/kotlin/team/duckie/app/android/convention/ApplicationConstants.kt
@@ -16,7 +16,7 @@ internal object ApplicationConstants {
     const val minSdk = 23
     const val targetSdk = 33
     const val compileSdk = 33
-    const val versionCode = 11
+    const val versionCode = 12
     const val versionName = "1.0.1"
     val javaVersion = JavaVersion.VERSION_17
 }
