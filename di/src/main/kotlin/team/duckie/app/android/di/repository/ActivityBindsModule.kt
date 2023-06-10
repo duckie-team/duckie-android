63ef2c08288d821f0a6d8b58c8f8814a13e27b99
EvergreenTree97 <tkdfhr7497@naver.com>
2023-06-10 13:02:37 +0900

    refactor: activity에서 kakaousecase를 주입할 수 있는 형태로 변경

--- di/src/main/kotlin/team/duckie/app/android/di/repository/ActivityBindsModule.kt
+++ di/src/main/kotlin/team/duckie/app/android/di/repository/ActivityBindsModule.kt
@@ -5,23 +5,19 @@
  * Please see full license: https://github.com/duckie-team/duckie-android/blob/develop/LICENSE
  */
 
-@file:Suppress("unused")
-
 package team.duckie.app.android.di.repository
 
-import android.app.Activity
+import dagger.Binds
 import dagger.Module
-import dagger.Provides
 import dagger.hilt.InstallIn
-import dagger.hilt.components.SingletonComponent
+import dagger.hilt.android.components.ActivityComponent
 import team.duckie.app.android.data.kakao.repository.KakaoRepositoryImpl
 import team.duckie.app.android.domain.kakao.repository.KakaoRepository
 
 @Module
-@InstallIn(SingletonComponent::class)
-object ProvidesModule {
-    @Provides
-    fun provideKakaoRepository(activityContext: Activity): KakaoRepository {
-        return KakaoRepositoryImpl(activityContext)
-    }
+@InstallIn(ActivityComponent::class)
+abstract class ActivityBindsModule {
+
+    @Binds
+    abstract fun bindsKakaoRepository(repository: KakaoRepositoryImpl): KakaoRepository
 }
