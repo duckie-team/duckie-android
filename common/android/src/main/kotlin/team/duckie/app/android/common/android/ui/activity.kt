c418f0c36f3ca1fbb21415aca74453152a359344
limsaehyun <sh00710000@gmail.com>
2023-06-25 15:26:41 +0900

    feat: startLauncherWithAnimation extension

--- common/android/src/main/kotlin/team/duckie/app/android/common/android/ui/activity.kt
+++ common/android/src/main/kotlin/team/duckie/app/android/common/android/ui/activity.kt
@@ -11,6 +11,7 @@ package team.duckie.app.android.common.android.ui
 
 import android.app.Activity
 import android.content.Intent
+import androidx.activity.result.ActivityResultLauncher
 
 /**
  * 기존 액티비티를 fade out 과 함께 닫고, 새로운 액티비티를 fade in 과 함께 시작합니다.
@@ -37,6 +38,24 @@ inline fun <reified T : Activity> Activity.startActivityWithAnimation(
     withFinish: Boolean = false,
 ) {
     startActivity(intentBuilder(Intent(this, T::class.java)))
+    overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out)
+    if (withFinish) finish()
+}
+
+/**
+ * Fade in 과 함께 새로운 Launcher 를 시작합니다.
+ *
+ *  * @param intentBuilder 새로 시작할 액티비티의 [Intent]
+ *  * @param withFinish 현재 액티비티를 종료할지 여부
+ */
+inline fun <reified T : Activity> Activity.startLauncherWithAnimation(
+    launcher: ActivityResultLauncher<Intent>,
+    intentBuilder: Intent.() -> Intent = { this },
+    withFinish: Boolean = false,
+) {
+    launcher.launch(
+        intentBuilder(Intent(this, T::class.java)),
+    )
     overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out)
     if (withFinish) finish()
 }
