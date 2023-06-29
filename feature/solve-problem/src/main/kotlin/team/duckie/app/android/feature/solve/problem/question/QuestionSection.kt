bf73951c7d05d5c9284f792b319663aa8127cbd1
EvergreenTree97 <tkdfhr7497@naver.com>
2023-06-29 17:22:14 +0900

    feat: 문제 풀기쪽 LazyColumn에서 Column 으로 변경

--- feature/solve-problem/src/main/kotlin/team/duckie/app/android/feature/solve/problem/question/QuestionSection.kt
+++ feature/solve-problem/src/main/kotlin/team/duckie/app/android/feature/solve/problem/question/QuestionSection.kt
@@ -7,51 +7,47 @@
 
 package team.duckie.app.android.feature.solve.problem.question
 
+import androidx.compose.foundation.layout.ColumnScope
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.lazy.LazyListScope
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
-import team.duckie.app.android.common.compose.DuckieFitImage
 import team.duckie.app.android.domain.exam.model.Question
 import team.duckie.app.android.feature.solve.problem.question.audio.AudioPlayer
+import team.duckie.app.android.feature.solve.problem.question.image.ImageBox
 import team.duckie.app.android.feature.solve.problem.question.video.VideoPlayer
 import team.duckie.quackquack.ui.component.QuackHeadLine2
 
 private val HorizontalPadding = PaddingValues(horizontal = 16.dp)
-internal fun LazyListScope.questionSection(
+
+@Composable
+internal fun ColumnScope.QuestionSection(
     page: Int,
     question: Question,
 ) {
-    item {
-        QuackHeadLine2(
-            text = "${page + 1}. ${question.text}",
-            padding = HorizontalPadding,
-        )
-    }
+    QuackHeadLine2(
+        text = "${page + 1}. ${question.text}",
+        padding = HorizontalPadding,
+    )
     when (question) {
         is Question.Text -> {}
         is Question.Image -> {
-            item {
-                DuckieFitImage(
-                    imageUrl = question.imageUrl,
-                )
-            }
+            ImageBox(
+                modifier = Modifier.weight(1f),
+                url = question.imageUrl,
+            )
         }
 
         is Question.Audio -> {
-            item {
-                AudioPlayer(
-                    modifier = Modifier.padding(paddingValues = HorizontalPadding),
-                    url = question.audioUrl,
-                )
-            }
+            AudioPlayer(
+                modifier = Modifier.padding(paddingValues = HorizontalPadding),
+                url = question.audioUrl,
+            )
         }
 
         is Question.Video -> {
-            item {
-                VideoPlayer(url = question.videoUrl)
-            }
+            VideoPlayer(url = question.videoUrl)
         }
     }
 }
