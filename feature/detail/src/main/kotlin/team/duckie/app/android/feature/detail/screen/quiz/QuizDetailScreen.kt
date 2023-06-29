d05e43b150301f4505c4ab929e6a93a0a873e789
limsaehyun <sh00710000@gmail.com>
2023-06-29 11:11:10 +0900

    feat: 덕퀴즈 내 기록 UI WIP

--- feature/detail/src/main/kotlin/team/duckie/app/android/feature/detail/screen/quiz/QuizDetailScreen.kt
+++ feature/detail/src/main/kotlin/team/duckie/app/android/feature/detail/screen/quiz/QuizDetailScreen.kt
@@ -9,9 +9,11 @@ package team.duckie.app.android.feature.detail.screen.quiz
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.ColumnScope
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.widthIn
@@ -21,6 +23,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.input.key.Key
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.DpSize
@@ -28,6 +31,7 @@ import androidx.compose.ui.unit.dp
 import team.duckie.app.android.common.compose.ui.Spacer
 import team.duckie.app.android.common.kotlin.fastForEachIndexed
 import team.duckie.app.android.domain.quiz.model.QuizInfo
+import team.duckie.app.android.domain.user.model.User
 import team.duckie.app.android.feature.detail.R
 import team.duckie.app.android.feature.detail.common.DetailContentLayout
 import team.duckie.app.android.feature.detail.viewmodel.state.DetailState
@@ -38,6 +42,9 @@ import team.duckie.quackquack.material.quackClickable
 import team.duckie.quackquack.material.shape.SquircleShape
 import team.duckie.quackquack.ui.QuackImage
 import team.duckie.quackquack.ui.QuackText
+import team.duckie.quackquack.ui.sugar.QuackBody1
+import team.duckie.quackquack.ui.sugar.QuackBody2
+import team.duckie.quackquack.ui.sugar.QuackTitle2
 
 @Composable
 internal fun QuizDetailContentLayout(
@@ -67,6 +74,36 @@ internal fun QuizDetailContentLayout(
 }
 
 @Composable
+private fun ColumnScope.MyRankingSection(
+    quizInfo: QuizInfo,
+) {
+    val user = quizInfo.user
+
+    QuackBody2(text = "내 기록")
+    Spacer(space = 4.dp)
+    Row(
+        modifier = Modifier.height(68.dp),
+        verticalAlignment = Alignment.CenterVertically,
+    ) {
+        QuackTitle2(text = "-")
+        Spacer(space = 12.dp)
+        UserProfileOrDefault(profileImageUrl = user.profileImageUrl)
+        Spacer(space = 8.dp)
+        Column {
+            QuackTitle2(text = user.nickname)
+            QuackText(
+                text = buildString {
+                    append(stringResource(id = R.string.score, quizInfo.score))
+                    append(" / ")
+                    append(stringResource(id = R.string.time, quizInfo.time))
+                },
+                typography = QuackTypography.Body1.change(color = QuackColor.Gray1),
+            )
+        }
+    }
+}
+
+@Composable
 private fun RankingSection(
     state: DetailState.Success,
     userContentClick: (Int) -> Unit,
@@ -82,6 +119,8 @@ private fun RankingSection(
             text = stringResource(id = R.string.quiz_ranking_title, state.mainTagNames),
             typography = QuackTypography.Title2,
         )
+        
+        MyRankingSection(quizInfo = )
 
         Spacer(space = 8.dp)
 
@@ -93,6 +132,29 @@ private fun RankingSection(
             )
         }
     }
+}
+
+@Composable
+fun UserProfileOrDefault(
+    size: DpSize = DpSize(44.dp, 44.dp),
+    profileImageUrl: String?,
+
+    ) {
+    profileImageUrl?.let {
+        QuackImage(
+            src = it,
+            modifier = Modifier
+                .size(size)
+                .clip(SquircleShape),
+            contentScale = ContentScale.FillBounds,
+        )
+    } ?: QuackImage(
+        src = QuackIcon.Profile.drawableId,
+        modifier = Modifier
+            .size(size)
+            .clip(SquircleShape),
+        contentScale = ContentScale.FillBounds,
+    )
 }
 
 @Composable
@@ -143,31 +205,9 @@ private fun RankingContent(
                         typography = QuackTypography.Subtitle.change(textColor),
                     )
                 }
-
                 Spacer(space = 12.dp)
-
-                user.profileImageUrl?.let {
-                    QuackImage(
-                        src = it,
-                        modifier = Modifier
-                            .size(
-                                DpSize(44.dp, 44.dp),
-                            )
-                            .clip(SquircleShape),
-                        contentScale = ContentScale.FillBounds,
-                    )
-                } ?: QuackImage(
-                    src = QuackIcon.Profile.drawableId,
-                    modifier = Modifier
-                        .size(
-                            DpSize(44.dp, 44.dp),
-                        )
-                        .clip(SquircleShape),
-                    contentScale = ContentScale.FillBounds,
-                )
-
+                UserProfileOrDefault(profileImageUrl = user.profileImageUrl)
                 Spacer(space = 8.dp)
-
                 QuackText(
                     text = user.nickname,
                     typography = QuackTypography.Title2.change(textColor),
