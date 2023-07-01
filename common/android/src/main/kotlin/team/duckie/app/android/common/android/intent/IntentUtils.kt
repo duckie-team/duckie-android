f177b0753423d4b2b5800abe8c41bac61dd4f948
ricky_0_k <ridickle7@gmail.com>
2023-07-02 00:45:08 +0900

    refactor: getAppPackageName() Composable 사용하지 않는 함수 형태로 재구현 & goToMarket 로직 개선

--- common/android/src/main/kotlin/team/duckie/app/android/common/android/intent/IntentUtils.kt
+++ common/android/src/main/kotlin/team/duckie/app/android/common/android/intent/IntentUtils.kt
@@ -12,8 +12,9 @@ import android.content.ActivityNotFoundException
 import android.content.Intent
 import android.net.Uri
 import android.util.Log
+import team.duckie.app.android.common.android.constant.getAppPackageName
 
-fun Activity.goToMarket(appPackageName: String) {
+fun Activity.goToMarket(appPackageName: String = getAppPackageName()) {
     // TODO(riflockle7): 추후 다른 곳에서 가져오도록 개선 필요
     try {
         startActivity(Intent(Intent.ACTION_VIEW, Uri.parse("market://details?id=$appPackageName")))
