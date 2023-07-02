8c15774fbb49d2ddecf3cd9d88b7fbaec4833ae0
limsaehyun <sh00710000@gmail.com>
2023-07-02 15:30:02 +0900

    feat: 랭킹 구별 extension

---
+++ common/kotlin/src/main/kotlin/team/duckie/app/android/common/kotlin/rank.kt
@@ -0,0 +1,14 @@
+/*
+ * Designed and developed by Duckie Team, 2022
+ *
+ * Licensed under the MIT.
+ * Please see full license: https://github.com/duckie-team/duckie-android/blob/develop/LICENSE
+ */
+
+package team.duckie.app.android.common.kotlin
+
+fun Int.isFirstRanked() =
+    this == 1
+
+fun Int.isTopRanked() =
+    this <= 10
