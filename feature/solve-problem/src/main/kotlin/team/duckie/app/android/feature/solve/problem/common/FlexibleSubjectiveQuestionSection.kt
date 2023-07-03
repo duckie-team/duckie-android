c0da5dd290d1ca08bedfe860caad09b942c9b722
limsaehyun <sh00710000@gmail.com>
2023-07-03 20:17:06 +0900

    feat: FlexibleImage WIP

--- feature/solve-problem/src/main/kotlin/team/duckie/app/android/feature/solve/problem/common/FlexibleSubjectiveQuestionSection.kt
+++ feature/solve-problem/src/main/kotlin/team/duckie/app/android/feature/solve/problem/common/FlexibleSubjectiveQuestionSection.kt
@@ -9,33 +9,52 @@
 
 package team.duckie.app.android.feature.solve.problem.common
 
+import androidx.compose.animation.core.animateDpAsState
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.BoxWithConstraints
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.imePadding
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.layout.size
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
+import androidx.compose.runtime.NonRestartableComposable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalConfiguration
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.platform.SoftwareKeyboardController
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
+import coil.compose.AsyncImage
+import coil.compose.rememberAsyncImagePainter
+import coil.request.ImageRequest
+import coil.transform.RoundedCornersTransformation
 import kotlinx.collections.immutable.ImmutableList
-import team.duckie.app.android.common.compose.CoverImageRatio
-import team.duckie.app.android.common.compose.composedWithKeyboardVisibility
 import team.duckie.app.android.common.compose.ui.Spacer
 import team.duckie.app.android.domain.exam.model.Problem
 import team.duckie.app.android.domain.exam.model.Question
 import team.duckie.app.android.feature.solve.problem.answer.shortanswer.ShortAnswerForm
 import team.duckie.app.android.feature.solve.problem.viewmodel.state.InputAnswer
-import team.duckie.quackquack.material.QuackColor
-import team.duckie.quackquack.ui.QuackImage
 import team.duckie.quackquack.ui.sugar.QuackHeadLine2
+
+@NonRestartableComposable
+@Composable
+private fun getFlexibleImageHeight(): Dp {
+    val configuration = LocalConfiguration.current
+    return ((configuration.screenWidthDp / 4) * 3).dp
+}
+
+private val FlexiableImageMargin: Dp = 96.dp
 
 /**
  * 키보드 상태에 따라서 이미지의 높이가 유동적으로 변하는 주관식 문제를 위한 섹션
@@ -50,6 +69,21 @@ fun FlexibleSubjectiveQuestionSection(
     keyboardController: SoftwareKeyboardController?,
 ) {
     val question = problem.question as Question.Image
+    val flexibleImageHeight = getFlexibleImageHeight()
+    val context = LocalContext.current
+
+    val cornerSizePx = with(LocalDensity.current) { 8.dp.toPx() }
+
+    val request = remember(key1 = question.imageUrl) {
+        ImageRequest.Builder(context)
+            .data(question.imageUrl)
+            .transformations(RoundedCornersTransformation(cornerSizePx))
+    }
+
+    val imagePainter = rememberAsyncImagePainter(
+        model = request.build(),
+        contentScale = ContentScale.Fit,
+    )
 
     Column(
         modifier = Modifier
@@ -62,28 +96,28 @@ fun FlexibleSubjectiveQuestionSection(
             text = "${pageIndex + 1}. ${question.text}",
         )
         Spacer(space = 12.dp)
-        Box(
-            modifier = Modifier
-                .composedWithKeyboardVisibility(
-                    whenKeyboardVisible = { weight(1f) },
-                    whenKeyboardHidden = {
-                        fillMaxWidth()
-                        aspectRatio(CoverImageRatio)
-                    },
-                )
-                .padding(horizontal = 16.dp)
-                .background(
-                    color = QuackColor.Black.value,
-                    shape = RoundedCornerShape(8.dp),
-                ),
-            contentAlignment = Alignment.Center,
-        ) {
-            QuackImage(
-                modifier = Modifier.fillMaxSize(),
-                src = question.imageUrl,
-                contentScale = ContentScale.Fit,
+        BoxWithConstraints {
+            val actualWidth = animateDpAsState(
+                targetValue = if (maxHeight >= flexibleImageHeight) flexibleImageHeight else maxHeight - FlexiableImageMargin,
+                label = "",
             )
+
+            Box(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .heightIn(max = actualWidth.value)
+                    .padding(horizontal = 16.dp)
+
+            ) {
+                Image(
+                    modifier = Modifier
+                        .fillMaxSize(),
+                    painter = imagePainter,
+                    contentDescription = null
+                )
+            }
         }
+
         Spacer(space = 24.dp)
         ShortAnswerForm(
             modifier = Modifier.padding(horizontal = 16.dp),
@@ -100,4 +134,5 @@ fun FlexibleSubjectiveQuestionSection(
         )
         Spacer(space = 16.dp)
     }
+
 }
