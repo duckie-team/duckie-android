fb956da1a1786e92d879a8cef2f8ba30c5b3534a
limsaehyun <sh00710000@gmail.com>
2023-07-08 20:46:45 +0900

    feat: ClipboardHelper

---
+++ common/android/src/main/kotlin/team/duckie/app/android/common/android/deeplink/ClipboardHelper.kt
@@ -0,0 +1,21 @@
+/*
+ * Designed and developed by Duckie Team, 2022
+ *
+ * Licensed under the MIT.
+ * Please see full license: https://github.com/duckie-team/duckie-android/blob/develop/LICENSE
+ */
+
+package team.duckie.app.android.common.android.deeplink
+
+import android.content.ClipData
+import android.content.ClipboardManager
+import android.content.Context
+
+object ClipboardHelper {
+
+    fun copyToClipboard(context: Context, label: String, text: String) {
+        val clipboard = context.getSystemService(Context.CLIPBOARD_SERVICE) as ClipboardManager
+        val clip = ClipData.newPlainText(label, text)
+        clipboard.setPrimaryClip(clip)
+    }
+}
