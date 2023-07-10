2bb362d378f24d98c337f71ccff1d8d43335022b
limsaehyun <sh00710000@gmail.com>
2023-07-10 19:51:31 +0900

    fix: android 9.0 WRITE_EXTERNAL_STORAGE permission issue

---
+++ common/android/src/main/kotlin/team/duckie/app/android/common/android/image/save.kt
@@ -0,0 +1,72 @@
+/*
+ * Designed and developed by Duckie Team, 2022
+ *
+ * Licensed under the MIT.
+ * Please see full license: https://github.com/duckie-team/duckie-android/blob/develop/LICENSE
+ */
+
+package team.duckie.app.android.common.android.image
+
+import android.content.ContentValues
+import android.content.Context
+import android.graphics.Bitmap
+import android.net.Uri
+import android.os.Environment
+import android.provider.MediaStore
+import java.io.File
+import java.io.File.separator
+import java.io.FileOutputStream
+import java.io.OutputStream
+
+fun saveImageInGallery(bitmap: Bitmap, context: Context, folderName: String) {
+    if (android.os.Build.VERSION.SDK_INT >= 29) {
+        val values = contentValues()
+        values.put(MediaStore.Images.Media.RELATIVE_PATH, "Pictures/$folderName")
+        values.put(MediaStore.Images.Media.IS_PENDING, true)
+        // RELATIVE_PATH and IS_PENDING are introduced in API 29.
+
+        val uri: Uri? =
+            context.contentResolver.insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, values)
+        if (uri != null) {
+            saveImageToStream(bitmap, context.contentResolver.openOutputStream(uri))
+            values.put(MediaStore.Images.Media.IS_PENDING, false)
+            context.contentResolver.update(uri, values, null, null)
+        }
+    } else {
+        val directory =
+            File(Environment.getExternalStorageDirectory().toString() + separator + folderName)
+        // getExternalStorageDirectory is deprecated in API 29
+
+        if (!directory.exists()) {
+            directory.mkdirs()
+        }
+        val fileName = System.currentTimeMillis().toString() + ".png"
+        val file = File(directory, fileName)
+        saveImageToStream(bitmap, FileOutputStream(file))
+        if (file.absolutePath != null) {
+            val values = contentValues()
+            values.put(MediaStore.Images.Media.DATA, file.absolutePath)
+            // .DATA is deprecated in API 29
+            context.contentResolver.insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, values)
+        }
+    }
+}
+
+private fun contentValues(): ContentValues {
+    val values = ContentValues()
+    values.put(MediaStore.Images.Media.MIME_TYPE, "image/png")
+    values.put(MediaStore.Images.Media.DATE_ADDED, System.currentTimeMillis() / 1000)
+    values.put(MediaStore.Images.Media.DATE_TAKEN, System.currentTimeMillis())
+    return values
+}
+
+private fun saveImageToStream(bitmap: Bitmap, outputStream: OutputStream?) {
+    if (outputStream != null) {
+        try {
+            bitmap.compress(Bitmap.CompressFormat.PNG, 100, outputStream)
+            outputStream.close()
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
+    }
+}
