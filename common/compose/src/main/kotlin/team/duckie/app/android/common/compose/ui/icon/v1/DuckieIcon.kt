c9e7ece286bc5ed6013c7717c37d101719ed4751
ricky_0_k <ridickle7@gmail.com>
2023-07-12 01:19:52 +0900

    chore: QuackIcon 클래스명 명확화

--- common/compose/src/main/kotlin/team/duckie/app/android/common/compose/ui/icon/v1/DuckieIcon.kt
+++ common/compose/src/main/kotlin/team/duckie/app/android/common/compose/ui/icon/v1/DuckieIcon.kt
@@ -8,13 +8,13 @@
 package team.duckie.app.android.common.compose.ui.icon.v1
 
 import team.duckie.app.android.common.compose.R
-import team.duckie.quackquack.ui.icon.QuackIcon
+import team.duckie.quackquack.ui.icon.QuackIcon as QuackV1Icon
 
-val QuackIcon.Companion.DefaultProfile get() = R.drawable.ic_default_profile
-val QuackIcon.Companion.Notice get() = R.drawable.ic_notice_24
+val QuackV1Icon.Companion.DefaultProfile get() = R.drawable.ic_default_profile
+val QuackV1Icon.Companion.Notice get() = R.drawable.ic_notice_24
 
-val QuackIcon.Companion.Create get() = R.drawable.ic_create_24
+val QuackV1Icon.Companion.Create get() = R.drawable.ic_create_24
 
-val QuackIcon.Companion.Crown get() = R.drawable.ic_crown_12
+val QuackV1Icon.Companion.Crown get() = R.drawable.ic_crown_12
 
-val QuackIcon.Companion.Clock get() = R.drawable.ic_clock_12
+val QuackV1Icon.Companion.Clock get() = R.drawable.ic_clock_12
