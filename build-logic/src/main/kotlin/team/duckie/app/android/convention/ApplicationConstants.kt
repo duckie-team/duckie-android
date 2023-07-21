86663ab1f7d99f1ac4c09dc0941de12d558dfa65
limsaehyun <sh00710000@gmail.com>
2023-07-21 16:33:02 +0900

    chore: bump versionCode 19 to 25

--- build-logic/src/main/kotlin/team/duckie/app/android/convention/ApplicationConstants.kt
+++ build-logic/src/main/kotlin/team/duckie/app/android/convention/ApplicationConstants.kt
@@ -16,7 +16,7 @@ internal object ApplicationConstants {
     const val minSdk = 23
     const val targetSdk = 33
     const val compileSdk = 33
-    const val versionCode = 23
-    const val versionName = "1.2.0"
+    const val versionCode = 25
+    const val versionName = "1.1.4"
     val javaVersion = JavaVersion.VERSION_17
 }
