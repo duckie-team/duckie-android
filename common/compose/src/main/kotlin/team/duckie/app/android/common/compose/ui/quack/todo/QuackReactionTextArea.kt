f84df917d00b21c2e2358b6e3fd3d766980cf510
limsaehyun <sh00710000@gmail.com>
2023-07-23 20:21:17 +0900

    refactor: QuackReactionTextArea 분리

---
+++ common/compose/src/main/kotlin/team/duckie/app/android/common/compose/ui/quack/todo/QuackReactionTextArea.kt
@@ -0,0 +1,113 @@
+/*
+ * Designed and developed by Duckie Team, 2022
+ *
+ * Licensed under the MIT.
+ * Please see full license: https://github.com/duckie-team/duckie-android/blob/develop/LICENSE
+ */
+
+package team.duckie.app.android.common.compose.ui.quack.todo
+
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.Layout
+import androidx.compose.ui.layout.MeasurePolicy
+import androidx.compose.ui.layout.layoutId
+import androidx.compose.ui.unit.dp
+import team.duckie.app.android.common.compose.asLoose
+import team.duckie.app.android.common.compose.getPlaceable
+import team.duckie.quackquack.material.QuackColor
+import team.duckie.quackquack.material.QuackTypography
+import team.duckie.quackquack.ui.QuackText
+import team.duckie.quackquack.ui.component.QuackReviewTextArea
+
+private const val RANKER_REACTION_MAX_LENGTH: Int = 40
+private const val RANKER_REACTION_PLACE_HOLDER: String = "소감을 적어주세요! 랭킹에 소감을 띄워드려요 :)"
+
+private object QuackReactionTextAreaLayoutId {
+    const val QuizReviewPlaceHolder = "QuizReviewPlaceHolder"
+    const val QuizReviewTextArea = "QuizReviewTextArea"
+    const val ReactionLimitText = "ReactionLimitText"
+}
+
+private fun getQuackReactionTextAreaMeasurePolicy(
+    placeholderVisible: Boolean = true,
+) = MeasurePolicy { measurables, constraints ->
+    val looseConstraints = constraints.asLoose()
+    val extraLooseConstraints = constraints.asLoose(width = true)
+
+    with(QuackReactionTextAreaLayoutId) {
+        val quizReviewPlaceHolderPlaceable =
+            measurables.getPlaceable(QuizReviewPlaceHolder, looseConstraints)
+        val quizReviewTextAreaPlaceable =
+            measurables.getPlaceable(QuizReviewTextArea, extraLooseConstraints)
+        val reactionLimitTextPlaceable =
+            measurables.getPlaceable(ReactionLimitText, looseConstraints)
+
+        layout(
+            width = constraints.maxWidth,
+            height = constraints.maxHeight,
+        ) {
+            quizReviewTextAreaPlaceable.place(x = 0, y = 0)
+            if (placeholderVisible) {
+                quizReviewPlaceHolderPlaceable.place(x = 0, y = 0)
+            }
+            reactionLimitTextPlaceable.place(
+                x = constraints.maxWidth - reactionLimitTextPlaceable.width,
+                y = quizReviewTextAreaPlaceable.height - reactionLimitTextPlaceable.height,
+            )
+        }
+    }
+}
+
+@Composable
+fun QuackReactionTextArea(
+    reaction: String,
+    onReactionChanged: (String) -> Unit,
+    maxLength: Int = RANKER_REACTION_MAX_LENGTH,
+) = with(QuackReactionTextAreaLayoutId) {
+    Layout(
+        modifier = Modifier.height(100.dp),
+        measurePolicy = getQuackReactionTextAreaMeasurePolicy(
+            placeholderVisible = reaction.isEmpty(),
+        ),
+        content = {
+            QuackReviewTextArea(
+                modifier = Modifier
+                    .layoutId(QuizReviewTextArea)
+                    .fillMaxHeight(),
+                text = reaction,
+                onTextChanged = { str ->
+                    if (str.length <= maxLength) {
+                        onReactionChanged(str)
+                    }
+                },
+                placeholderText = "",
+                focused = true,
+            )
+            QuackText(
+                modifier = Modifier
+                    .layoutId(QuizReviewPlaceHolder)
+                    .padding(all = 16.dp),
+                text = RANKER_REACTION_PLACE_HOLDER,
+                typography = QuackTypography.Body1.change(
+                    color = QuackColor.Gray2,
+                ),
+            )
+            QuackText(
+                modifier = Modifier
+                    .layoutId(ReactionLimitText)
+                    .padding(
+                        bottom = 12.dp,
+                        end = 12.dp,
+                    ),
+                text = "${reaction.length} / $RANKER_REACTION_MAX_LENGTH",
+                typography = QuackTypography.Body1.change(
+                    color = QuackColor.Gray2,
+                ),
+            )
+        },
+    )
+}
--- feature/exam-result/src/main/java/team/duckie/app/android/feature/exam/result/screen/ExamResultScreen.kt
+++ feature/exam-result/src/main/java/team/duckie/app/android/feature/exam/result/screen/ExamResultScreen.kt
@@ -10,9 +10,7 @@ package team.duckie.app.android.feature.exam.result.screen
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.imePadding
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.systemBarsPadding
@@ -22,21 +20,17 @@ import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.layout.Layout
-import androidx.compose.ui.layout.MeasurePolicy
-import androidx.compose.ui.layout.layoutId
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.SpanStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import team.duckie.app.android.common.compose.activityViewModel
-import team.duckie.app.android.common.compose.asLoose
-import team.duckie.app.android.common.compose.getPlaceable
 import team.duckie.app.android.common.compose.ui.ErrorScreen
 import team.duckie.app.android.common.compose.ui.LoadingScreen
 import team.duckie.app.android.common.compose.ui.Spacer
 import team.duckie.app.android.common.compose.ui.dialog.DuckieDialog
+import team.duckie.app.android.common.compose.ui.quack.todo.QuackReactionTextArea
 import team.duckie.app.android.feature.exam.result.R
 import team.duckie.app.android.feature.exam.result.common.ResultBottomBar
 import team.duckie.app.android.feature.exam.result.screen.exam.ExamResultContent
@@ -45,15 +39,11 @@ import team.duckie.app.android.feature.exam.result.viewmodel.ExamResultScreen
 import team.duckie.app.android.feature.exam.result.viewmodel.ExamResultState
 import team.duckie.app.android.feature.exam.result.viewmodel.ExamResultViewModel
 import team.duckie.quackquack.material.QuackColor
-import team.duckie.quackquack.material.QuackTypography
-import team.duckie.quackquack.ui.QuackText
-import team.duckie.quackquack.ui.component.QuackReviewTextArea
 import team.duckie.quackquack.ui.component.QuackTopAppBar
 import team.duckie.quackquack.ui.icon.QuackIcon
 import team.duckie.quackquack.ui.span
 import team.duckie.quackquack.ui.sugar.QuackHeadLine1
 
-private const val RANKER_REACTION_MAX_LENGTH: Int = 40
 internal const val RANKER_THRESHOLD = 10
 
 @Composable
@@ -168,101 +158,11 @@ private fun ExamResultReactionDialogInternal(
             ),
         )
         Spacer(space = 12.dp)
-        QuizResultReactionTextArea(
-            ranking = ranking,
+        QuackReactionTextArea(
             reaction = reaction,
             onReactionChanged = onReactionChanged,
         )
     }
-}
-
-private object QuizResultReactionTextAreaLayoutId {
-    const val QuizReviewPlaceHolder = "QuizReviewPlaceHolder"
-    const val QuizReviewTextArea = "QuizReviewTextArea"
-    const val ReactionLimitText = "ReactionLimitText"
-}
-
-private fun getQuizResultReactionMeasurePolicy(
-    placeholderVisible: Boolean = true,
-) = MeasurePolicy { measurables, constraints ->
-    val looseConstraints = constraints.asLoose()
-    val extraLooseConstraints = constraints.asLoose(width = true)
-
-    with(QuizResultReactionTextAreaLayoutId) {
-        val quizReviewPlaceHolderPlaceable =
-            measurables.getPlaceable(QuizReviewPlaceHolder, looseConstraints)
-        val quizReviewTextAreaPlaceable =
-            measurables.getPlaceable(QuizReviewTextArea, extraLooseConstraints)
-        val reactionLimitTextPlaceable =
-            measurables.getPlaceable(ReactionLimitText, looseConstraints)
-
-        layout(
-            width = constraints.maxWidth,
-            height = constraints.maxHeight,
-        ) {
-            quizReviewTextAreaPlaceable.place(x = 0, y = 0)
-            if (placeholderVisible) {
-                quizReviewPlaceHolderPlaceable.place(x = 0, y = 0)
-            }
-            reactionLimitTextPlaceable.place(
-                x = constraints.maxWidth - reactionLimitTextPlaceable.width,
-                y = quizReviewTextAreaPlaceable.height - reactionLimitTextPlaceable.height,
-            )
-        }
-    }
-}
-
-@Composable
-private fun QuizResultReactionTextArea(
-    reaction: String,
-    ranking: Int,
-    onReactionChanged: (String) -> Unit,
-) = with(QuizResultReactionTextAreaLayoutId) {
-    Layout(
-        modifier = Modifier.height(100.dp),
-        measurePolicy = getQuizResultReactionMeasurePolicy(
-            placeholderVisible = reaction.isEmpty(),
-        ),
-        content = {
-            QuackReviewTextArea(
-                modifier = Modifier
-                    .layoutId(QuizReviewTextArea)
-                    .fillMaxHeight(),
-                text = reaction,
-                onTextChanged = { str ->
-                    if (str.length <= RANKER_REACTION_MAX_LENGTH) {
-                        onReactionChanged(str)
-                    }
-                },
-                placeholderText = "",
-                focused = true,
-            )
-            QuackText(
-                modifier = Modifier
-                    .layoutId(QuizReviewPlaceHolder)
-                    .padding(all = 16.dp),
-                text = stringResource(
-                    id = R.string.exam_result_ranked_text_area_hint,
-                    ranking,
-                ),
-                typography = QuackTypography.Body1.change(
-                    color = QuackColor.Gray2,
-                ),
-            )
-            QuackText(
-                modifier = Modifier
-                    .layoutId(ReactionLimitText)
-                    .padding(
-                        bottom = 12.dp,
-                        end = 12.dp,
-                    ),
-                text = "${reaction.length} / $RANKER_REACTION_MAX_LENGTH",
-                typography = QuackTypography.Body1.change(
-                    color = QuackColor.Gray2,
-                ),
-            )
-        },
-    )
 }
 
 @Composable
