d9ddd4ac30f3b78e17c7945fcbe262d95fb6f2d5
limsaehyun <sh00710000@gmail.com>
2023-07-30 20:20:17 +0900

    feat: 오답 댓글쓰기 ViewModel 구현

--- feature/exam-result/src/main/java/team/duckie/app/android/feature/exam/result/viewmodel/ExamResultState.kt
+++ feature/exam-result/src/main/java/team/duckie/app/android/feature/exam/result/viewmodel/ExamResultState.kt
@@ -7,7 +7,10 @@
 
 package team.duckie.app.android.feature.exam.result.viewmodel
 
+import android.annotation.SuppressLint
+import kotlinx.collections.immutable.toImmutableList
 import team.duckie.app.android.common.kotlin.isTopRanked
+import java.time.LocalDateTime
 
 enum class ExamResultScreen {
     EXAM_RESULT,
@@ -43,8 +46,44 @@ sealed class ExamResultState {
 
         // dialog visible
         val isReactionValid: Boolean = ranking.isTopRanked() && isBestRecord,
+
+        // wrong answer
+        val myAnswer: String = "",
+        val profileImg: String = "",
+        val equalAnswerCount: Int = 0,
+        val wrongComment: String = "",
+        val otherComments: kotlinx.collections.immutable.ImmutableList<WrongAnswerComment> =
+            (0..3).map { // TODO(limsaehyun) for using UI test, delete it before deploy
+                WrongAnswerComment.dummy()
+            }.toImmutableList(),
+
     ) : ExamResultState() {
         val percent: Double = (ranking.toDouble() / solvedCount.toDouble()) * 100
+
+        data class WrongAnswerComment(
+            val id: Int,
+            val profileImg: String,
+            val nickname: String,
+            val createAt: LocalDateTime,
+            val answer: String,
+            val comment: String,
+            val isLike: Boolean = false,
+            val likeCnt: Int,
+        ) {
+            companion object {
+                @SuppressLint("NewApi")
+                fun dummy() = WrongAnswerComment(
+                    id = 0,
+                    profileImg = "https://duckie-resource.s3.ap-northeast-2.amazonaws.com/profile/1676192267474",
+                    nickname = "세현",
+                    createAt = LocalDateTime.now(),
+                    answer = "맹구",
+                    comment = "아니 이건 아니지 ㅋㅋㅋ",
+                    isLike = true,
+                    likeCnt = 8,
+                )
+            }
+        }
     }
 
     data class Error(
