3e42b5d60316789227cd94bdd76c9cdd57bc43d6
임세현 <sh00710000@gmail.com>
2023-08-13 21:00:23 +0900

    Refactor/friends migrate quack v2 (#618)
    * chore: quack-v1 의존성 제거 및 quack-v2 의존성 추가
    
    * refactor: friend quack v2 마이그레이션

--- feature/friends/build.gradle.kts
+++ feature/friends/build.gradle.kts
@@ -30,6 +30,7 @@ dependencies {
         libs.ktx.lifecycle.runtime,
         libs.compose.ui.material, // needs for Scaffold
         libs.compose.lifecycle.runtime,
-        libs.quack.ui.components,
+        libs.quack.v2.ui,
+        libs.kotlin.collections.immutable,
     )
 }
