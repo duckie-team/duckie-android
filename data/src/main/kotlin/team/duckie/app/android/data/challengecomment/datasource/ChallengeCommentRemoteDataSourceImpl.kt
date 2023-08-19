3d57cb0eb6ea930efef0c710dcf3ea5ca0b9a92c
limsaehyun <sh00710000@gmail.com>
2023-08-19 23:44:53 +0900

    feat: 오답 댓글 종아요 비즈니스 로직 작업

--- data/src/main/kotlin/team/duckie/app/android/data/challengecomment/datasource/ChallengeCommentRemoteDataSourceImpl.kt
+++ data/src/main/kotlin/team/duckie/app/android/data/challengecomment/datasource/ChallengeCommentRemoteDataSourceImpl.kt
@@ -16,11 +16,16 @@ package team.duckie.app.android.data.challengecomment.datasource
 
 import io.ktor.client.request.get
 import io.ktor.client.request.parameter
+import io.ktor.client.request.post
+import io.ktor.client.statement.bodyAsText
+import team.duckie.app.android.common.kotlin.exception.duckieResponseFieldNpe
 import team.duckie.app.android.data._datasource.client
 import team.duckie.app.android.data._exception.util.responseCatching
+import team.duckie.app.android.data._util.jsonBody
+import team.duckie.app.android.data._util.toStringJsonMap
+import team.duckie.app.android.data.challengecomment.mapper.toDomain
 import team.duckie.app.android.data.challengecomment.model.CommentOrderTypeData
 import team.duckie.app.android.data.challengecomment.model.GetChallengeCommentListResponse
-import team.duckie.app.android.data.challengecomment.mapper.toDomain
 import team.duckie.app.android.domain.challengecomment.model.ChallengeCommentList
 import javax.inject.Inject
 
@@ -40,5 +45,31 @@ class ChallengeCommentRemoteDataSourceImpl @Inject constructor() :
             response = response,
             parse = GetChallengeCommentListResponse::toDomain,
         )
+    }
+
+    override suspend fun postChallengeCommentHeart(commentId: Int): Int {
+        val response = client.post("/challenge-comment-hearts") {
+            jsonBody {
+                "challengeCommentId" to commentId
+            }
+        }
+        return responseCatching(
+            statusCode = response.status.value,
+            response = response.bodyAsText(),
+        ) { body ->
+            val json = body.toStringJsonMap()
+            json["id"]?.toInt() ?: duckieResponseFieldNpe("id")
+        }
+    }
+
+    override suspend fun deleteChallengeCommentHeart(commentId: Int): Boolean {
+        val response = client.post("/challenge-comment-hearts/${commentId}")
+        return responseCatching(
+            statusCode = response.status.value,
+            response = response.bodyAsText(),
+        ) { body ->
+            val json = body.toStringJsonMap()
+            json["success"]?.toBoolean() ?: duckieResponseFieldNpe("success")
+        }
     }
 }
