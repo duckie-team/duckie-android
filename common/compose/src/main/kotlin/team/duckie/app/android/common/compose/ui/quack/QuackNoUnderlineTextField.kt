f9b67a4613ac1a7af0c6e68e857efb45120f2310
limsaehyun <sh00710000@gmail.com>
2023-08-27 22:47:47 +0900

    fix: change QuackNoUnderlineTextField measurePolicy

--- common/compose/src/main/kotlin/team/duckie/app/android/common/compose/ui/quack/QuackNoUnderlineTextField.kt
+++ common/compose/src/main/kotlin/team/duckie/app/android/common/compose/ui/quack/QuackNoUnderlineTextField.kt
@@ -12,8 +12,6 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
@@ -24,24 +22,36 @@ import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.SolidColor
+import androidx.compose.ui.layout.Layout
+import androidx.compose.ui.layout.MeasurePolicy
+import androidx.compose.ui.layout.layoutId
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.DpSize
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.offset
+import team.duckie.app.android.common.compose.centerVertical
+import team.duckie.app.android.common.compose.centerVerticalWithMaxHeight
+import team.duckie.app.android.common.compose.heightOrZero
 import team.duckie.app.android.common.compose.ui.icon.v1.ArrowSendId
+import team.duckie.app.android.common.compose.ui.quack.TextFieldDecorationLayoutId.LeadingId
+import team.duckie.app.android.common.compose.ui.quack.TextFieldDecorationLayoutId.PlaceholderId
+import team.duckie.app.android.common.compose.ui.quack.TextFieldDecorationLayoutId.TextFieldId
+import team.duckie.app.android.common.compose.ui.quack.TextFieldDecorationLayoutId.TrailingId
+import team.duckie.app.android.common.compose.widthOrZero
+import team.duckie.app.android.common.kotlin.fastFirstOrNull
 import team.duckie.app.android.common.kotlin.runIf
 import team.duckie.quackquack.material.QuackColor
 import team.duckie.quackquack.material.QuackTypography
 import team.duckie.quackquack.material.icon.QuackIcon
 import team.duckie.quackquack.material.quackClickable
-import team.duckie.quackquack.ui.QuackIcon
 import team.duckie.quackquack.ui.QuackImage
 
 @Composable
@@ -60,6 +70,7 @@ fun QuackNoUnderlineTextField(
     @DrawableRes trailingIcon: Int? = null,
     trailingIconSize: Dp = 24.dp,
     trailingIconOnClick: (() -> Unit)? = null,
+    trailingIconTint: Color = Color.Unspecified,
     keyboardOptions: KeyboardOptions = KeyboardOptions(imeAction = ImeAction.Done),
     keyboardActions: KeyboardActions = KeyboardActions(),
 ) {
@@ -102,12 +113,20 @@ fun QuackNoUnderlineTextField(
                 leadingIcon = leadingIcon,
                 trailingIcon = trailingIcon,
                 startPadding = startPadding,
-                trailingEndPadding = trailingEndPadding,
+                trailingStartPadding = trailingEndPadding,
                 trailingIconOnClick = trailingIconOnClick,
                 trailingIconSize = trailingIconSize,
+                trailingIconTint = trailingIconTint,
             )
         },
     )
+}
+
+private object TextFieldDecorationLayoutId {
+    const val TextFieldId = "TextField"
+    const val PlaceholderId = "Hint"
+    const val LeadingId = "Leading"
+    const val TrailingId = "Trailing"
 }
 
 @Composable
@@ -119,69 +138,122 @@ private fun TextFieldDecoration(
     leadingIcon: Int?,
     trailingIcon: Int?,
     startPadding: Dp = 0.dp,
-    trailingEndPadding: Dp = 0.dp,
+    leadingEndPadding: Dp = 0.dp,
+    trailingStartPadding: Dp = 16.dp,
     trailingIconSize: Dp = 24.dp,
     trailingIconOnClick: (() -> Unit)?,
-) {
-    Box(
+    trailingIconTint: Color = Color.Unspecified,
+) = with(TextFieldDecorationLayoutId) {
+    Layout(
         modifier = Modifier.fillMaxWidth(),
-        contentAlignment = Alignment.CenterStart,
-    ) {
-        textField()
-        Row(
-            verticalAlignment = Alignment.CenterVertically,
-        ) {
+        content = {
             if (leadingIcon != null) {
                 QuackImage(
                     modifier = Modifier
+                        .layoutId(LeadingId)
                         .size(DpSize(16.dp, 16.dp))
-                        .padding(end = trailingEndPadding)
+                        .padding(end = leadingEndPadding)
                         .quackClickable(
                             onClick = trailingIconOnClick,
                             rippleEnabled = false,
-                        ),
+                        )
+                        .padding(end = 16.dp),
                     src = leadingIcon,
                 )
             }
-
             if (isPlaceholder && placeholderText != null) {
-                Box(
-                    propagateMinConstraints = true,
-                ) {
-                    Text(
-                        modifier = Modifier.padding(start = startPadding),
-                        text = placeholderText,
-                        style = QuackTypography.Body1.asComposeStyle().copy(
-                            color = QuackColor.Gray2.value,
-                        ),
-                        maxLines = 1,
-                        softWrap = false,
-                        overflow = TextOverflow.Ellipsis,
-                    )
-                }
-            } else {
-                null
+                Text(
+                    modifier = Modifier
+                        .layoutId(PlaceholderId)
+                        .padding(start = startPadding),
+                    text = placeholderText,
+                    style = QuackTypography.Body1.asComposeStyle().copy(
+                        color = QuackColor.Gray2.value,
+                    ),
+                    maxLines = 1,
+                    softWrap = false,
+                    overflow = TextOverflow.Ellipsis,
+                )
             }
-            Spacer(modifier = Modifier.weight(1f))
+            Box(
+                modifier = Modifier.layoutId(TextFieldId)
+            ) {
+                textField()
+            }
             if (trailingIcon != null) {
                 QuackImage(
                     modifier = Modifier
+                        .layoutId(TrailingId)
                         .quackClickable(
                             onClick = trailingIconOnClick,
                             rippleEnabled = false,
                         )
                         .size(trailingIconSize)
-                        .padding(end = trailingEndPadding),
+                        .padding(start = trailingStartPadding),
                     src = trailingIcon,
+                    tint = QuackColor(trailingIconTint), // TODO(limsaehuyn): conflict 방지용, 타입 자체를 QuackColor로 변경 요망
                 )
             }
-        }
+        },
+        measurePolicy = remember { quackNoUnderlineTextFieldMeasurePolicy() },
+    )
+}
+
+private fun quackNoUnderlineTextFieldMeasurePolicy() = MeasurePolicy { measurables, constraints ->
+    val textConstraints = constraints.copy(minHeight = 0)
+    val relaxedConstraints = constraints.copy(minWidth = 0, minHeight = 0)
+
+    val layoutWidth = constraints.maxWidth
+
+    val trailingPlaceable = measurables.fastFirstOrNull { it.layoutId == TrailingId }
+        ?.measure(relaxedConstraints)
+    val leadingPlaceable =
+        measurables.fastFirstOrNull { it.layoutId == LeadingId }?.measure(constraints)
+
+    val placeHolderPlaceable = measurables.fastFirstOrNull { it.layoutId == PlaceholderId }
+        ?.measure(textConstraints)
+
+    val leadingWidth = leadingPlaceable.widthOrZero()
+    val trailingWidth = trailingPlaceable.widthOrZero()
+    val occupiedSpaceHorizontally = leadingWidth + trailingWidth
+
+    val textFieldPlaceable = measurables.fastFirstOrNull { it.layoutId == TextFieldId }
+        ?.measure(
+            textConstraints.copy(minHeight = 0).offset(horizontal = -occupiedSpaceHorizontally)
+        )
+
+    val maxOfHeight = maxOf(
+        trailingPlaceable.heightOrZero(),
+        leadingPlaceable.heightOrZero(),
+        placeHolderPlaceable.heightOrZero(),
+        textFieldPlaceable.heightOrZero()
+    )
+
+    layout(layoutWidth, maxOfHeight) {
+        leadingPlaceable?.placeRelative(
+            0,
+            maxOfHeight.centerVertical(leadingPlaceable.heightOrZero())
+        )
+        trailingPlaceable?.placeRelative(
+            layoutWidth - trailingWidth,
+            maxOfHeight.centerVertical(trailingPlaceable.heightOrZero())
+        )
+        textFieldPlaceable?.placeRelative(
+            leadingWidth,
+            maxOfHeight.centerVertical(textFieldPlaceable.heightOrZero())
+        )
+        placeHolderPlaceable?.placeRelative(
+            leadingWidth,
+            maxOfHeight.centerVertical(placeHolderPlaceable.heightOrZero())
+        )
     }
 }
+
 
 @Preview
 @Composable
 fun PreviewQuackNoUnderlineTextField() {
+    val (value, onValueChanged) = remember { mutableStateOf("") }
     Column(
         modifier = Modifier
             .fillMaxSize()
@@ -191,11 +263,11 @@ fun PreviewQuackNoUnderlineTextField() {
             modifier = Modifier
                 .fillMaxWidth()
                 .height(52.dp),
-            text = "텍스트",
-            onTextChanged = {},
+            text = value,
+            onTextChanged = onValueChanged,
             placeholderText = "플레이스홀더",
 //        leadingIcon = QuackIcon.Search,
-        trailingIcon = QuackIcon.ArrowSendId,
+            trailingIcon = QuackIcon.ArrowSendId,
             trailingIconOnClick = {},
             paddingValues = PaddingValues(
                 vertical = 16.dp,
@@ -205,8 +277,8 @@ fun PreviewQuackNoUnderlineTextField() {
             modifier = Modifier
                 .fillMaxWidth()
                 .background(QuackColor.Gray4.value),
-            text = "댓글을 남겨보세요!",
-            onTextChanged = { },
+            text = value,
+            onTextChanged = onValueChanged,
             placeholderText = "댓글을 남겨보세요!",
             trailingIcon = QuackIcon.ArrowSendId,
             trailingIconOnClick = { },
