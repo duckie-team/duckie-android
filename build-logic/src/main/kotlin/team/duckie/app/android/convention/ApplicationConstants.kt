b886d6eb7fad9d686f33ac01ade25fcf15dc82b3
EvergreenTree97 <tkdfhr7497@naver.com>
2023-09-05 16:44:16 +0900

    chore: upgrade versioncode

--- build-logic/src/main/kotlin/team/duckie/app/android/convention/ApplicationConstants.kt
+++ build-logic/src/main/kotlin/team/duckie/app/android/convention/ApplicationConstants.kt
@@ -16,7 +16,7 @@ internal object ApplicationConstants {
     const val minSdk = 23
     const val targetSdk = 34
     const val compileSdk = 34
-    const val versionCode = 25
+    const val versionCode = 26
     const val versionName = "1.3.0"
     val javaVersion = JavaVersion.VERSION_17
 }
