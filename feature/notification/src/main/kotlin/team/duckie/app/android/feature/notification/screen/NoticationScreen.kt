d0d35a9e2cf56a8c70efe8aad6b87ac81e091cc0
EvergreenTree97 <tkdfhr7497@naver.com>
2023-09-07 23:34:59 +0900

    refactor: 알림 화면 구현 준비중으로 대체

--- feature/notification/src/main/kotlin/team/duckie/app/android/feature/notification/screen/NoticationScreen.kt
+++ feature/notification/src/main/kotlin/team/duckie/app/android/feature/notification/screen/NoticationScreen.kt
@@ -12,30 +12,14 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.statusBarsPadding
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
-import androidx.lifecycle.compose.collectAsStateWithLifecycle
-import team.duckie.app.android.common.compose.activityViewModel
-import team.duckie.app.android.common.compose.ui.ErrorScreen
 import team.duckie.app.android.common.compose.ui.NoItemScreen
 import team.duckie.app.android.common.compose.ui.Spacer
-import team.duckie.app.android.common.compose.ui.quack.QuackCrossfade
-import team.duckie.app.android.common.compose.ui.quack.todo.QuackTopAppBar
 import team.duckie.app.android.common.compose.ui.skeleton
-import team.duckie.app.android.feature.notification.R
-import team.duckie.app.android.feature.notification.viewmodel.NotificationViewModel
-import team.duckie.quackquack.material.icon.QuackIcon
-import team.duckie.quackquack.material.icon.quackicon.Outlined
-import team.duckie.quackquack.material.icon.quackicon.outlined.ArrowBack
 import team.duckie.quackquack.material.quackClickable
 import team.duckie.quackquack.material.shape.SquircleShape
 import team.duckie.quackquack.ui.QuackImage
@@ -45,12 +29,24 @@ import team.duckie.quackquack.ui.sugar.QuackBody3
 @Composable
 internal fun NotificationScreen(
     modifier: Modifier = Modifier,
-    viewModel: NotificationViewModel = activityViewModel(),
+    // viewModel: NotificationViewModel = activityViewModel(),
 ) {
-    val state by viewModel.container.stateFlow.collectAsStateWithLifecycle()
+    // val state by viewModel.container.stateFlow.collectAsStateWithLifecycle()
 
     Column(modifier = modifier) {
-        QuackTopAppBar(
+        // TODO(EvergreenTree97) : 알림 화면 구현 후 제거
+        Column(
+            modifier = Modifier.fillMaxSize(),
+            verticalArrangement = Arrangement.Top,
+            horizontalAlignment = Alignment.CenterHorizontally,
+        ) {
+            Spacer(space = 120.dp)
+            NoItemScreen(
+                title = "아직 알림기능이 준비중입니다",
+                description = "조금만 기다려주세요:)",
+            )
+        }
+        /*QuackTopAppBar(
             leadingIcon = QuackIcon.Outlined.ArrowBack,
             leadingText = stringResource(id = R.string.notification),
             onLeadingIconClick = viewModel::clickBackPress,
@@ -107,10 +103,11 @@ internal fun NotificationScreen(
                     }
                 }
             }
-        }
+        }*/
     }
 }
 
+@Suppress("UnusedPrivateMember")
 @Composable
 private fun NotificationItem(
     thumbnailUrl: String,
