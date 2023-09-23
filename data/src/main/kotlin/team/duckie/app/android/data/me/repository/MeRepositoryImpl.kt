5be837dc17ca893e37404a57c6e05f5dc3731060
EvergreenTree97 <tkdfhr7497@naver.com>
2023-09-23 12:35:18 +0900

    fix: getMe를 호출할 때, 매번 get auth/token을 하는 문제 수정

--- data/src/main/kotlin/team/duckie/app/android/data/me/repository/MeRepositoryImpl.kt
+++ data/src/main/kotlin/team/duckie/app/android/data/me/repository/MeRepositoryImpl.kt
@@ -11,7 +11,6 @@ import androidx.datastore.core.DataStore
 import androidx.datastore.preferences.core.Preferences
 import androidx.datastore.preferences.core.edit
 import kotlinx.coroutines.flow.first
-import team.duckie.app.android.common.kotlin.exception.ExceptionCode.ClientMeIdNull
 import team.duckie.app.android.common.kotlin.exception.ExceptionCode.ClientMeTokenNull
 import team.duckie.app.android.common.kotlin.exception.ExceptionCode.ServerUserIdStrange
 import team.duckie.app.android.common.kotlin.exception.duckieClientLogicProblemException
@@ -34,7 +33,10 @@ class MeRepositoryImpl @Inject constructor(
     private var isProceedEnabled: Boolean? = null
     private var me: User? = null
     override suspend fun getMe(): User {
-        // 0. DevMode 에서 API
+        return me ?: checkAndGetMe()
+    }
+
+    private suspend fun checkAndGetMe(): User {
         if (!isStageChecked) {
             isStageChecked = true
             devModeDataSource.setApiEnvironment(getIsStage())
@@ -47,19 +49,17 @@ class MeRepositoryImpl @Inject constructor(
         val meToken = getMeToken() ?: duckieClientLogicProblemException(code = ClientMeTokenNull)
 
         // 2. 토큰 검증한다.
-        val accessTokenValid = authDataSource.checkAccessToken(meToken).userId > 0
+        val accessTokenCheckResponse = authDataSource.checkAccessToken(meToken)
+        val accessTokenValid = accessTokenCheckResponse.userId > 0
 
         if (accessTokenValid) {
-            // 3. DataStore 에 id 값이 있는지 체크
-            val meId = getMeId() ?: duckieClientLogicProblemException(code = ClientMeIdNull)
-
             // 4. me 객체값이 초기화 되었는지 확인
             return me ?: kotlin.run {
                 // 5. accessToken 관련 설정
                 authDataSource.attachAccessTokenToHeader(meToken)
 
                 // 6. id 기반으로 User 가져온 뒤 앱 내에 User 값 설정
-                val user = userDataSource.get(meId)
+                val user = userDataSource.get(accessTokenCheckResponse.userId)
                 setMe(user)
 
                 // 7. User 값 반환
@@ -88,12 +88,6 @@ class MeRepositoryImpl @Inject constructor(
         dataStore.edit { preferences ->
             preferences.remove(PreferenceKey.Account.AccessToken)
         }
-    }
-
-    private suspend fun getMeId(): Int? {
-        // TODO(riflockle7): 더 좋은 구현 방법이 있을까?
-        // ref: https://medium.com/androiddevelopers/datastore-and-synchronous-work-576f3869ec4c
-        return dataStore.data.first()[PreferenceKey.User.Id]?.toInt()
     }
 
     private suspend fun getMeToken(): String? {
