b088fc1362e23e3b1784e736342f2d7f4ebd56de
Choi Sang Rok <tkdfhr7497@naver.com>
2023-09-24 15:10:31 +0900

    스플래시에 로띠 적용 (#653)
    * chore: lottie 버전 올리기 및 의존성 추가
    
    * feat: lottie animation 추가
    
    * fix: animation 시간 조정

--- presentation/src/main/kotlin/team/duckie/app/android/presentation/screen/IntroScreen.kt
+++ presentation/src/main/kotlin/team/duckie/app/android/presentation/screen/IntroScreen.kt
@@ -13,17 +13,21 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.DpSize
 import androidx.compose.ui.unit.dp
+import com.airbnb.lottie.compose.LottieAnimation
+import com.airbnb.lottie.compose.LottieCompositionSpec
+import com.airbnb.lottie.compose.LottieConstants
+import com.airbnb.lottie.compose.rememberLottieComposition
 import org.orbitmvi.orbit.compose.collectAsState
 import team.duckie.app.android.common.android.intent.goToMarket
 import team.duckie.app.android.common.compose.activityViewModel
@@ -40,6 +44,9 @@ internal fun IntroScreen(
     viewModel: IntroViewModel = activityViewModel(),
 ) {
     val activity = LocalContext.current as Activity
+    val composition by rememberLottieComposition(
+        spec = LottieCompositionSpec.RawRes(R.raw.bg_splash),
+    )
 
     Column(
         modifier = Modifier
@@ -47,8 +54,8 @@ internal fun IntroScreen(
             .background(color = QuackColor.White.value)
             .padding(systemBarPaddings)
             .padding(
-                top = 78.dp,
-                bottom = 34.dp,
+                top = 65.dp,
+                bottom = 38.dp,
             ),
         verticalArrangement = Arrangement.SpaceBetween,
         horizontalAlignment = Alignment.End,
@@ -71,16 +78,9 @@ internal fun IntroScreen(
             )
             QuackHeadLine1(text = stringResource(R.string.intro_slogan))
         }
-        QuackImage(
-            modifier = Modifier
-                .size(
-                    size = DpSize(
-                        width = 276.dp,
-                        height = 255.dp,
-                    ),
-                )
-                .offset(x = 125.dp),
-            src = R.drawable.img_duckie_intro,
+        LottieAnimation(
+            composition = composition,
+            iterations = LottieConstants.IterateForever,
         )
     }
 
