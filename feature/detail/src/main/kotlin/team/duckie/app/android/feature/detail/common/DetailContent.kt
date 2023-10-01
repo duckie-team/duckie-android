d9d796b91c0914448a637d89a68af6fa3142e2c3
limsaehyun <sh00710000@gmail.com>
2023-10-01 16:29:50 +0900

    [QA-46] fix: home detail tag LazyRow to FlowRow

--- feature/detail/src/main/kotlin/team/duckie/app/android/feature/detail/common/DetailContent.kt
+++ feature/detail/src/main/kotlin/team/duckie/app/android/feature/detail/common/DetailContent.kt
@@ -5,12 +5,14 @@
  * Please see full license: https://github.com/duckie-team/duckie-android/blob/develop/LICENSE
  */
 
-@file:OptIn(ExperimentalQuackQuackApi::class)
+@file:OptIn(ExperimentalQuackQuackApi::class, ExperimentalLayoutApi::class)
 
 package team.duckie.app.android.feature.detail.common
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.ExperimentalLayoutApi
+import androidx.compose.foundation.layout.FlowRow
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -19,12 +21,11 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.lazy.LazyRow
-import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.key
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -37,6 +38,7 @@ import coil.compose.AsyncImage
 import team.duckie.app.android.common.compose.GetHeightRatioW328H240
 import team.duckie.app.android.common.compose.ui.QuackMaxWidthDivider
 import team.duckie.app.android.common.compose.ui.quack.QuackProfileImage
+import team.duckie.app.android.common.kotlin.fastForEach
 import team.duckie.app.android.feature.detail.R
 import team.duckie.app.android.feature.detail.viewmodel.state.DetailState
 import team.duckie.quackquack.material.QuackColor
@@ -125,19 +127,22 @@ internal fun DetailContentLayout(
         Spacer(modifier = Modifier.height(12.dp))
 
         // 태그 목록
-        LazyRow(
-            horizontalArrangement = Arrangement.spacedBy(4.dp),
-            contentPadding = PaddingValues(
+        FlowRow(
+            modifier = Modifier.padding(
                 horizontal = 16.dp,
                 vertical = 4.dp,
             ),
+            horizontalArrangement = Arrangement.spacedBy(8.dp),
+            verticalArrangement = Arrangement.spacedBy(4.dp)
         ) {
-            items(items = state.tagNames) { tagName ->
-                QuackTag(
-                    text = tagName,
-                    style = QuackTagStyle.GrayscaleFlat,
-                    onClick = { tagItemClick(tagName) },
-                )
+            state.tagNames.fastForEach { tagName ->
+                key(tagName) {
+                    QuackTag(
+                        text = tagName,
+                        style = QuackTagStyle.GrayscaleFlat,
+                        onClick = { tagItemClick(tagName) },
+                    )
+                }
             }
         }
 
